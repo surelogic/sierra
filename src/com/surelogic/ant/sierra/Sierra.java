20c82b9cb71df978d145f6abdcaf35cf3c931d79
Ethan Urie <ethan.urie@surelogic.com>
2007-07-25 19:28:11 +0000

    Moved to the sierra-tools project.
    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/sierra-ant@629 d02ae147-ee12-4a35-9452-91c89e8b2592

--- src/com/surelogic/ant/sierra/Sierra.java
+++ src/com/surelogic/ant/sierra/Sierra.java
@@ -48,6 +48,7 @@ task's jar.
 package com.surelogic.ant.sierra;
 
 import java.io.File;
+import java.util.Arrays;
 import java.util.Vector;
 
 import org.apache.tools.ant.BuildException;
@@ -58,13 +59,36 @@ import org.apache.tools.ant.Task;
  * 
  */
 public class Sierra extends Task {
-	private String serverURL = null;
-	private File resultsDir = null;
-	private Project project = null;
-	private boolean clean = false;
-	private Tools tools = null;
-	private File srcdir = null;
-	private File bindir = null;
+	private org.apache.tools.ant.Project proj = getProject();
+	
+	// Optional attribute, if present, we send the WSDL file to this server
+	private String serverURL = null; 										
+	
+	// Optional, if omitted, the system's tmp folder is used
+	private File destDir= new File(System.getProperty("java.io.tmp")); 		
+	
+	// Required
+	private Project project = null; 	
+	
+	// Optional, defaults to false
+	private boolean clean = false; 		
+	
+    // Optional, if omitted, all tools are run and PMD's java version is set to 1.5
+	private Tools tools = null;    		
+	
+    // Optional
+	private File srcdir = null;			
+	
+    // Optional
+	private File bindir = null;			
+	
+	/* *********************** CONSTANTS *******************************/
+	private final static String[] toolList = new String[]{"findbugs", "pmd"};
+	private static final String DEFAULT_PMD_JAVA_VERSION = "1.5";
+	
+	static{
+		Arrays.sort(toolList);
+	}
 
 	/**
 	 * @see Task
@@ -72,47 +96,42 @@ public class Sierra extends Task {
 	public void execute() {
 		validateParameters();
 		runTools();
-		parseResults();
 		generateWSDL();
 		if (serverURL != null) {
 			uploadWSDL();
 		}
-		cleanup();
+		
+		if (clean) {
+			cleanup();
+		}
 	}
 
 	/**
 	 * Runs the tools
 	 */
 	private void runTools() {
-
-	}
-
-	/**
-	 * Parses the tools' results files into the SIERRA database
-	 */
-	private void parseResults() {
-
+		log("Running tools...", org.apache.tools.ant.Project.MSG_INFO);
 	}
 
 	/**
 	 * Generates a WSDL file from the updated database
 	 */
 	private void generateWSDL() {
-
+		log("Generating the WSDL file...", org.apache.tools.ant.Project.MSG_INFO);
 	}
 
 	/**
 	 * Optional action. Uploads the generated WSDL file to the desired server.
 	 */
 	private void uploadWSDL() {
-
+		log("Uploading the WSDL file to " + serverURL + "...", org.apache.tools.ant.Project.MSG_INFO);
 	}
 
 	/**
 	 * Cleans up after the task
 	 */
 	private void cleanup() {
-
+		log("Cleaning up...", org.apache.tools.ant.Project.MSG_INFO);
 	}
 
 	/**
@@ -120,33 +139,30 @@ public class Sierra extends Task {
 	 * invalid, this method will throw a BuildException
 	 */
 	public void validateParameters() {
+		if(destDir != null && !destDir.isDirectory()){
+			throw new BuildException(
+					"'destdir' must be a valid directory.");
+		}
+		
+		if(srcdir == null){
+			// TODO ensure that there are source directories defined later
+		}
+		
+		if (bindir == null){
+			// TODO ensure that there are binary directories defined
+		}
 
 		if (project == null) {
 			throw new BuildException(
-					"No projects were defined. The <projects> sub-tag is required.");
+					"No project was defined. The <projects> sub-tag is required.");
 		} else {
 			project.validate();
 		}
 
-		if (tools == null) {
-			throw new BuildException(
-					"No tools sub-tag was defined. The <tools> sub-tag is required.");
-		} else {
+		if (tools != null) {
 			tools.validate();
 		}
 
-	}
-
-	public void setServerURL(String serverURL) {
-		this.serverURL = serverURL;
-	}
-
-	public String getServerURL() {
-		return serverURL;
-	}
-
-	public void addConfiguredTools(Tools tools) {
-		this.tools = tools;
 	}
 
 	/**
@@ -157,82 +173,74 @@ public class Sierra extends Task {
 	 * 
 	 */
 	public class Tools {
-		private File baseDir = null;
-		private String jdkVersion = null;
-		private File resultsDir = new File(System.getProperty("java.io.tmpdir"));
+		private PmdConfig pmdConfig = null;
+		private String[] exclude = null;
 
 		public void validate() {
-			if (baseDir == null) {
-				throw new BuildException(
-						"No baseDir was set for the <tools> sub-tag. This is required.");
-			} else {
-				if (!baseDir.isDirectory()) {
-					throw new BuildException(
-							"The file specified by the baseDir parameter must be a valid directory.");
+			if(exclude != null){
+				for (String tool : exclude) {
+					if(Arrays.binarySearch(toolList, tool.toLowerCase()) < 0){
+						StringBuffer buf = new StringBuffer();
+						buf.append(tool);
+						buf.append(" is not a valid tool name. Valid tool names are: \n");
+						for (String toolName : toolList) {
+							buf.append(toolName);
+							buf.append("\n");
+						}
+        				throw new BuildException(buf.toString());
+					}
 				}
-				// TODO Ensure this is the proper directory - i.e. it contains
-				// our tools
 			}
+			if(pmdConfig == null){
+				pmdConfig = new PmdConfig();
+				pmdConfig.setJavaVersion(DEFAULT_PMD_JAVA_VERSION);
+			}
+			else{
+				pmdConfig.validate();
+			}
+		}
+		
+		public void setExclude(String list){
+			exclude = list.split(",");
+			for (int i = 0; i < exclude.length; i++){
+				exclude[i] = exclude[i].trim();
+			}
+		}
+		
+		public String[] getExclude(){
+			return exclude;
+		}
+		
+		public void addConfiguredPmdConfig(PmdConfig config){
+			this.pmdConfig = config;
+		}
 
-			if (jdkVersion == null) {
+		public PmdConfig getPmdConfig(){
+			return pmdConfig;
+		}
+	}
+	
+	/**
+	 * Represents a configuration attribute for the PMD tool
+	 * @author ethan
+	 *
+	 */
+	public class PmdConfig {
+		private String javaVersion = null;
+		
+		public void validate(){
+			if(!javaVersion.matches("\\d+\\.\\d+(\\.\\d+)*")){
 				throw new BuildException(
-						"The jdkVersion parameter is required.");
-			} else {
-				// TODO Ensure that the string adhers to a valid version number
-				// format, i.e., xx.xx.xx
-			}
-
-			if (!resultsDir.isDirectory()) {
-				throw new BuildException(
-						"The parameter, resultsDir, must be a valid directory. "
-								+ resultsDir.getAbsolutePath()
-								+ " is not valid.");
+					"Invalid version string for pmdconfig's 'javaVersion' attribute. Must be of the form: XX.XX.XX where X is a number, such as 1.4.2.");
 			}
 		}
-
-		/**
-		 * @return the baseDir
-		 */
-		public final File getBaseDir() {
-			return baseDir;
+		
+		public void setJavaVersion(String version){
+			this.javaVersion = version;
 		}
-
-		/**
-		 * @param baseDir
-		 *            the baseDir to set
-		 */
-		public final void setBaseDir(File baseDir) {
-			this.baseDir = baseDir;
-		}
-
-		/**
-		 * @return the jdkVersion
-		 */
-		public final String getJdkVersion() {
-			return jdkVersion;
-		}
-
-		/**
-		 * @param jdkVersion
-		 *            the jdkVersion to set
-		 */
-		public final void setJdkVersion(String jdkVersion) {
-			this.jdkVersion = jdkVersion;
-		}
-
-		/**
-		 * @return the resultsDir
-		 */
-		public final File getResultsDir() {
-			return resultsDir;
-		}
-
-		/**
-		 * @param resultsDir
-		 *            the resultsDir to set
-		 */
-		public final void setResultsDir(File resultsDir) {
-			this.resultsDir = resultsDir;
+		
+		public String getJavaVersion(){
+			return javaVersion;
 		}
 	}
 
@@ -246,24 +254,24 @@ public class Sierra extends Task {
 		private String name = null;
 
 		// If set, this will override the baseDir value set in the Projects
-		private File baseDir = null;
-		private Source source = null;
-		private Binary binary = null;
+		private File dir = null;
+		private Vector<Source> sources = new Vector<Source>();
+		private Vector<Binary> binaries = new Vector<Binary>();
 
 		public void validate() {
 
 			if (name == null) {
 				throw new BuildException(
-						"Parameter 'name' is required for project.");
+						"Parameter 'name' is required for 'project'.");
 			}
-			if (baseDir == null ){
+			if (dir == null) {
 				throw new BuildException(
 						"Parameter 'baseDir' is required if it is not defined for the enclosing <projects> tag.");
-			} else if (baseDir != null) {
-				if (!baseDir.isDirectory()) {
+			} else if (dir != null) {
+				if (!dir.isDirectory()) {
 					throw new BuildException(
 							"Parameter 'baseDir' must be a valid directory. "
-									+ baseDir.getAbsolutePath()
+									+ dir.getAbsolutePath()
 									+ " is not a valid directory.");
 				}
 			}
@@ -278,36 +286,112 @@ public class Sierra extends Task {
 		}
 
 		public final File getBaseDir() {
-			return baseDir;
+			return dir;
 		}
 
 		public final void setBaseDir(File baseDir) {
-			this.baseDir = baseDir;
-		}
-		
-		public void addConfiguredSource(Source src){
-			this.source = src;
+			this.dir = baseDir;
 		}
 
-		public void addConfiguredBinary(Binary bin){
-			this.binary = bin;
+		public void addConfiguredSource(Source src) {
+			sources.add(src);
 		}
-		
-		public Source getSource(){
-			return source;
+
+		public void addConfiguredBinary(Binary bin) {
+			binaries.add(bin);
 		}
-		
-		public Binary getBinary(){
-			return binary;
+
+		public Vector<Source> getSources() {
+			return sources;
+		}
+
+		public Vector<Binary> getBinaries() {
+			return binaries;
 		}
 	}
-	
-	public class Source{
+
+	public class Source {
 		
+
+	}
+
+	public class Binary {
+
 	}
 	
-	public class Binary{
-		
+	
+	
+	/******************************************************
+	 * Getters and Setters for attributes
+	 ******************************************************/
+	
+	public void setServerURL(String serverURL) {
+		this.serverURL = serverURL;
+	}
+
+	public String getServerURL() {
+		return serverURL;
+	}
+
+	public void addConfiguredTools(Tools tools) {
+		this.tools = tools;
+	}
+
+
+	/**
+	 * @return the destDir
+	 */
+	public final File getDestDir() {
+		return destDir;
+	}
+
+	/**
+	 * @param destDir the destDir to set
+	 */
+	public final void setDestDir(File destDir) {
+		this.destDir = destDir;
+	}
+
+	/**
+	 * @return the clean
+	 */
+	public final boolean isClean() {
+		return clean;
+	}
+
+	/**
+	 * @param clean the clean to set
+	 */
+	public final void setClean(boolean clean) {
+		this.clean = clean;
+	}
+
+	/**
+	 * @return the srcdir
+	 */
+	public final File getSrcdir() {
+		return srcdir;
+	}
+
+	/**
+	 * @param srcdir the srcdir to set
+	 */
+	public final void setSrcdir(File srcdir) {
+		this.srcdir = srcdir;
+	}
+
+	/**
+	 * @return the bindir
+	 */
+	public final File getBindir() {
+		return bindir;
+	}
+
+	/**
+	 * @param bindir the bindir to set
+	 */
+	public final void setBindir(File bindir) {
+		this.bindir = bindir;
 	}
 
 }
