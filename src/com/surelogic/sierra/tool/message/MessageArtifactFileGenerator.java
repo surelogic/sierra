8ff14a0f90668de868d893080ef78f5f5b223c20
Tanmay Sinha <tanmay.sinha@surelogic.com>
2007-07-30 20:10:09 +0000

    Log changes
    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/sierra-tool@734 d02ae147-ee12-4a35-9452-91c89e8b2592

--- src/com/surelogic/sierra/tool/message/MessageArtifactFileGenerator.java
+++ src/com/surelogic/sierra/tool/message/MessageArtifactFileGenerator.java
@@ -5,9 +5,6 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
