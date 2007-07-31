dcb97719302feafb039b4775ca58306a06613871
Tim Halloran <tim.halloran@surelogic.com>
2007-07-31 21:10:30 +0000

    Working
    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/sierra-client-eclipse@756 d02ae147-ee12-4a35-9452-91c89e8b2592

--- src/com/surelogic/sierra/client/eclipse/model/FindingsColumn.java
+++ src/com/surelogic/sierra/client/eclipse/model/FindingsColumn.java
@@ -2,6 +2,19 @@ package com.surelogic.sierra.client.eclipse.model;
 
 public enum FindingsColumn {
 
-	PACKAGE, CLASS
+	PROJECT("Project__PRJ");
 
+	private final String f_title;
+
+	private FindingsColumn(String title) {
+		f_title = title;
+	}
+
+	public String getColumnWithTitle() {
+		return this.toString() + " \"" + f_title + "\"";
+	}
+
+	public String getColumnWithTitleAndTreeDivider() {
+		return this.toString() + " \"" + f_title + "|\"";
+	}
 }
--- src/com/surelogic/sierra/client/eclipse/model/FindingsOrganization.java
+++ src/com/surelogic/sierra/client/eclipse/model/FindingsOrganization.java
@@ -1,11 +1,39 @@
 package com.surelogic.sierra.client.eclipse.model;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
+/**
+ * This class defines one organization of the findings for the findings view.
+ */
 public final class FindingsOrganization {
 
 	private final List<FindingsColumn> f_treePart = new ArrayList<FindingsColumn>();
 
 	private final List<FindingsColumn> f_tablePart = new ArrayList<FindingsColumn>();
+
+	public List<FindingsColumn> getMutableTreePart() {
+		return f_treePart;
+	}
+
+	public List<FindingsColumn> getMutableTablePart() {
+		return f_tablePart;
+	}
+
+	public String getColumnList() {
+		Iterator<FindingsColumn> treePartIterator = f_treePart.iterator();
+		boolean moreColumns = treePartIterator.hasNext();
+		while (moreColumns) {
+			FindingsColumn column = treePartIterator.next();
+			moreColumns = treePartIterator.hasNext();
+			if (moreColumns) {
+				column.getColumnWithTitle();
+			} else {
+				column.getColumnWithTitleAndTreeDivider();
+			}
+
+		}
+		return null;
+	}
 }
