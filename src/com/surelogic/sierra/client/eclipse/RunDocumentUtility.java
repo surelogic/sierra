1fb74228a855dcfcc405976767354024fd03859f
Tim Halloran <tim.halloran@surelogic.com>
2007-08-03 20:24:18 +0000

    Integration of Ant task
    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/sierra-client-eclipse@809 d02ae147-ee12-4a35-9452-91c89e8b2592

---
+++ src/com/surelogic/sierra/client/eclipse/RunDocumentUtility.java
@@ -0,0 +1,14 @@
+package com.surelogic.sierra.client.eclipse;
+
+import java.io.File;
+
+public final class RunDocumentUtility {
+	
+	private RunDocumentUtility() {
+		// no instances
+	}
+	
+	public static void loadRunDocument(final File runDocument) {
+		// TODO: call stuff :-)
+	}
+}
