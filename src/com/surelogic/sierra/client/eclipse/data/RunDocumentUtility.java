e72c0c5bd7f57187cb249b14671a28d51c319dd0
Tim Halloran <tim.halloran@surelogic.com>
2007-08-03 21:19:49 +0000

    Working
    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/sierra-client-eclipse@826 d02ae147-ee12-4a35-9452-91c89e8b2592

---
+++ src/com/surelogic/sierra/client/eclipse/data/RunDocumentUtility.java
@@ -0,0 +1,22 @@
+package com.surelogic.sierra.client.eclipse.data;
+
+import java.io.File;
+
+public final class RunDocumentUtility {
+
+	private RunDocumentUtility() {
+		// no instances
+	}
+
+	/**
+	 * Parses a run document into the database and generates findings. When this
+	 * method is completed the run document has been fully loaded into the
+	 * Sierra client and is ready to be examined via the user interface.
+	 * 
+	 * @param runDocument
+	 *            the run document.
+	 */
+	public static void loadRunDocument(final File runDocument) {
+		// TODO
+	}
+}
