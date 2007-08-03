b547cecbae64de773153325d0b8677e8a6b2861b
Tim Halloran <tim.halloran@surelogic.com>
2007-08-03 21:14:30 +0000

    Working
    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/sierra-jdbc@824 d02ae147-ee12-4a35-9452-91c89e8b2592

---
+++ src/com/surelogic/sierra/schema/SchemaScriptUtility.java
@@ -0,0 +1,18 @@
+package com.surelogic.sierra.schema;
+
+import java.net.URL;
+
+public final class SchemaScriptUtility {
+
+	private SchemaScriptUtility() {
+		// no instances
+	}
+
+	public static final String DATABASE_SQL = "/com/surelogic/sierra/db/schema01.sql";
+
+	public static URL getDatabaseSQL() {
+		final URL result = SchemaScriptUtility.class.getResource(DATABASE_SQL);
+		return result;
+	}
+
+}
