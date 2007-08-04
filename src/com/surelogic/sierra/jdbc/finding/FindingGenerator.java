3dd3a6451f7243800cba5e253690ca00329f0256
Nathan Boy <nathan.boy@surelogic.com>
2007-08-04 12:00:26 +0000

    Fixed performance problems.
    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/sierra-jdbc@839 d02ae147-ee12-4a35-9452-91c89e8b2592

--- src/com/surelogic/sierra/jdbc/finding/FindingGenerator.java
+++ src/com/surelogic/sierra/jdbc/finding/FindingGenerator.java
@@ -7,10 +7,6 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 import com.surelogic.sierra.tool.message.Importance;
 import com.surelogic.sierra.tool.message.Priority;
@@ -33,13 +29,12 @@ public class FindingGenerator {
 			+ " )"
 			+ " WHERE A.RUN_ID = ? AND A.FINDING_ID IS NULL";
 
-	private static final String UPDATE_ARTIFACT_FINDING = "UPDATE ARTIFACT A SET A.FINDING_ID = ? WHERE A.ID = ?";
-
 	private static final String UNASSIGNED_ARTIFACTS_SELECT = "SELECT A.ID,A.PRIORITY,A.SEVERITY,S.HASH,CU.CLASS_NAME,CU.PACKAGE_NAME,A.FINDING_TYPE_ID"
 			+ " FROM ARTIFACT A, SOURCE_LOCATION S, COMPILATION_UNIT CU"
 			+ " WHERE"
 			+ " A.FINDING_ID IS NULL AND A.RUN_ID = ? AND S.ID = A.PRIMARY_SOURCE_LOCATION_ID AND CU.ID = S.COMPILATION_UNIT_ID";
 
+	public static final String MATCH_SELECT = "SELECT FINDING_ID FROM SIERRA_MATCH WHERE HASH = ? AND CLASS_NAME = ? AND PACKAGE_NAME = ? AND FINDING_TYPE_ID = ?";
 	public static final String MATCH_INSERT = "INSERT INTO SIERRA_MATCH (HASH, CLASS_NAME, PACKAGE_NAME, FINDING_TYPE_ID, FINDING_ID, TRAIL_ID) VALUES (?,?,?,?,?,?)";
 	public static final String FINDING_INSERT = "INSERT INTO FINDING (TRAIL_ID, IMPORTANCE) VALUES (?,?)";
 	public static final String TRAIL_INSERT = "INSERT INTO TRAIL (UID) VALUES (?)";
@@ -48,10 +43,9 @@ public class FindingGenerator {
 	private final PreparedStatement updateArtifactsWithExistingMatch;
 	private final PreparedStatement unassignedArtifacts;
 	private final PreparedStatement insertMatch;
-
+	private final PreparedStatement selectMatch;
 	private final PreparedStatement insertFinding;
 	private final PreparedStatement insertTrail;
-	private final PreparedStatement updateArtifact;
 	private final Connection conn;
 
 	public FindingGenerator(Connection conn) {
@@ -59,11 +53,12 @@ public class FindingGenerator {
 		try {
 			insertMatch = conn.prepareStatement(MATCH_INSERT,
 					Statement.RETURN_GENERATED_KEYS);
+			selectMatch = conn.prepareStatement(MATCH_SELECT,
+					Statement.RETURN_GENERATED_KEYS);
 			insertTrail = conn.prepareStatement(TRAIL_INSERT,
 					Statement.RETURN_GENERATED_KEYS);
 			insertFinding = conn.prepareStatement(FINDING_INSERT,
 					Statement.RETURN_GENERATED_KEYS);
-			updateArtifact = conn.prepareStatement(UPDATE_ARTIFACT_FINDING);
 			updateArtifactsWithExistingMatch = conn
 					.prepareStatement(UPDATE_ARTIFACTS_WITH_EXISTING_MATCH);
 			unassignedArtifacts = conn
@@ -78,52 +73,42 @@ public class FindingGenerator {
 
 			unassignedArtifacts.setLong(1, runId);
 			unassignedArtifacts.setMaxRows(CHUNK_SIZE);
-			List<ArtifactResult> results = new ArrayList<ArtifactResult>(
-					CHUNK_SIZE);
-			Map<MatchRecord.PK, Long> findingMap = new HashMap<MatchRecord.PK, Long>(
-					CHUNK_SIZE);
-			do {
-				results.clear();
-				findingMap.clear();
-				updateArtifactsWithExistingMatch.setLong(1, runId);
-				updateArtifactsWithExistingMatch.executeUpdate();
-				conn.commit();
-				ResultSet result = unassignedArtifacts.executeQuery();
-				while (result.next()) {
-					ArtifactResult art = new ArtifactResult();
-					int idx = 1;
-					art.id = result.getLong(idx++);
-					art.p = Priority.values()[result.getInt(idx++)];
-					art.s = Severity.values()[result.getInt(idx++)];
-					art.m = new MatchRecord();
-					idx = art.m.readPK(result, idx);
-					results.add(art);
+			updateArtifactsWithExistingMatch.setLong(1, runId);
+			updateArtifactsWithExistingMatch.executeUpdate();
+			conn.commit();
+			ResultSet result = unassignedArtifacts.executeQuery();
+			int counter = 0;
+			while (result.next()) {
+				ArtifactResult art = new ArtifactResult();
+				int idx = 1;
+				art.id = result.getLong(idx++);
+				art.p = Priority.values()[result.getInt(idx++)];
+				art.s = Severity.values()[result.getInt(idx++)];
+				art.m = new MatchRecord();
+				idx = art.m.readPK(result, idx);
+				art.m.fillWithPK(selectMatch, 1);
+				ResultSet match = selectMatch.executeQuery();
+				if (!match.next()) {
+					counter++;
+					MatchRecord m = art.m;
+					TrailRecord t = new TrailRecord();
+					FindingRecord f = new FindingRecord();
+					f.setTrail(t);
+					f.setImportance(calculateImportance(art.s, art.p));
+					m.setFinding(f);
+					m.setTrail(t);
+					insert(insertTrail, t);
+					insert(insertFinding, f);
+					m.fill(insertMatch, 1);
+					insertMatch.executeUpdate();
 				}
-				result.close();
-				for (ArtifactResult art : results) {
-					Long findingId = findingMap.get(art.m.getId());
-					if (findingId == null) {
-						MatchRecord m = art.m;
-						TrailRecord t = new TrailRecord();
-						FindingRecord f = new FindingRecord();
-						f.setTrail(t);
-						f.setImportance(calculateImportance(art.s, art.p));
-						m.setFinding(f);
-						m.setTrail(t);
-						insert(insertTrail, t);
-						insert(insertFinding, f);
-						m.fill(insertMatch, 1);
-						insertMatch.executeUpdate();
-						findingId = f.getId();
-						findingMap.put(art.m.getId(), findingId);
-					}
-					updateArtifact.setLong(1, findingId);
-					updateArtifact.setLong(2, art.id);
-					updateArtifact.executeUpdate();
+				if (counter == CHUNK_SIZE) {
+					conn.commit();
+					counter = 0;
 				}
-				conn.commit();
-			} while (results.size() > 0);
-
+			}
+			conn.commit();
+			updateArtifactsWithExistingMatch.executeUpdate();
 			conn.commit();
 		} catch (SQLException e) {
 			sqlError(e);
