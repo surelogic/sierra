9533e0e0cae85dc63f57ff7b1a73d2c8de3b4788
Ethan Urie <ethan.urie@surelogic.com>
2007-08-08 18:03:04 +0000

    removed unused methods.
    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/sierra-tool@951 d02ae147-ee12-4a35-9452-91c89e8b2592

--- src/com/surelogic/sierra/tool/ant/SierraAnalysis.java
+++ src/com/surelogic/sierra/tool/ant/SierraAnalysis.java
@@ -68,7 +68,6 @@ package com.surelogic.sierra.tool.ant;
 
 import java.io.File;
 import java.io.FilenameFilter;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
@@ -80,11 +79,9 @@ import java.util.Vector;
 import org.apache.tools.ant.AntClassLoader;
 import org.apache.tools.ant.BuildException;
 import org.apache.tools.ant.Task;
-import org.apache.tools.ant.taskdefs.Execute;
-import org.apache.tools.ant.taskdefs.ExecuteWatchdog;
+import org.apache.tools.ant.launch.Launcher;
 import org.apache.tools.ant.taskdefs.Redirector;
 import org.apache.tools.ant.types.CommandlineJava;
-import org.apache.tools.ant.types.Environment;
 import org.apache.tools.ant.types.Path;
 import org.apache.tools.ant.types.RedirectorElement;
 
@@ -103,8 +100,6 @@ import com.surelogic.sierra.tool.message.TigerServiceClient;
 public class SierraAnalysis extends Task {
 	protected Redirector redirector = new Redirector(this);
 	protected RedirectorElement redirectorElement;
-	private Environment env = new Environment();
-	private Long timeout = null;
 	private CommandlineJava cmdl = new CommandlineJava();
 	private boolean uploadSuccessful = false;
 
@@ -603,130 +598,6 @@ public class SierraAnalysis extends Task {
 		this.bindir.append(bindir);
 	}
 
-	/***************************************************************************
-	 * Helper methods taken from Ant's Java task
-	 **************************************************************************/
-
-	/**
-	 * Executes the given classname with the given arguments in a separate VM.
-	 * 
-	 * @param command
-	 *            String[] of command-line arguments.
-	 */
-	private int fork(String[] command) throws BuildException {
-		Execute exe = new Execute(redirector.createHandler(), createWatchdog());
-		setupExecutable(exe, command);
-
-		try {
-			int rc = exe.execute();
-			redirector.complete();
-			if (exe.killedProcess()) {
-				throw new BuildException("Timeout: killed the sub-process");
-			}
-			return rc;
-		} catch (IOException e) {
-			throw new BuildException(e, getLocation());
-		}
-	}
-
-	/**
-	 * Executes the given classname with the given arguments in a separate VM.
-	 * 
-	 * @param command
-	 *            String[] of command-line arguments.
-	 */
-	private void spawn(String[] command) throws BuildException {
-		Execute exe = new Execute();
-		setupExecutable(exe, command);
-		try {
-			exe.spawn();
-		} catch (IOException e) {
-			throw new BuildException(e, getLocation());
-		}
-	}
-
-	/**
-	 * Do all configuration for an executable that is common across the
-	 * {@link #fork(String[])} and {@link #spawn(String[])} methods.
-	 * 
-	 * @param exe
-	 *            executable.
-	 * @param command
-	 *            command to execute.
-	 */
-	private void setupExecutable(Execute exe, String[] command) {
-		exe.setAntRun(getProject());
-		setupWorkingDir(exe);
-		setupEnvironment(exe);
-		setupCommandLine(exe, command);
-	}
-
-	/**
-	 * Set up our environment variables.
-	 * 
-	 * @param exe
-	 *            executable.
-	 */
-	private void setupEnvironment(Execute exe) {
-		String[] environment = env.getVariables();
-		if (environment != null) {
-			for (int i = 0; i < environment.length; i++) {
-				log("Setting environment variable: " + environment[i],
-						org.apache.tools.ant.Project.MSG_VERBOSE);
-			}
-		}
-		exe.setNewenvironment(false);
-		exe.setEnvironment(environment);
-	}
-
-	/**
-	 * Set the working dir of the new process.
-	 * 
-	 * @param exe
-	 *            executable.
-	 * @throws BuildException
-	 *             if the dir doesn't exist.
-	 */
-	private void setupWorkingDir(Execute exe) {
-		if (project.getDir() == null) {
-			project.setDir(getProject().getBaseDir());
-		} else if (!project.getDir().exists()
-				|| !project.getDir().isDirectory()) {
-			throw new BuildException(project.getDir().getAbsolutePath()
-					+ " is not a valid directory", getLocation());
-		}
-		exe.setWorkingDirectory(project.getDir());
-	}
-
-	/**
-	 * Set the command line for the exe. On VMS, hands off to
-	 * {@link #setupCommandLineForVMS(Execute, String[])}.
-	 * 
-	 * @param exe
-	 *            executable.
-	 * @param command
-	 *            command to execute.
-	 */
-	private void setupCommandLine(Execute exe, String[] command) {
-		exe.setCommandline(command);
-	}
-
-	/**
-	 * Create the Watchdog to kill a runaway process.
-	 * 
-	 * @return new watchdog.
-	 * 
-	 * @throws BuildException
-	 *             under unknown circumstances.
-	 * 
-	 * @since Ant 1.5
-	 */
-	protected ExecuteWatchdog createWatchdog() throws BuildException {
-		if (timeout == null) {
-			return null;
-		}
-		return new ExecuteWatchdog(timeout.longValue());
-	}
 
 	public CommandlineJava getCommandLine() {
 		return cmdl;
