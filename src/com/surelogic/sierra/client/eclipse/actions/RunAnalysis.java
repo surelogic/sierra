02f8c688f24d3dd72d44965c41fa285db8bf0bbe
Tanmay Sinha <tanmay.sinha@surelogic.com>
2007-08-10 19:44:16 +0000

    PMD 4 added. PMD 3.9 is removed, also multiple projects are now supported.
    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/sierra-client-eclipse@1025 d02ae147-ee12-4a35-9452-91c89e8b2592

--- src/com/surelogic/sierra/client/eclipse/actions/RunAnalysis.java
+++ src/com/surelogic/sierra/client/eclipse/actions/RunAnalysis.java
@@ -3,6 +3,7 @@ package com.surelogic.sierra.client.eclipse.actions;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -22,6 +23,7 @@ import org.eclipse.ui.IObjectActionDelegate;
 import org.eclipse.ui.IWorkbenchPart;
 
 import com.surelogic.common.eclipse.BalloonUtility;
+import com.surelogic.sierra.client.eclipse.Activator;
 import com.surelogic.sierra.client.eclipse.SLog;
 import com.surelogic.sierra.client.eclipse.data.RunDocumentUtility;
 import com.surelogic.sierra.tool.SierraLogger;
@@ -40,7 +42,15 @@ public final class RunAnalysis implements IObjectActionDelegate {
 
 	private File buildFile;
 
-	private File runDocument;
+	private BuildFileGenerator bfg;
+
+	private Stack<File> runDocuments;
+
+	private static final Status PROPER_TERMINATION = new Status(IStatus.OK,
+			Activator.PLUGIN_ID, "Proper termination");
+
+	private static final Status IMPROPER_TERMINATION = new Status(
+			IStatus.ERROR, Activator.PLUGIN_ID, "Improper termination");
 
 	// private static final Date dateRunTime = Calendar.getInstance().getTime();
 
@@ -50,6 +60,8 @@ public final class RunAnalysis implements IObjectActionDelegate {
 	private static final String SIERRA_RESULTS = ".SierraResults";
 
 	private static final String PARSED_FILE_SUFFIX = ".xml.parsed";
+
+	private static final String ANT_LOG_FILE = "sierra-ant.log";
 
 	public RunAnalysis() {
 
@@ -79,13 +91,18 @@ public final class RunAnalysis implements IObjectActionDelegate {
 					selectedProjects.add((IJavaProject) selection);
 				}
 			}
+
+			List<File> buildFiles = new ArrayList<File>();
+			runDocuments = new Stack<File>();
+
 			for (IJavaProject project : selectedProjects) {
 				// log.info("Generating XML...");
 
 				String projectPath = project.getResource().getLocation()
 						.toString();
-				runDocument = new File(resultRoot + File.separator
+				File runDocument = new File(resultRoot + File.separator
 						+ project.getProject().getName() + PARSED_FILE_SUFFIX);
+				runDocuments.push(runDocument);
 
 				Config config = new Config();
 
@@ -96,60 +113,153 @@ public final class RunAnalysis implements IObjectActionDelegate {
 				config.setJavaVendor(javaVendor);
 				config.setJavaVersion(javaVersion);
 
-				BuildFileGenerator bfg = new BuildFileGenerator(config);
+				bfg = new BuildFileGenerator(config);
 				buildFile = bfg.writeBuildFile();
+				buildFiles.add(buildFile);
 
-				// TODO: FIX THIS - Currently progress monitors are not handled
-				// properly, output from ant task is also lost
-				if (buildFile != null) {
+			}
 
-					Job runBuildfile = new Job("Running Sierra...") {
+			if (buildFiles.size() > 1) {
+				buildFile = bfg.writeMultipleProjectBuildFile(buildFiles);
+			}
 
-						@Override
-						protected IStatus run(IProgressMonitor monitor) {
-							try {
-								AntRunner runner = new AntRunner();
-								runner.setBuildFileLocation(buildFile
-										.getAbsolutePath());
-								runner
-										.setArguments("-Dmessage=Building -verbose");
-								runner.run();
+			// TODO: FIX THIS - Currently progress monitors are not handled
+			// properly, output from ant task is also lost
+			if (buildFile != null) {
 
-							} catch (CoreException e) {
-								log.log(Level.SEVERE,
-										"Core exception while trying to excute ant build task"
-												+ e);
+				// Thread thread = new Thread(new Runnable() {
+				//
+				// public void run() {
+				// try {
+				// AntRunner runner = new AntRunner();
+				// runner.setBuildFileLocation(buildFile
+				// .getAbsolutePath());
+				// runner
+				// .addBuildLogger("org.apache.tools.ant.DefaultLogger");
+				// // runner.setMessageOutputLevel(Project.MSG_DEBUG);
+				// runner.setArguments("-logfile " + resultRoot
+				// + File.separator + ANT_LOG_FILE);
+				// // runner.addBuildListener(className)
+				// BalloonUtility
+				// .showMessage(
+				// "Sierra Analysis Started",
+				// "You may continue to work as the analysis runs. "
+				// + "You will be notified when the analysis has been
+				// completed.");
+				// runner.run();
+				//
+				// while (!runDocuments.isEmpty()) {
+				// File runDocumentHolder = runDocuments.pop();
+				// RunDocumentUtility.loadRunDocument(
+				// runDocumentHolder, null);
+				// }
+				// BalloonUtility
+				// .showMessage("Sierra Analysis Completed",
+				// "You may now examine the analysis results.");
+				//
+				// } catch (CoreException e) {
+				// log.log(Level.SEVERE,
+				// "Core exception while trying to excute ant build task"
+				// + e);
+				// }
+				//
+				// }
+				//
+				// });
+				// thread.start();
+
+				Job runBuildfile = new Job("Running Sierra...") {
+					// Project p = new Project();
+					// p.setUserProperty("ant.file", buildFile
+					// .getAbsolutePath());
+					// p.init();
+					// ProjectHelper helper = ProjectHelper
+					// .getProjectHelper();
+					// p.addReference("ant.projecthelper", helper);
+					// helper.parse(p, buildFile);
+					// p.executeTarget(p.getDefaultTarget());
+					// DefaultLogger consoleLogger = new
+					// DefaultLogger();
+					// consoleLogger.setOutputPrintStream(System.out);
+					// consoleLogger.setErrorPrintStream(System.err);
+					@Override
+					protected IStatus run(IProgressMonitor monitor) {
+						try {
+							monitor.beginTask("Starting analysis...",
+									IProgressMonitor.UNKNOWN);
+							AntRunner runner = new AntRunner();
+							runner.setBuildFileLocation(buildFile
+									.getAbsolutePath());
+							// runner.setMessageOutputLevel(Project.MSG_DEBUG);
+							runner
+									.addBuildLogger("org.apache.tools.ant.DefaultLogger");
+							runner.setArguments("-logfile " + resultRoot
+									+ File.separator + ANT_LOG_FILE);
+							BalloonUtility
+									.showMessage(
+											"Sierra Analysis Started",
+											"You may continue to work as the analysis runs. "
+													+ "You will be notified when the analysis has been completed.");
+							if (!AntRunner.isBuildRunning()) {
+								runner.run(monitor);
+							} else {
+								BalloonUtility
+										.showMessage(
+												"One instance of same sierra analysis is already running",
+												"Please wait for it to finish before restarting");
+
 							}
-							return Status.OK_STATUS;
+							return PROPER_TERMINATION;
+						} catch (CoreException e) {
+							log.log(Level.SEVERE,
+									"Core exception while trying to excute ant build task"
+											+ e);
+						} finally {
+							monitor.done();
 						}
 
-					};
+						return IMPROPER_TERMINATION;
 
-					runBuildfile.setPriority(Job.SHORT);
-					runBuildfile.addJobChangeListener(new JobChangeAdapter() {
+					}
 
-						@Override
-						public void done(IJobChangeEvent event) {
-							RunDocumentUtility.loadRunDocument(runDocument,
-									null);
+				};
+
+				runBuildfile.setPriority(Job.SHORT);
+				runBuildfile.addJobChangeListener(new JobChangeAdapter() {
+
+					@Override
+					public void done(IJobChangeEvent event) {
+						if (event.getResult().equals(PROPER_TERMINATION)) {
+							log.info("Starting database entry...");
+							while (!runDocuments.isEmpty()) {
+								File runDocumentHolder = runDocuments.pop();
+								RunDocumentUtility.loadRunDocument(
+										runDocumentHolder, null);
+							}
 							BalloonUtility
 									.showMessage("Sierra Analysis Completed",
 											"You may now examine the analysis results.");
-
+							log.info("Finished everything");
+						} else {
+							BalloonUtility.showMessage(
+									"Sierra Analysis Completed with errors",
+									"Check the logs.");
 						}
 
-					});
-					runBuildfile.schedule();
-					BalloonUtility
-							.showMessage(
-									"Sierra Analysis Started",
-									"You may continue to work as the analysis runs. "
-											+ "You will be notified when the analysis has been completed.");
+					}
 
-				}
+				});
+				runBuildfile.schedule();
+				BalloonUtility
+						.showMessage(
+								"Sierra Analysis Started",
+								"You may continue to work as the analysis runs. "
+										+ "You will be notified when the analysis has been completed.");
 
 			}
+
 		}
+
 	}
 
 	public void selectionChanged(IAction action, ISelection selection) {
