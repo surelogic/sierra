b9f41f6d7ac7e13f1a62a8a2e0fd52411904acd8
Spencer Whitman <spencer.whitman@surelogic.com>
2007-08-22 18:09:06 +0000

    ASSIGNED - bug 893: Implement new DB structure for products, projects, and qualifiers https://fluid.surelogic.com/bugzilla/show_bug.cgi?id=893
    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/sierra-jdbc@1178 d02ae147-ee12-4a35-9452-91c89e8b2592

---
+++ src/com/surelogic/sierra/jdbc/productProject/ProductProjectRecordFactory.java
@@ -0,0 +1,30 @@
+package com.surelogic.sierra.jdbc.productProject;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import com.surelogic.sierra.jdbc.record.BaseMapper;
+import com.surelogic.sierra.jdbc.record.ProductProjectRecord;
+
+public class ProductProjectRecordFactory {
+
+	private final Connection conn;
+
+	private final BaseMapper pprMapper;
+
+	private ProductProjectRecordFactory(Connection conn) throws SQLException {
+		this.conn = conn;
+
+		pprMapper = new BaseMapper(conn, null, null, null);
+	}
+
+	public static ProductProjectRecordFactory getInstance(Connection conn)
+			throws SQLException {
+		return new ProductProjectRecordFactory(conn);
+	}
+
+	public ProductProjectRecord newProductProject() {
+		return new ProductProjectRecord(pprMapper);
+	}
+
+}
