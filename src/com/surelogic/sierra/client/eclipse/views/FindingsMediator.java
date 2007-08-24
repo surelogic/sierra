6253ebb74726a0da2ef2b82e5c3ff7bc8c7967e1
Tim Halloran <tim.halloran@surelogic.com>
2007-08-24 20:53:22 +0000

    Logging change
    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/sierra-client-eclipse@1209 d02ae147-ee12-4a35-9452-91c89e8b2592

--- src/com/surelogic/sierra/client/eclipse/views/FindingsMediator.java
+++ src/com/surelogic/sierra/client/eclipse/views/FindingsMediator.java
@@ -1,12 +1,52 @@
 package com.surelogic.sierra.client.eclipse.views;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.graphics.Point;
+import org.eclipse.swt.graphics.Rectangle;
+import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.widgets.Combo;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Event;
 import org.eclipse.swt.widgets.ExpandItem;
+import org.eclipse.swt.widgets.Listener;
+import org.eclipse.swt.widgets.Menu;
+import org.eclipse.swt.widgets.MenuItem;
+import org.eclipse.swt.widgets.ToolItem;
+import org.eclipse.swt.widgets.Widget;
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.part.PageBook;
+
+import com.surelogic.adhoc.views.QueryUtility;
+import com.surelogic.sierra.client.eclipse.Data;
+import com.surelogic.sierra.client.eclipse.SLog;
+import com.surelogic.sierra.client.eclipse.model.FindingsModel;
+import com.surelogic.sierra.client.eclipse.model.FindingsOrganization;
+import com.surelogic.sierra.tool.message.Importance;
 
 public final class FindingsMediator {
 
+	private FindingsModel f_manager = new FindingsModel();
+
+	private final Listener f_updateFindingsOverview;
+
+	private final PageBook f_pages;
+
+	private final Widget f_noFindingsPage;
+
+	private final Composite f_findingsPage;
+
 	private final Combo f_projectCombo;
+
+	private final ToolItem f_groupings;
+
+	private final Combo f_groupByCombo;
+
+	private final ToolItem f_filters;
 
 	private final Composite f_topSash;
 
@@ -18,15 +58,133 @@ public final class FindingsMediator {
 
 	private final Composite f_logComp;
 
-	FindingsMediator(Combo projectCombo, Composite topSash,
+	private Composite results = null;
+
+	FindingsMediator(PageBook pages, Widget noFindingsPage,
+			Composite findingsPage, Combo projectCombo, ToolItem groupings,
+			Combo groupByCombo, ToolItem filters, Composite topSash,
 			ExpandItem detailsItem, Composite detailsComp, ExpandItem logItem,
 			Composite logComp) {
+		f_pages = pages;
+		f_noFindingsPage = noFindingsPage;
+		f_findingsPage = findingsPage;
 		f_projectCombo = projectCombo;
+		f_groupings = groupings;
+		f_groupByCombo = groupByCombo;
+		f_filters = filters;
 		f_topSash = topSash;
 		f_detailsItem = detailsItem;
 		f_detailsComp = detailsComp;
 		f_logItem = logItem;
 		f_logComp = logComp;
+
+		final Listener filterCheckListener = new Listener() {
+			public void handleEvent(Event event) {
+				MenuItem item = (MenuItem) event.widget;
+				Importance i = Importance.valueOf(item.getText());
+				if (i != null) {
+					if (item.getSelection()) {
+						f_manager.getFilter().add(i);
+						System.out.println("add:" + i);
+					} else {
+						f_manager.getFilter().remove(i);
+						System.out.println("remove:" + i);
+
+					}
+					// TODO signal to update the results.
+				}
+			}
+		};
+		final Menu filterMenu = new Menu(f_findingsPage.getShell(), SWT.POP_UP);
+		for (Importance i : Importance.values()) {
+			MenuItem item = new MenuItem(filterMenu, SWT.CHECK);
+			item.setText(i.toString());
+			item.addListener(SWT.Selection, filterCheckListener);
+		}
+		f_filters.addListener(SWT.Selection, new Listener() {
+			public void handleEvent(Event event) {
+				Rectangle r = f_filters.getBounds();
+				Point p = new Point(r.x, r.y + r.height);
+				p = f_filters.getDisplay().map(f_filters.getControl(), null, p);
+				filterMenu.setLocation(p);
+				filterMenu.setVisible(true);
+			}
+		});
+
+		f_updateFindingsOverview = new Listener() {
+			public void handleEvent(Event event) {
+				String key = f_groupByCombo.getItem(f_groupByCombo
+						.getSelectionIndex());
+				if (key == null)
+					return;
+				FindingsOrganization org = f_manager.get(key);
+				if (org == null) {
+					SLog.logWarning("no FindingsOrganization for key " + key);
+					return;
+				}
+				String project = f_projectCombo.getText();
+				if (project == null) {
+					SLog.logWarning("no project to qualify key " + key);
+					return;
+				}
+				String query = org.getQuery(project, f_manager.getFilter());
+				System.out.println(key + " selected");
+				System.out.println("query \"" + query + "\"");
+				try {
+					final Connection c = Data.getConnection();
+					try {
+						final Statement st = c.createStatement();
+						try {
+							boolean hasResultSet = st.execute(query);
+							if (hasResultSet) {
+								// result set
+								final ResultSet rs = st.getResultSet();
+								final String[] columnLabels = QueryUtility
+										.getColumnLabels(rs);
+								final String[][] rows = QueryUtility.getRows(
+										rs, 20000);
+								PlatformUI.getWorkbench().getDisplay()
+										.asyncExec(new Runnable() {
+											public void run() {
+												if (results != null) {
+													results.dispose();
+													results = QueryUtility
+															.construct(
+																	f_topSash,
+																	columnLabels,
+																	rows);
+													results
+															.setLayoutData(new GridData(
+																	SWT.FILL,
+																	SWT.FILL,
+																	true, true));
+													f_topSash.layout();
+												}
+											}
+										});
+								// do stuff with result set
+								System.out.println("got a result set");
+
+							} else {
+								// update count or no results
+								int updateCount = st.getUpdateCount();
+							}
+						} catch (SQLException e) {
+							// an error occurred with the query
+							SLog.logError("SQL problem in findings view", e);
+						} finally {
+							st.close();
+						}
+					} finally {
+						c.close();
+					}
+				} catch (SQLException e) {
+					SLog.logError("Could not work with the embedded database",
+							e);
+				}
+			}
+		};
+
 	}
 
 	public void setFocus() {
--- src/com/surelogic/sierra/client/eclipse/views/FindingsView.java
+++ src/com/surelogic/sierra/client/eclipse/views/FindingsView.java
@@ -1,15 +1,8 @@
 package com.surelogic.sierra.client.eclipse.views;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-
 import org.eclipse.jdt.ui.ISharedImages;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.custom.SashForm;
-import org.eclipse.swt.graphics.Point;
-import org.eclipse.swt.graphics.Rectangle;
 import org.eclipse.swt.layout.FillLayout;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
@@ -25,31 +18,29 @@ import org.eclipse.swt.widgets.Menu;
 import org.eclipse.swt.widgets.MenuItem;
 import org.eclipse.swt.widgets.ToolBar;
 import org.eclipse.swt.widgets.ToolItem;
-import org.eclipse.swt.widgets.Tree;
-import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.part.PageBook;
 import org.eclipse.ui.part.ViewPart;
 
-import com.surelogic.adhoc.views.QueryUtility;
 import com.surelogic.common.eclipse.SLImages;
-import com.surelogic.sierra.client.eclipse.Data;
-import com.surelogic.sierra.client.eclipse.SLog;
-import com.surelogic.sierra.client.eclipse.model.FindingsModel;
-import com.surelogic.sierra.client.eclipse.model.FindingsOrganization;
-import com.surelogic.sierra.tool.message.Importance;
 
 public final class FindingsView extends ViewPart {
 
+	private static final String NO_FINDINGS = "No findings ... please run Sierra analysis on a project to generate a set of findings";
+
 	private FindingsMediator f_mediator = null;
-
-	private FindingsModel f_manager = new FindingsModel();
-
-	private Composite results = null;
 
 	@Override
 	public void createPartControl(final Composite parent) {
-		parent.setLayout(new GridLayout());
 
-		final Composite projectSelector = new Composite(parent, SWT.NONE);
+		final PageBook pages = new PageBook(parent, SWT.NONE);
+
+		final Label noFindingsPage = new Label(pages, SWT.WRAP | SWT.CENTER);
+		noFindingsPage.setText(NO_FINDINGS);
+
+		final Composite findingsPage = new Composite(pages, SWT.NONE);
+		findingsPage.setLayout(new GridLayout());
+
+		final Composite projectSelector = new Composite(findingsPage, SWT.NONE);
 		projectSelector.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true,
 				false));
 		final GridLayout gridLayout = new GridLayout();
@@ -64,17 +55,15 @@ public final class FindingsView extends ViewPart {
 		label.setAlignment(SWT.RIGHT);
 		label.setText("Project:");
 
-		final Combo projectCombo = new Combo(projectSelector, SWT.DROP_DOWN);
-		projectCombo.setItems(new String[] { "Test", "JEdit", "Project 3",
-				"Project 4" });
-		projectCombo.select(0);
+		final Combo projectCombo = new Combo(projectSelector, SWT.DROP_DOWN
+				| SWT.READ_ONLY);
 		projectCombo.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true,
 				false));
 
 		/*
 		 * Findings for the project
 		 */
-		final Group findingsGroup = new Group(parent, SWT.NONE);
+		final Group findingsGroup = new Group(findingsPage, SWT.NONE);
 		findingsGroup.setText("Analysis Findings");
 		findingsGroup
 				.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true));
@@ -102,83 +91,6 @@ public final class FindingsView extends ViewPart {
 		groupBy.setText("by");
 		final Combo groupByCombo = new Combo(findingsBar, SWT.DROP_DOWN
 				| SWT.READ_ONLY);
-		groupByCombo.setItems(f_manager.getKeys());
-		groupByCombo.select(0);
-
-		final Listener updateFindingsOverview = new Listener() {
-			public void handleEvent(Event event) {
-				String key = groupByCombo.getItem(groupByCombo
-						.getSelectionIndex());
-				if (key == null)
-					return;
-				FindingsOrganization org = f_manager.get(key);
-				if (org == null) {
-					SLog.logWarning("no FindingsOrganization for key " + key);
-					return;
-				}
-				String project = projectCombo.getText();
-				if (project == null) {
-					SLog.logWarning("no project to qualify key " + key);
-					return;
-				}
-				String query = org.getQuery(project, f_manager.getFilter());
-				System.out.println(key + " selected");
-				System.out.println("query \"" + query + "\"");
-				try {
-					final Connection c = Data.getConnection();
-					try {
-						final Statement st = c.createStatement();
-						try {
-							boolean hasResultSet = st.execute(query);
-							if (hasResultSet) {
-								// result set
-								final ResultSet rs = st.getResultSet();
-								final String[] columnLabels = QueryUtility
-										.getColumnLabels(rs);
-								final String[][] rows = QueryUtility.getRows(
-										rs, 20000);
-								PlatformUI.getWorkbench().getDisplay()
-										.asyncExec(new Runnable() {
-											public void run() {
-												if (results != null) {
-													results.dispose();
-													results = QueryUtility
-															.construct(
-																	topSash,
-																	columnLabels,
-																	rows);
-													results
-															.setLayoutData(new GridData(
-																	SWT.FILL,
-																	SWT.FILL,
-																	true, true));
-													topSash.layout();
-												}
-											}
-										});
-								// do stuff with result set
-								System.out.println("got a result set");
-
-							} else {
-								// update count or no results
-								int updateCount = st.getUpdateCount();
-							}
-						} catch (SQLException e) {
-							// an error occurred with the query
-							SLog.logError("SQL problem in findings view", e);
-						} finally {
-							st.close();
-						}
-					} finally {
-						c.close();
-					}
-				} catch (SQLException e) {
-					SLog.logError("Could not work with the embedded database",
-							e);
-				}
-			}
-		};
-		groupByCombo.addListener(SWT.Selection, updateFindingsOverview);
 
 		/*
 		 * Toolbar for analysis findings
@@ -193,39 +105,6 @@ public final class FindingsView extends ViewPart {
 				.setToolTipText("Configure the filters to be applied to this view");
 		filter.setText("Filters");
 
-		final Listener filterCheckListener = new Listener() {
-			public void handleEvent(Event event) {
-				MenuItem item = (MenuItem) event.widget;
-				Importance i = Importance.valueOf(item.getText());
-				if (i != null) {
-					if (item.getSelection()) {
-						f_manager.getFilter().add(i);
-						System.out.println("add:" + i);
-					} else {
-						f_manager.getFilter().remove(i);
-						System.out.println("remove:" + i);
-
-					}
-					updateFindingsOverview.handleEvent(null);
-				}
-			}
-		};
-		final Menu filterMenu = new Menu(parent.getShell(), SWT.POP_UP);
-		for (Importance i : Importance.values()) {
-			MenuItem item = new MenuItem(filterMenu, SWT.CHECK);
-			item.setText(i.toString());
-			item.addListener(SWT.Selection, filterCheckListener);
-		}
-		filter.addListener(SWT.Selection, new Listener() {
-			public void handleEvent(Event event) {
-				Rectangle r = filter.getBounds();
-				Point p = new Point(r.x, r.y + r.height);
-				p = filter.getDisplay().map(toolBar, null, p);
-				filterMenu.setLocation(p);
-				filterMenu.setVisible(true);
-			}
-		});
-
 		new ToolItem(toolBar, SWT.SEPARATOR);
 
 		final ToolItem export = new ToolItem(toolBar, SWT.PUSH);
@@ -233,7 +112,7 @@ public final class FindingsView extends ViewPart {
 		export.setToolTipText("Export findings to a file");
 		export.setText("Export");
 
-		final Menu toolBarMenu = new Menu(parent.getShell(), SWT.POP_UP);
+		final Menu toolBarMenu = new Menu(findingsPage.getShell(), SWT.POP_UP);
 		final MenuItem showText = new MenuItem(toolBarMenu, SWT.CHECK);
 		showText.setText("Show Text");
 		showText.setSelection(true);
@@ -254,15 +133,6 @@ public final class FindingsView extends ViewPart {
 		toolBar.setMenu(toolBarMenu);
 		groupBy.setMenu(toolBarMenu);
 
-		/*
-		 * Analysis findings
-		 */
-
-		Tree tree = new Tree(topSash, SWT.BORDER | SWT.H_SCROLL | SWT.V_SCROLL
-				| SWT.FULL_SELECTION);
-		tree.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true));
-		results = tree;
-
 		ExpandBar bar = new ExpandBar(sf, SWT.V_SCROLL);
 		int barIndex = 0;
 		// Second item
@@ -282,9 +152,11 @@ public final class FindingsView extends ViewPart {
 		sf.setWeights(new int[] { 3, 1 });
 		bar.setSpacing(2);
 
-		f_mediator = new FindingsMediator(projectCombo, topSash, detailsItem,
-				detailsComp, logItem, logComp);
-		updateFindingsOverview.handleEvent(null);
+		f_mediator = new FindingsMediator(pages, noFindingsPage, findingsPage,
+				projectCombo, groupings, groupByCombo, filter, topSash,
+				detailsItem, detailsComp, logItem, logComp);
+
+		pages.showPage(noFindingsPage);
 	}
 
 	@Override
