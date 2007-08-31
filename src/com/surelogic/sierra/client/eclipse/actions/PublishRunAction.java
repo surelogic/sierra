30cb70a3e6d8ce6f1fa53e58a0bb5f834c1425e7
Tim Halloran <tim.halloran@surelogic.com>
2007-08-31 17:11:52 +0000

    Working
    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/sierra-client-eclipse@1342 d02ae147-ee12-4a35-9452-91c89e8b2592

---
+++ src/com/surelogic/sierra/client/eclipse/actions/PublishRunAction.java
@@ -0,0 +1,27 @@
+package com.surelogic.sierra.client.eclipse.actions;
+
+import org.eclipse.jface.action.IAction;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.IWorkbenchWindowActionDelegate;
+
+public final class PublishRunAction implements IWorkbenchWindowActionDelegate {
+
+	public void dispose() {
+		// Nothing to do
+	}
+
+	public void init(IWorkbenchWindow window) {
+		// Nothing to do
+
+	}
+
+	public void run(IAction action) {
+		// TODO Auto-generated method stub
+	}
+
+	public void selectionChanged(IAction action, ISelection selection) {
+		// Nothing to do
+	}
+
+}
--- src/com/surelogic/sierra/client/eclipse/perspectives/SierraExplorerPerspective.java
+++ src/com/surelogic/sierra/client/eclipse/perspectives/SierraExplorerPerspective.java
@@ -1,9 +1,11 @@
 package com.surelogic.sierra.client.eclipse.perspectives;
 
+import org.eclipse.ui.IFolderLayout;
 import org.eclipse.ui.IPageLayout;
 import org.eclipse.ui.IPerspectiveFactory;
 
 import com.surelogic.sierra.client.eclipse.views.FindingsView;
+import com.surelogic.sierra.client.eclipse.views.SierraServersView;
 
 /**
  * Defines the Sierra Explorer perspective within the workbench.
@@ -13,10 +15,12 @@ public final class SierraExplorerPerspective implements IPerspectiveFactory {
 	public void createInitialLayout(IPageLayout layout) {
 		final String editorArea = layout.getEditorArea();
 
-		layout.addView("org.eclipse.jdt.ui.PackageExplorer", IPageLayout.LEFT,
-				0.20f, editorArea);
-
-		layout.addView(FindingsView.class.getName(), IPageLayout.TOP, 0.80f,
+		layout.addView(FindingsView.class.getName(), IPageLayout.LEFT, 0.50f,
 				editorArea);
+
+		final IFolderLayout bottom = layout.createFolder("bottom",
+				IPageLayout.BOTTOM, 0.50f, editorArea);
+		bottom.addView(SierraServersView.class.getName());
+		bottom.addView("org.eclipse.jdt.ui.PackageExplorer");
 	}
 }
