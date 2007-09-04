be276c7108ed1a2de9d8e5a18b5865f9288fdfc1
Nathan Boy <nathan.boy@surelogic.com>
2007-09-04 19:01:17 +0000

    Added project deletion.
    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/sierra-jdbc@1370 d02ae147-ee12-4a35-9452-91c89e8b2592

--- src/com/surelogic/sierra/jdbc/project/ProjectManager.java
+++ src/com/surelogic/sierra/jdbc/project/ProjectManager.java
@@ -7,54 +7,90 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Collection;
 
-
+import com.surelogic.common.SLProgressMonitor;
 import com.surelogic.sierra.jdbc.project.ProjectRecordFactory;
 import com.surelogic.sierra.jdbc.record.ProjectRecord;
+import com.surelogic.sierra.jdbc.scan.ScanManager;
 
 public class ProjectManager {
 
 	@SuppressWarnings("unused")
 	private final Connection conn;
-	
-	private static final String FIND_ALL = "SELECT NAME FROM PROJECT";
-	private final PreparedStatement findAllprojectNames;
 
+	private static final String FIND_ALL = "SELECT NAME FROM PROJECT";
+
+	private final PreparedStatement findAllprojectNames;
+	private final PreparedStatement findProjectRuns;
 	private final ProjectRecordFactory projectFactory;
-	
+
 	private ProjectManager(Connection conn) throws SQLException {
 		this.conn = conn;
-		
+
 		projectFactory = ProjectRecordFactory.getInstance(conn);
-		
+
 		findAllprojectNames = conn.prepareStatement(FIND_ALL);
+		findProjectRuns = conn
+				.prepareStatement("SELECT R.UID FROM RUN R WHERE R.PROJECT_ID = ?");
 	}
 
 	public Collection<String> getAllProjectNames() throws SQLException {
 		ResultSet rs = findAllprojectNames.executeQuery();
 		Collection<String> projectNames = new ArrayList<String>();
-		while(rs.next()) {
+		while (rs.next()) {
 			projectNames.add(rs.getString(1));
 		}
 		return projectNames;
 	}
-	
+
 	public Long newProject(String name) throws SQLException {
 		ProjectRecord project = projectFactory.newProject();
 		project.setName(name);
-		
-		if(project.select()) {
+
+		if (project.select()) {
 			// XXX Throw error
 		}
-		
+
 		project.insert();
-		
+
 		return project.getId();
 	}
-	
-	public void clearProject(String name) throws SQLException {
-		//TODO
+
+	public void deleteProject(String name, SLProgressMonitor monitor)
+			throws SQLException {
+		ProjectRecord rec = projectFactory.newProject();
+		rec.setName(name);
+		if (!rec.select()) {
+			throw new IllegalArgumentException("No project with name " + name
+					+ " exists.");
+		}
+		ScanManager scanMan = ScanManager.getInstance(conn);
+		Collection<String> scans = getProjectScans(rec.getId());
+		if (monitor != null) {
+			monitor.beginTask("Deleting Project", scans.size() + 1);
+		}
+		for (String uid : scans) {
+			scanMan.deleteScan(uid);
+			if (monitor != null) {
+				if (monitor.isCanceled())
+					return;
+				monitor.worked(1);
+			}
+		}
+		rec.delete();
+		monitor.done();
 	}
-	
+
+	private Collection<String> getProjectScans(Long projectId)
+			throws SQLException {
+		Collection<String> runs = new ArrayList<String>();
+		findProjectRuns.setLong(1, projectId);
+		ResultSet set = findProjectRuns.executeQuery();
+		while (set.next()) {
+			runs.add(set.getString(1));
+		}
+		return runs;
+	}
+
 	public static ProjectManager getInstance(Connection conn)
 			throws SQLException {
 		return new ProjectManager(conn);
--- src/com/surelogic/sierra/jdbc/project/ProjectRecordFactory.java
+++ src/com/surelogic/sierra/jdbc/project/ProjectRecordFactory.java
@@ -7,28 +7,25 @@ import com.surelogic.sierra.jdbc.record.BaseMapper;
 import com.surelogic.sierra.jdbc.record.ProjectRecord;
 
 public class ProjectRecordFactory {
-	
+
 	private static final String PROJECT_SELECT = "SELECT ID FROM PROJECT WHERE NAME = ?";
 	private static final String PROJECT_INSERT = "INSERT INTO PROJECT (NAME,SETTINGS_REVISION) VALUES (?,0)";
-	
-	@SuppressWarnings("unused")
-	private final Connection conn;
+	private static final String PROJECT_DELETE = "DELETE FROM PROJECT WHERE ID = ?";
 
 	private final BaseMapper projectMapper;
 
 	private ProjectRecordFactory(Connection conn) throws SQLException {
-		this.conn = conn;
-
 		projectMapper = new BaseMapper(conn, PROJECT_INSERT, PROJECT_SELECT,
-				null);
+				PROJECT_DELETE);
 	}
 
-	public static ProjectRecordFactory getInstance(Connection conn) throws SQLException {
+	public static ProjectRecordFactory getInstance(Connection conn)
+			throws SQLException {
 		return new ProjectRecordFactory(conn);
 	}
-	
+
 	public ProjectRecord newProject() {
 		return new ProjectRecord(projectMapper);
 	}
-	
+
 }
