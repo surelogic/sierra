8ca854fdea1d7b6c15502d9f0223e55a919c5b5a
Nathan Boy <nathan.boy@surelogic.com>
2007-09-05 18:42:16 +0000

    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/sierra-tool@1397 d02ae147-ee12-4a35-9452-91c89e8b2592

--- src/com/surelogic/sierra/tool/message/MessageWarehouse.java
+++ src/com/surelogic/sierra/tool/message/MessageWarehouse.java
@@ -46,7 +46,7 @@ public class MessageWarehouse {
 			.getName());
 
 	private static final MessageWarehouse INSTANCE = new MessageWarehouse();
-	private static final int COUNT = 200;
+	private static final int COUNT = 10;
 	private final JAXBContext ctx;
 	private final Marshaller marshaller;
 	private final Unmarshaller unmarshaller;
