7c0b7bf99242b92587c80dc664e6085b9d3f7558
Tim Halloran <tim.halloran@surelogic.com>
2007-09-07 00:54:42 +0000

    Show error dialog if the delete fails (not just log it).
    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/sierra-client-eclipse@1431 d02ae147-ee12-4a35-9452-91c89e8b2592

--- src/com/surelogic/sierra/client/eclipse/jobs/DeleteProjectDataJob.java
+++ src/com/surelogic/sierra/client/eclipse/jobs/DeleteProjectDataJob.java
@@ -9,7 +9,10 @@ import java.util.logging.Level;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.jface.dialogs.ProgressMonitorDialog;
 import org.eclipse.jface.operation.IRunnableWithProgress;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.MessageBox;
 import org.eclipse.swt.widgets.Shell;
+import org.eclipse.ui.PlatformUI;
 
 import com.surelogic.common.SLProgressMonitor;
 import com.surelogic.common.eclipse.SLProgressMonitorWrapper;
@@ -37,12 +40,12 @@ public final class DeleteProjectDataJob {
 					slMonitor.beginTask("Deleting selected projects",
 							f_projectNames.size() * 4);
 
-					Connection conn;
+					boolean jobFailed = false;
 					try {
-						conn = Data.getConnection();
+						final Connection conn = Data.getConnection();
 
 						conn.setAutoCommit(false);
-						ProjectManager manager = ProjectManager
+						final ProjectManager manager = ProjectManager
 								.getInstance(conn);
 						try {
 							for (final String projectName : f_projectNames) {
@@ -50,20 +53,36 @@ public final class DeleteProjectDataJob {
 								conn.commit();
 							}
 						} catch (Exception e) {
-							final String msg = "Deletion of Sierra data about projects '"
-									+ f_projectNames + "' failed.";
+							final String msg = "Deletion of Sierra data about projects "
+									+ f_projectNames + " failed.";
 							SLLogger.getLogger().log(Level.SEVERE, msg, e);
+							jobFailed = true;
 						} finally {
 							conn.close();
 						}
-
 					} catch (SQLException e1) {
-						final String msg = "Deletion of Sierra data about projects '"
-								+ f_projectNames + "' failed.";
+						final String msg = "Deletion of Sierra data about projects "
+								+ f_projectNames + " failed.";
 						SLLogger.getLogger().log(Level.SEVERE, msg, e1);
+						jobFailed = true;
 					}
 					monitor.done();
 					DatabaseHub.getInstance().notifyProjectDeleted();
+					if (!jobFailed) {
+						PlatformUI.getWorkbench().getDisplay().asyncExec(
+								new Runnable() {
+									public void run() {
+										final MessageBox dialog = new MessageBox(
+												shell, SWT.ICON_ERROR | SWT.OK);
+										dialog.setText("Failure");
+										dialog
+												.setMessage("Deletion of Sierra data about projects "
+														+ f_projectNames
+														+ " failed.");
+										dialog.open();
+									}
+								});
+					}
 				}
 			});
 		} catch (Exception e) {
