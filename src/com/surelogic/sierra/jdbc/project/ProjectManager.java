fc467e873f897365f683750ec5ef462019cb8ded
Nathan Boy <nathan.boy@surelogic.com>
2007-09-10 18:44:13 +0000

    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/sierra-jdbc@1468 d02ae147-ee12-4a35-9452-91c89e8b2592

--- src/com/surelogic/sierra/jdbc/project/ProjectManager.java
+++ src/com/surelogic/sierra/jdbc/project/ProjectManager.java
@@ -146,39 +146,13 @@ public class ProjectManager {
 	public void deleteProject(String projectName, SLProgressMonitor monitor)
 			throws SQLException {
 		ProjectRecord rec = projectFactory.newProject();
-		
-		if(rec == null || projectName == null) {
-			System.out.println("**************************************");
-			return;
-		}
-		
 		rec.setName(projectName);
-		
 		if (rec.select()) {
 			if (monitor != null)
 				monitor.subTask("Deleting scans for project " + projectName);
 		
 			scanManager.deleteScans(getProjectScans(rec.getId()), monitor);
-
-			if (monitor != null)
-				monitor.subTask("Deleting matches for project " + projectName);
-
-			deleteMatches.setLong(1, rec.getId());
-			deleteMatches.executeUpdate();
-			if (monitor != null) {
-				if (monitor.isCanceled())
-					return;
-				monitor.worked(1);
-				monitor.subTask("Deleting findings for project " + projectName);
-			}
-			
-			deleteFindings.setLong(1, rec.getId());
-			deleteFindings.executeUpdate();
-			if (monitor != null) {
-				if (monitor.isCanceled())
-					return;
-				monitor.worked(1);
-			}
+			findingManager.deleteFindings(projectName, monitor);
 			rec.delete();
 		}
 	}
