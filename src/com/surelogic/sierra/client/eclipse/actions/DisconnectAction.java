f2316553ef710f41c59bd2f67cbcf58759d509fb
Tim Halloran <tim.halloran@surelogic.com>
2007-09-10 12:45:14 +0000

    Working
    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/sierra-client-eclipse@1454 d02ae147-ee12-4a35-9452-91c89e8b2592

--- src/com/surelogic/sierra/client/eclipse/actions/DisconnectAction.java
+++ src/com/surelogic/sierra/client/eclipse/actions/DisconnectAction.java
@@ -1,28 +1,13 @@
 package com.surelogic.sierra.client.eclipse.actions;
 
-import java.io.File;
 import java.util.List;
 
 import org.eclipse.jdt.core.IJavaProject;
-import org.eclipse.ui.PlatformUI;
-
-import com.surelogic.sierra.client.eclipse.dialogs.ServerLocationDialog;
-import com.surelogic.sierra.client.eclipse.model.SierraServerManager;
-import com.surelogic.sierra.client.eclipse.model.SierraServerModel;
 
 public final class DisconnectAction extends AbstractProjectSelectedMenuAction {
 
 	@Override
 	protected void run(List<IJavaProject> selectedProjects) {
-		SierraServerManager m = new SierraServerManager();
-		SierraServerModel s = new SierraServerModel(m, "Server 1");
-		SierraServerModel s2 = new SierraServerModel(m, "Server 2");
-		s.setHost("fluid.surelogic.com");
-		// TODO: Just for testing of the user password dialog
-		ServerLocationDialog d = new ServerLocationDialog(PlatformUI
-				.getWorkbench().getActiveWorkbenchWindow().getShell(), s, true);
-		d.open();
-		System.out.println(s);
-
+		// TODO: do something reasonable
 	}
 }
