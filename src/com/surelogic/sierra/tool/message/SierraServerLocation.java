f287b10516991b19df3d14c30c5c6be9510e634c
Tim Halloran <tim.halloran@surelogic.com>
2007-09-12 15:21:05 +0000

    Server needs a protocol
    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/sierra-tool@1541 d02ae147-ee12-4a35-9452-91c89e8b2592

--- src/com/surelogic/sierra/tool/message/SierraServerLocation.java
+++ src/com/surelogic/sierra/tool/message/SierraServerLocation.java
@@ -1,52 +1,93 @@
 package com.surelogic.sierra.tool.message;
 
 public class SierraServerLocation {
-	private final String host;
-	private final Integer port;
-	private final String user;
-	private final String pass;
 
-	public SierraServerLocation(String host, Integer port, String user,
-			String pass) {
-		this.host = host;
-		this.port = port;
-		this.user = user;
-		this.pass = pass;
+	private static final int DEFAULT_PORT = 8080;
+
+	private static final String UNLABELED_SERVER = "unlabeled server";
+
+	public SierraServerLocation(String host, boolean secure, int port,
+			String user, String password) {
+		this(UNLABELED_SERVER, host, secure, port, user, password);
+	}
+
+	public SierraServerLocation(String label, String host, boolean secure,
+			int port, String user, String pass) {
+		f_host = host;
+		f_secure = secure;
+		f_port = port;
+		f_user = user;
+		f_password = pass;
+		f_label = label;
 	}
 
 	public SierraServerLocation(String server) {
+		/*
+		 * TODO: fix this to set the protocol properly
+		 */
 		String[] strArr = server.split(":");
 		String strPort = strArr[1];
 		if (strPort != null) {
-			this.port = Integer.parseInt(strPort);
+			f_port = Integer.parseInt(strPort);
 		} else {
-			this.port = null;
+			f_port = DEFAULT_PORT;
 		}
-		this.host = strArr[0];
-		this.user = null;
-		this.pass = null;
-		// TODO
+		f_host = strArr[0];
+		f_secure = false;
+		f_user = null;
+		f_password = null;
+		f_label = UNLABELED_SERVER;
 	}
+
+	/**
+	 * A client label for this server location, or <tt>unknown</tt> if not a
+	 * client connection, e.g., in the Ant task.
+	 */
+	private final String f_label;
+
+	public String getLabel() {
+		return f_label;
+	}
+
+	private final boolean f_secure;
+
+	public boolean isSecure() {
+		return f_secure;
+	}
+
+	public String getProtocol() {
+		return f_secure ? "https" : "http";
+	}
+
+	private final String f_host;
 
 	public String getHost() {
-		return host;
+		return f_host;
 	}
 
-	public Integer getPort() {
-		return port;
+	private final int f_port;
+
+	public int getPort() {
+		return f_port;
 	}
+
+	private final String f_user;
 
 	public String getUser() {
-		return user;
+		return f_user;
 	}
 
+	private final String f_password;
+
 	public String getPass() {
-		return pass;
+		return f_password;
 	}
 
 	@Override
 	public String toString() {
 		final StringBuilder b = new StringBuilder();
+		b.append("'").append(getLabel()).append("' is ");
+		b.append(getProtocol()).append("://");
 		b.append(getHost()).append(":").append(getPort());
 		b.append("/user=\"").append(getUser()).append("\" ");
 		b.append(" password=\"").append(getPass()).append("\"");
