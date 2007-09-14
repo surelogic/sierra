25c01d829787015b064b57b47c3187f7fb16450c
Tanmay Sinha <tanmay.sinha@surelogic.com>
2007-09-14 20:06:00 +0000

    Updated hash generation
    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/sierra-tool@1625 d02ae147-ee12-4a35-9452-91c89e8b2592

--- src/com/surelogic/sierra/tool/analyzer/HashGenerator.java
+++ src/com/surelogic/sierra/tool/analyzer/HashGenerator.java
@@ -30,10 +30,6 @@ public class HashGenerator {
 
 	private int countFileAccess = 0;
 
-	// private static List<String> valueUp = new ArrayList<String>();
-	//
-	// private static List<String> valueDown = new ArrayList<String>();
-
 	private static long hashValue;
 
 	private static int previousLine = -1;
@@ -41,8 +37,6 @@ public class HashGenerator {
 	private static String previousFileName;
 
 	private static String currentFileName;
-
-	// private static BufferedReader in;
 
 	private static File currentFile;
 
@@ -66,14 +60,14 @@ public class HashGenerator {
 	public static void main(String args[]) {
 
 		HashGenerator hashGenerator = HashGenerator.getInstance();
-		System.out.println("\t" + hashGenerator.getHash("fibonacci.java", 1));
-		System.out.println("\t" + hashGenerator.getHash("fibonacci.java", 3));
-		System.out.println("\t" + hashGenerator.getHash("fibonacci.java", 3));
-		System.out.println("\t" + hashGenerator.getHash("fibonacci.java", 14));
-		//
-		// System.out.println("\t" + hashGenerator.getHash("Parsing.java", 3));
-		// System.out.println("\t" + hashGenerator.getHash("Parsing.java", 3));
-		// System.out.println("\t" + hashGenerator.getHash("Parsing.java", 7));
+		System.out
+				.println("\t" + hashGenerator.getHash("C:/fibonacci.java", 1));
+		System.out
+				.println("\t" + hashGenerator.getHash("C:/fibonacci.java", 5));
+		System.out
+				.println("\t" + hashGenerator.getHash("C:/fibonacci.java", 5));
+		System.out.println("\t"
+				+ hashGenerator.getHash("C:/fibonacci.java", 18));
 
 	}
 
@@ -85,13 +79,9 @@ public class HashGenerator {
 
 			if ((currentFile == null)
 					|| (!previousFileName.equals(currentFileName))) {
-
-				// System.out.println("File :" + currentFileName);
 				currentFile = new File(currentFileName);
 				previousFileName = currentFileName;
 				previousLine = -1;
-				// valueDown.clear();
-				// valueUp.clear();
 			}
 
 			if (previousLine == -1) {
@@ -105,23 +95,6 @@ public class HashGenerator {
 
 			BufferedReader in = new BufferedReader(new FileReader(currentFile));
 			String holder = in.readLine();
-			// while (holder != null) {
-			//
-			// String tempHolder[] = holder.split("\\s+");
-			// StringBuffer result = new StringBuffer();
-			//
-			// if (tempHolder.length > 0) {
-			// for (int i = 0; i < tempHolder.length; i++) {
-			// result.append(tempHolder[i]);
-			// }
-			// }
-			//
-			// holder = result.toString();
-			//
-			// valueUp.add(holder);
-			// holder = in.readLine();
-			// }
-
 			String valueUp = "";
 			String valueDown = "";
 
@@ -149,7 +122,7 @@ public class HashGenerator {
 								.length());
 					}
 				}
-				if ((!"LAST".equals(valueDown)) && (lineCounter > lineNumber)) {
+				if ((!"LAST".equals(valueDown)) && (lineCounter >= lineNumber)) {
 					if (valueDown.length() < 30) {
 						valueDown += holder;
 						if (valueDown.length() > 30) {
@@ -169,8 +142,8 @@ public class HashGenerator {
 				valueDown = LAST;
 			}
 
-			// System.out.println("\tValue up :" + valueUp);
-			// System.out.println("\tValue down :" + valueDown);
+			System.out.println("Value up :" + valueUp);
+			System.out.println("Value down :" + valueDown);
 			int hashUp = valueUp.hashCode();
 			int hashDown = valueDown.hashCode();
 
@@ -179,103 +152,16 @@ public class HashGenerator {
 			return hashValue;
 
 		} catch (FileNotFoundException e) {
-			log.log(Level.SEVERE, "The file " + fileName + " was not found."
-					+ e);
+			log
+					.log(Level.SEVERE, "The file " + fileName
+							+ " was not found.", e);
+			throw new RuntimeException("While generating hash :"
+					+ e.getMessage());
 		} catch (IOException e) {
-			log.log(Level.SEVERE, "I/O exception while hash generation." + e);
+			log.log(Level.SEVERE, "I/O exception while hash generation.", e);
+			throw new RuntimeException("While generating hash :"
+					+ e.getMessage());
 		}
-
-		return null;
-
-	}
-
-	@Deprecated
-	public Long getHash(File originalFile, int lineNumber) {
-
-		try {
-			BufferedReader in = new BufferedReader(new FileReader(currentFile));
-			String holder = in.readLine();
-			// while (holder != null) {
-			//
-			// String tempHolder[] = holder.split("\\s+");
-			// StringBuffer result = new StringBuffer();
-			//
-			// if (tempHolder.length > 0) {
-			// for (int i = 0; i < tempHolder.length; i++) {
-			// result.append(tempHolder[i]);
-			// }
-			// }
-			//
-			// holder = result.toString();
-			//
-			// valueUp.add(holder);
-			// holder = in.readLine();
-			// }
-
-			String valueUp = "";
-			String valueDown = "";
-
-			int lineCounter = 0;
-			while (holder != null) {
-
-				lineCounter++;
-
-				String tempHolder[] = holder.split("\\s+");
-				StringBuffer result = new StringBuffer();
-
-				if (tempHolder.length > 0) {
-					for (int i = 0; i < tempHolder.length; i++) {
-						result.append(tempHolder[i]);
-					}
-				}
-
-				holder = result.toString();
-
-				if ((!"FIRST".equals(valueUp)) && (lineCounter < lineNumber)) {
-					valueUp += holder;
-					if (valueUp.length() > 30) {
-						int extraChars = valueUp.length() - 30;
-						valueUp = valueUp.substring(extraChars, valueUp
-								.length());
-					}
-				}
-				if ((!"LAST".equals(valueDown)) && (lineCounter > lineNumber)) {
-					if (valueDown.length() < 30) {
-						valueDown += holder;
-						if (valueDown.length() > 30) {
-							valueDown = valueDown.substring(0, 30);
-						}
-					}
-				}
-
-				holder = in.readLine();
-			}
-
-			if (valueUp.equals("")) {
-				valueUp = FIRST;
-			}
-
-			if (valueDown.equals("")) {
-				valueDown = LAST;
-			}
-
-			// System.out.println("\tValue up :" + valueUp);
-			// System.out.println("\tValue down :" + valueDown);
-			int hashUp = valueUp.hashCode();
-			int hashDown = valueDown.hashCode();
-
-			hashValue = (((long) hashDown) << 32) + hashUp;
-			return hashValue;
-
-		} catch (FileNotFoundException e) {
-			log.log(Level.SEVERE, "The file " + originalFile.getAbsolutePath()
-					+ " was not found." + e);
-		} catch (IOException e) {
-			log.log(Level.SEVERE, "I/O exception while hash generation." + e);
-		}
-
-		return null;
-
 	}
 
 	public void generateHash(Map<String, Map<Integer, Long>> hashHolder) {
