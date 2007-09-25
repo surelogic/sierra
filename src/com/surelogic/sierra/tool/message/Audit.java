8c855cf980dec16a07924df582682daba65bd6f1
Nathan Boy <nathan.boy@surelogic.com>
2007-09-25 03:00:01 +0000

    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/sierra-tool@1717 d02ae147-ee12-4a35-9452-91c89e8b2592

--- src/com/surelogic/sierra/tool/message/Audit.java
+++ src/com/surelogic/sierra/tool/message/Audit.java
@@ -81,48 +81,4 @@ public class Audit {
 				+ ")";
 	}
 
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((event == null) ? 0 : event.hashCode());
-		result = prime * result
-				+ ((timestamp == null) ? 0 : timestamp.hashCode());
-		result = prime * result + ((user == null) ? 0 : user.hashCode());
-		result = prime * result + ((value == null) ? 0 : value.hashCode());
-		return result;
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		final Audit other = (Audit) obj;
-		if (event == null) {
-			if (other.event != null)
-				return false;
-		} else if (!event.equals(other.event))
-			return false;
-		if (timestamp == null) {
-			if (other.timestamp != null)
-				return false;
-		} else if (!timestamp.equals(other.timestamp))
-			return false;
-		if (user == null) {
-			if (other.user != null)
-				return false;
-		} else if (!user.equals(other.user))
-			return false;
-		if (value == null) {
-			if (other.value != null)
-				return false;
-		} else if (!value.equals(other.value))
-			return false;
-		return true;
-	}
-
 }
--- test/com/surelogic/sierra/tool/message/SierraServiceTest.java
+++ test/com/surelogic/sierra/tool/message/SierraServiceTest.java
@@ -75,7 +75,7 @@ public class SierraServiceTest {
 		merges.add(merge);
 		List<Match> matches = new ArrayList<Match>();
 		merge.setMatch(matches);
-		matches.add(new Match("package", "class", 34L, "ShortVariable"));
+		matches.add(new Match("package", "class", 34L, "SW_SWING_METHODS_INVOKED_IN_SWING_THREAD"));
 		try {
 			MergeAuditTrailResponse mergeRes = service
 					.mergeAuditTrails(mergeReq);
@@ -84,7 +84,7 @@ public class SierraServiceTest {
 			String newTrail = mergeRes.getTrail().get(0);
 			assertNotNull(trail);
 			assertEquals(trail, newTrail);
-			matches.add(new Match("package2", "class2", 35L, "ShortVariable"));
+			matches.add(new Match("package2", "class2", 35L, "SW_SWING_METHODS_INVOKED_IN_SWING_THREAD"));
 			mergeRes = service.mergeAuditTrails(mergeReq);
 			newTrail = mergeRes.getTrail().get(0);
 			assertEquals(trail, newTrail);
@@ -97,7 +97,7 @@ public class SierraServiceTest {
 			auditTrail.setFinding(trail);
 			List<Audit> audits = new ArrayList<Audit>();
 			auditTrail.setAudits(audits);
-			Audit audit = new Audit(new Date(), AuditEvent.COMMENT,
+			Audit audit = new Audit(null, AuditEvent.COMMENT,
 					"Some comment");
 			audits.add(audit);
 			service.commitAuditTrails(auditTrails);
@@ -107,8 +107,10 @@ public class SierraServiceTest {
 			request.setRevision(-1L);
 			AuditTrailResponse response = service.getAuditTrails(request);
 			AuditTrailUpdate update = response.getUpdate().get(0);
-			assertEquals(audit, update.getAudit().get(
-					update.getAudit().size() - 1));
+			assertEquals(audit.getEvent(), update.getAudit().get(
+					update.getAudit().size() - 1).getEvent());
+			assertEquals(audit.getValue(), update.getAudit().get(
+					update.getAudit().size() - 1).getValue());
 			assertEquals(2, update.getMatch().size());
 		} catch (ServerMismatchException e) {
 			fail();
