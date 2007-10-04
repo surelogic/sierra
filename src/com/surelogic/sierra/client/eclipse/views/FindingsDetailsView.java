e52d5879163a8d09aef9a3156193082b3913ed07
Tanmay Sinha <tanmay.sinha@surelogic.com>
2007-10-04 19:30:32 +0000

    Sample details view
    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/sierra-client-eclipse@1886 d02ae147-ee12-4a35-9452-91c89e8b2592

--- src/com/surelogic/sierra/client/eclipse/views/FindingsDetailsView.java
+++ src/com/surelogic/sierra/client/eclipse/views/FindingsDetailsView.java
@@ -1,5 +1,6 @@
 package com.surelogic.sierra.client.eclipse.views;
 
+import org.eclipse.jdt.ui.ISharedImages;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
@@ -9,23 +10,21 @@ import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Group;
 import org.eclipse.swt.widgets.Label;
-import org.eclipse.swt.widgets.TabFolder;
-import org.eclipse.swt.widgets.TabItem;
+import org.eclipse.swt.widgets.List;
 import org.eclipse.swt.widgets.Text;
+import org.eclipse.swt.widgets.Tree;
+import org.eclipse.swt.widgets.TreeItem;
 import org.eclipse.ui.part.ViewPart;
 
-public class FindingsDetailsView extends ViewPart {
+import com.surelogic.common.eclipse.SLImages;
 
-	private static final String TEXT_SUMMARY = "Summary :";
-	private static final String TEXT_CLASS = "Class :";
-	private static final String TEXT_PACKAGE = "Package :";
-	private static final String TEXT_LINENO = "Line Number :";
+public class FindingsDetailsView extends ViewPart {
 
 	@Override
 	public void createPartControl(Composite parent) {
 
 		final Composite composite = new Composite(parent, SWT.NONE);
-		GridLayout layout = new GridLayout(2, false);
+		GridLayout layout = new GridLayout(3, false);
 		GridData layoutData = new GridData(SWT.FILL, SWT.FILL, true, true);
 		composite.setLayoutData(layoutData);
 		composite.setLayout(layout);
@@ -36,64 +35,68 @@ public class FindingsDetailsView extends ViewPart {
 		infoComposite.setLayoutData(layoutData);
 		infoComposite.setLayout(layout);
 
-		// TOP LEFT BLOCK
-		final Group overviewGroup = new Group(infoComposite, SWT.NONE);
-		layout = new GridLayout(1, false);
+		createOverviewBlock(infoComposite);
+		createAuditBlock(infoComposite);
+		createArtifactsBlock(composite);
+
+	}
+
+	private void createArtifactsBlock(Composite parent) {
+		// RIGHT BLOCK
+
+		final Group staticInfoGroup = new Group(parent, SWT.NONE);
+		GridLayout layout = new GridLayout(1, false);
+		GridData layoutData = new GridData(SWT.FILL, SWT.FILL, true, true);
+		staticInfoGroup.setText("Artifacts");
+
+		staticInfoGroup.setLayout(layout);
+		staticInfoGroup.setLayoutData(layoutData);
+
+		final Tree artifactsTree = new Tree(staticInfoGroup, SWT.V_SCROLL
+				| SWT.BORDER);
 		layoutData = new GridData(SWT.FILL, SWT.FILL, true, true);
-		overviewGroup.setLayoutData(layoutData);
-		overviewGroup.setLayout(layout);
-		overviewGroup.setText("Overview");
+		artifactsTree.setLayoutData(layoutData);
+		artifactsTree.setLinesVisible(true);
 
-		// Summary information
+		final TreeItem fbTreeItem = new TreeItem(artifactsTree, SWT.NONE);
+		fbTreeItem.setText("FindBugs\u2122");
 
-		final Composite locationInfoComposite = new Composite(overviewGroup,
+		final TreeItem fbArtifact = new TreeItem(fbTreeItem, SWT.NONE);
+		fbArtifact
+				.setText("sample.fb.Testy.main(String[]) uses reflection to check "
+						+ "for the presence of an "
+						+ "annotation that has default retention");
+
+		final TreeItem pmdTreeItem = new TreeItem(artifactsTree, SWT.NONE);
+		pmdTreeItem.setText("PMD\u2122");
+
+		final TreeItem pmdArtifact = new TreeItem(pmdTreeItem, SWT.NONE);
+		pmdArtifact.setText("All methods are static.  Consider using Singleton"
+				+ " instead.  Alternatively, you could add a private "
+				+ "constructor or make the class abstract to silence "
+				+ "this warning.");
+
+	}
+
+	private void createAuditBlock(Composite parent) {
+		// BOTTOM LEFT BLOCK
+
+		final Group auditGroup = new Group(parent, SWT.NONE);
+		GridLayout layout = new GridLayout(2, false);
+		GridData layoutData = new GridData(SWT.FILL, SWT.FILL, true, true);
+		auditGroup.setLayoutData(layoutData);
+		auditGroup.setLayout(layout);
+		auditGroup.setText("Audit");
+
+		final Composite radioButtonsComposite = new Composite(auditGroup,
 				SWT.NONE);
-		layout = new GridLayout(2, false);
-		layoutData = new GridData(SWT.TOP, SWT.LEFT, true, false);
-		locationInfoComposite.setLayout(layout);
-		locationInfoComposite.setLayoutData(layoutData);
+		layout = new GridLayout(1, false);
+		layoutData = new GridData(SWT.TOP, SWT.LEFT, false, false);
+		radioButtonsComposite.setLayout(layout);
+		radioButtonsComposite.setLayoutData(layoutData);
 
-		final Composite sourceLocationInfoComposite = new Composite(
-				locationInfoComposite, SWT.NONE);
-		layout = new GridLayout(2, false);
-		layoutData = new GridData(SWT.TOP, SWT.LEFT, true, false);
-		sourceLocationInfoComposite.setLayoutData(layoutData);
-		sourceLocationInfoComposite.setLayout(layout);
-
-		final Label packageLabel = new Label(sourceLocationInfoComposite,
-				SWT.NONE);
-		packageLabel.setText(TEXT_PACKAGE);
-
-		final Label packageNameText = new Label(sourceLocationInfoComposite,
-				SWT.NONE);
-		layoutData = new GridData(SWT.FILL, SWT.FILL, true, false);
-		layoutData.widthHint = 400;
-		packageNameText.setLayoutData(layoutData);
-		packageNameText.setText("com.surelogic.adhoc.views");
-
-		final Label classLabel = new Label(sourceLocationInfoComposite,
-				SWT.NONE);
-		classLabel.setText(TEXT_CLASS);
-
-		final Label classNameText = new Label(sourceLocationInfoComposite,
-				SWT.NONE);
-		layoutData = new GridData(SWT.FILL, SWT.FILL, true, false);
-		layoutData.widthHint = 400;
-		classNameText.setLayoutData(layoutData);
-		classNameText.setText("AdHocQueryResultsViewMediator");
-
-		final Label lineLabel = new Label(sourceLocationInfoComposite, SWT.NONE);
-		lineLabel.setText(TEXT_LINENO);
-
-		final Label lineNoText = new Label(sourceLocationInfoComposite,
-				SWT.NONE);
-		layoutData = new GridData(SWT.FILL, SWT.FILL, true, false);
-		layoutData.widthHint = 20;
-		lineNoText.setLayoutData(layoutData);
-		lineNoText.setText("2");
-
-		final Button quickAudit = new Button(locationInfoComposite, SWT.RADIO);
-		layoutData = new GridData(SWT.CENTER, SWT.CENTER, false, false);
+		final Button quickAudit = new Button(radioButtonsComposite, SWT.RADIO);
+		layoutData = new GridData(SWT.TOP, SWT.LEFT, false, false);
 		quickAudit.setLayoutData(layoutData);
 		quickAudit.setText("Quick Audit");
 		quickAudit.setToolTipText("Select to audit a finding.");
@@ -107,35 +110,7 @@ public class FindingsDetailsView extends ViewPart {
 
 		});
 
-		final Composite summaryComposite = new Composite(overviewGroup,
-				SWT.NONE);
-		layout = new GridLayout(1, false);
-		layoutData = new GridData(SWT.FILL, SWT.FILL, true, true);
-		summaryComposite.setLayoutData(layoutData);
-		summaryComposite.setLayout(layout);
-
-		// Summary text : can be changed
-		final Label summaryLabel = new Label(summaryComposite, SWT.NONE);
-		summaryLabel.setText(TEXT_SUMMARY);
-
-		final Text summaryText = new Text(summaryComposite, SWT.MULTI
-				| SWT.BORDER);
-		layoutData = new GridData(GridData.FILL, GridData.FILL, true, true, 1,
-				3);
-		layoutData.widthHint = 300;
-		summaryText.setLayoutData(layoutData);
-		summaryText.setText("Sample summary");
-
-		// BOTTOM LEFT BLOCK
-
-		final Group auditGroup = new Group(infoComposite, SWT.NONE);
-		layout = new GridLayout(3, false);
-		layoutData = new GridData(SWT.FILL, SWT.FILL, true, true);
-		auditGroup.setLayoutData(layoutData);
-		auditGroup.setLayout(layout);
-		auditGroup.setText("Audit");
-
-		final Group importanceGroup = new Group(auditGroup, SWT.NONE);
+		final Group importanceGroup = new Group(radioButtonsComposite, SWT.NONE);
 		layout = new GridLayout(1, false);
 		importanceGroup.setLayout(layout);
 		layoutData = new GridData(SWT.TOP, SWT.LEFT, false, true);
@@ -157,26 +132,32 @@ public class FindingsDetailsView extends ViewPart {
 
 		// Comment text
 
-		final Composite commentComposite = new Composite(auditGroup, SWT.NONE);
+		final Composite completeCommentsComposite = new Composite(auditGroup,
+				SWT.NONE);
 		layout = new GridLayout(1, false);
 		layoutData = new GridData(SWT.FILL, SWT.FILL, true, true);
+		completeCommentsComposite.setLayout(layout);
+		completeCommentsComposite.setLayoutData(layoutData);
+
+		final Composite commentComposite = new Composite(
+				completeCommentsComposite, SWT.NONE);
+		layout = new GridLayout(3, false);
+		layoutData = new GridData(SWT.FILL, SWT.FILL, true, false);
 		commentComposite.setLayoutData(layoutData);
 		commentComposite.setLayout(layout);
 
 		final Label commentLabel = new Label(commentComposite, SWT.NONE);
 		commentLabel.setText("Comment :");
 
-		final Text commentText = new Text(commentComposite, SWT.MULTI
-				| SWT.BORDER | SWT.H_SCROLL | SWT.V_SCROLL);
-		layoutData = new GridData(GridData.FILL, GridData.FILL, true, true, 1,
-				3);
+		final Text commentText = new Text(commentComposite, SWT.BORDER);
+		layoutData = new GridData(SWT.FILL, SWT.FILL, true, true);
 		layoutData.widthHint = 300;
 		commentText.setLayoutData(layoutData);
 		commentText.setText("Some comment");
 
 		// Add Comment button
 
-		final Button commentButton = new Button(auditGroup, SWT.PUSH);
+		final Button commentButton = new Button(commentComposite, SWT.PUSH);
 		commentButton.setText("Add");
 		commentButton.addSelectionListener(new SelectionAdapter() {
 
@@ -187,25 +168,69 @@ public class FindingsDetailsView extends ViewPart {
 
 		});
 
-		// RIGHT BLOCK
-
-		final Composite staticInfoComposite = new Composite(composite, SWT.NONE);
-		layout = new GridLayout(1, false);
+		final List logList = new List(completeCommentsComposite, SWT.BORDER
+				| SWT.V_SCROLL);
 		layoutData = new GridData(SWT.FILL, SWT.FILL, true, true);
+		logList.setLayoutData(layoutData);
 
-		staticInfoComposite.setLayout(layout);
-		staticInfoComposite.setLayoutData(layoutData);
+		// Sample
+		logList
+				.add("Jane Doe (Comment, Oct 2, 2007 11:32 AM) : Fixed on John Doe's suggestion");
 
-		final TabFolder tabFolder = new TabFolder(staticInfoComposite, SWT.NONE);
-		layoutData = new GridData(SWT.FILL, SWT.FILL, true, true);
-		tabFolder.setLayoutData(layoutData);
+		logList
+				.add("John Doe (Comment, Oct 1, 2007 10:32 AM) : Do this to fix this bug");
+		logList
+				.add("John Doe (Audit, Oct 1, 2007 10:31 AM) : Audited the finding");
 
-		final TabItem logTab = new TabItem(tabFolder, SWT.NONE);
-		logTab.setText("Logs");
+	}
 
-		final TabItem artifactsTab = new TabItem(tabFolder, SWT.NONE);
-		artifactsTab.setText("Artifacts");
+	private void createOverviewBlock(Composite parent) {
+		// TOP LEFT BLOCK
+		final Composite overviewComposite = new Composite(parent, SWT.NONE);
+		GridLayout layout = new GridLayout(2, false);
+		GridData layoutData = new GridData(SWT.FILL, SWT.FILL, true, false);
+		overviewComposite.setLayoutData(layoutData);
+		overviewComposite.setLayout(layout);
 
+		// Summary text : can be changed
+		final Label summaryLabel = new Label(overviewComposite, SWT.NONE);
+		layoutData = new GridData(SWT.TOP, SWT.LEFT, false, false);
+		summaryLabel.setLayoutData(layoutData);
+		summaryLabel.setImage(SLImages.getImage(SLImages.IMG_SIERRA_LOGO));
+
+		final Text summaryText = new Text(overviewComposite, SWT.BORDER);
+		layoutData = new GridData(SWT.FILL, SWT.FILL, true, false);
+		layoutData.widthHint = 300;
+		summaryText.setLayoutData(layoutData);
+		summaryText.setText("Sample summary");
+
+		// Summary information
+
+		// final Composite locationInfoComposite = new Composite(
+		// overviewComposite, SWT.NONE);
+		// layout = new GridLayout(2, false);
+		// layoutData = new GridData(SWT.TOP, SWT.LEFT, true, false);
+		// locationInfoComposite.setLayoutData(layoutData);
+		// locationInfoComposite.setLayout(layout);
+
+		final Label packageLabel = new Label(overviewComposite, SWT.NONE);
+		packageLabel.setImage(SLImages
+				.getJDTImage(ISharedImages.IMG_OBJS_PACKAGE));
+
+		final Label packageNameText = new Label(overviewComposite, SWT.NONE);
+		layoutData = new GridData(SWT.FILL, SWT.FILL, true, false);
+		layoutData.widthHint = 300;
+		packageNameText.setLayoutData(layoutData);
+		packageNameText.setText("com.surelogic.adhoc.views");
+
+		final Label classLabel = new Label(overviewComposite, SWT.NONE);
+		classLabel.setImage(SLImages.getJDTImage(ISharedImages.IMG_OBJS_CLASS));
+
+		final Label classNameText = new Label(overviewComposite, SWT.NONE);
+		layoutData = new GridData(SWT.FILL, SWT.FILL, true, false);
+		layoutData.widthHint = 300;
+		classNameText.setLayoutData(layoutData);
+		classNameText.setText("AdHocQueryResultsViewMediator " + "(Line 2)");
 	}
 
 	@Override
