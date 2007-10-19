755d266ffbb49b5aeb7b905d7dc15027a031d96b
Tim Halloran <tim.halloran@surelogic.com>
2007-10-19 14:09:23 +0000

    Working
    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/sierra-client-eclipse@2227 d02ae147-ee12-4a35-9452-91c89e8b2592

--- src/com/surelogic/sierra/client/eclipse/model/selection/FilterProject.java
+++ src/com/surelogic/sierra/client/eclipse/model/selection/FilterProject.java
@@ -1,5 +1,10 @@
 package com.surelogic.sierra.client.eclipse.model.selection;
 
+import java.util.Collections;
+import java.util.List;
+
+import com.surelogic.sierra.client.eclipse.model.Projects;
+
 public final class FilterProject extends Filter {
 
 	public static final ISelectionFilterFactory FACTORY = new AbstractFilterFactory() {
@@ -24,5 +29,16 @@ public final class FilterProject extends Filter {
 	@Override
 	protected String getColumnName() {
 		return "PROJECT";
+	}
+
+	@Override
+	protected void deriveAllValues() {
+		final List<String> projectNames = Projects.getInstance()
+				.getProjectNames();
+		Collections.sort(projectNames);
+		synchronized (this) {
+			f_allValues.clear();
+			f_allValues.addAll(projectNames);
+		}
 	}
 }
