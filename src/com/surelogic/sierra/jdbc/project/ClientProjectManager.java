eab2735f9457f0d587c5f7e0e1f30aa752d38e8f
Nathan Boy <nathan.boy@surelogic.com>
2007-11-20 16:09:46 +0000

    Added axis web client.
    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/sierra-jdbc@3498 d02ae147-ee12-4a35-9452-91c89e8b2592

--- src/com/surelogic/sierra/jdbc/project/ClientProjectManager.java
+++ src/com/surelogic/sierra/jdbc/project/ClientProjectManager.java
@@ -24,8 +24,7 @@ import com.surelogic.sierra.tool.message.SettingsReply;
 import com.surelogic.sierra.tool.message.SettingsRequest;
 import com.surelogic.sierra.tool.message.SierraServerLocation;
 import com.surelogic.sierra.tool.message.SierraService;
-import com.surelogic.sierra.tool.message.SierraServiceClient;
-
+import com.surelogic.sierra.tool.message.axis.SierraServiceClient;
 public class ClientProjectManager extends ProjectManager {
 
 	private final ClientFindingManager findingManager;
@@ -52,7 +51,7 @@ public class ClientProjectManager extends ProjectManager {
 		 * if we have any updates to settings.
 		 */
 		SierraService service = new SierraServiceClient(server)
-				.getSierraServicePort();
+				;
 
 		// Look up project. If it doesn't exist, create it and relate it to the
 		// server.
