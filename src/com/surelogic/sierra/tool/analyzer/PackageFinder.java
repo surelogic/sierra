c66b2c6430ceb165d161de8f02cd2c0cfa47aee3
Ethan Urie <ethan.urie@surelogic.com>
2007-11-27 22:16:53 +0000

    rewrote to remove eclipse dependencies
    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/sierra-tool@3603 d02ae147-ee12-4a35-9452-91c89e8b2592

--- src/com/surelogic/sierra/tool/analyzer/PackageFinder.java
+++ src/com/surelogic/sierra/tool/analyzer/PackageFinder.java
@@ -1,67 +1,98 @@
+/**
+ * This is a simple class that finds what package a java file is part of given a File object.
+ * This also serves as a test class for the unit test
+ * 
+ * Testing:
+ * package com.wrong.company.name.package;
+ package edu.wrong.package.name;
+ package
+ org.
+ wrong.
+ company;
+ */
 package com.surelogic.sierra.tool.analyzer;
 
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
-import org.eclipse.jdt.core.dom.AST;
-import org.eclipse.jdt.core.dom.ASTParser;
-import org.eclipse.jdt.core.dom.ASTVisitor;
-import org.eclipse.jdt.core.dom.CompilationUnit;
-import org.eclipse.jdt.core.dom.PackageDeclaration;
+import com.surelogic.sierra.tool.SierraToolConstants;
 
 public class PackageFinder {
 
+	private static final String PACKAGE = "package";
 	private static final PackageFinder INSTANCE = new PackageFinder();
 
 	public static PackageFinder getInstance() {
 		return INSTANCE;
 	}
 
-	public String getPackage(File target) {
-
+	public String getPackage(final File target) {
+		BufferedReader in = null;
+		String pkg = null;
+		
 		try {
-			BufferedReader in = new BufferedReader(new FileReader(target));
+			in = new BufferedReader(new FileReader(target));
+			
+			// Use to double-check the result against the filesystem location
+			// Convert the path
+			final String stringToReplace = Matcher.quoteReplacement(File.separator);
+			final String pkgPath = target.getCanonicalPath().replace(stringToReplace, ".");
+			System.out.println("Package path: " + pkgPath);
+			
 
-			StringBuffer buffer = new StringBuffer();
+			final StringBuffer buffer = new StringBuffer();
 			String line = null;
+			//Read in the file...simple...though not elegant
 			while (null != (line = in.readLine())) {
-				buffer.append("\t" + line);
+				buffer.append(line);
 				buffer.append("\n");
 			}
-			in.close();
 
-			ASTParser parser = ASTParser.newParser(AST.JLS3);
-			parser.setKind(ASTParser.K_COMPILATION_UNIT);
-			final String text = buffer.toString();
-			parser.setSource(text.toCharArray());
-			final CompilationUnit node = (CompilationUnit) parser
-					.createAST(null);
+			// Regex: \\s == whitespace (including newline) \\w == word character
+			Pattern pattern = Pattern.compile(
+					"^\\s*package\\s*((\\w+\\s*?\\.\\s*?)*\\w+)\\s*?;",
+					Pattern.MULTILINE | Pattern.DOTALL);
 
-			PackageVisitor visitor = new PackageVisitor();
-			node.accept(visitor);
-			return visitor.getPackageName();
+			final Matcher matcher = pattern.matcher(buffer.toString());
+			int index = 0;
+			while (matcher.find(index)) {
+				System.out.println(matcher.group(1));
+				pkg = matcher.group(1);
+				pkg = pkg.replaceAll("\\s", ""); // get rid of any newlines
+													// or whatnot
+				System.out.println("Package: " + pkg);
+				//If the package we found matches the path, we're done, otherwise continue
+    			if (pkgPath.contains(pkg)) {
+    				break;
+    			}
+    			else{
+    				index = matcher.end();
+    			}
+			}
+
+			if (pkg == null) {
+				// Set to the default package
+				pkg = SierraToolConstants.DEFAULT_PACKAGE;
+				System.out.println("Default");
+			}
+
 		} catch (IOException e) {
 			// Handle exception
+			e.printStackTrace();
+		} finally {
+			if (in != null) {
+				try {
+					in.close();
+				} catch (IOException e) {
+					// can't do anything about this
+				}
+			}
 		}
 
-		return null;
-
+		return pkg;
 	}
-
-	static class PackageVisitor extends ASTVisitor {
-		private static String packageName = "Default Package";
-
-		@Override
-		public boolean visit(PackageDeclaration node) {
-			packageName = node.getName().toString();
-			return super.visit(node);
-		}
-
-		public String getPackageName() {
-			return packageName;
-		}
-	}
-
 }
