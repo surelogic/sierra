380cefd9eb9bb98ed203c677cd706ba2e4dd8a7a
Ethan Urie <ethan.urie@surelogic.com>
2007-11-30 15:11:51 +0000

    rewrote the command line
    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/sierra-tool@3686 d02ae147-ee12-4a35-9452-91c89e8b2592

--- src/com/surelogic/sierra/tool/ant/Tools.java
+++ src/com/surelogic/sierra/tool/ant/Tools.java
@@ -388,7 +388,8 @@ public class Tools {
 			String[] paths = analysis.getClasspath().list();
 			//Find the 
 			int index = -1;
-			Pattern pattern = Pattern.compile(Pattern.quote("(.*)" + File.separator + "sierra-tool-aoi.jar.*"));
+			String separator = Pattern.quote(File.separator);
+			Pattern pattern = Pattern.compile( separator + "? (.+" + separator + ")*sierra-tool-aoi.jar.*");
 			antProject.log( "Pattern: " + pattern.toString(), org.apache.tools.ant.Project.MSG_DEBUG);
 			Matcher matcher = null;
 			
