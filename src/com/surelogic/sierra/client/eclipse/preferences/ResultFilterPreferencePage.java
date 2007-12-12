25ccffcd58bd64ae3e917125da0b714b75420a3a
Tim Halloran <tim.halloran@surelogic.com>
2007-12-12 23:11:49 +0000

    Working on filter on finding types
    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/sierra-client-eclipse@3868 d02ae147-ee12-4a35-9452-91c89e8b2592

--- src/com/surelogic/sierra/client/eclipse/preferences/ResultFilterPreferencePage.java
+++ src/com/surelogic/sierra/client/eclipse/preferences/ResultFilterPreferencePage.java
@@ -16,12 +16,16 @@ import org.eclipse.jface.preference.PreferencePage;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.SWTError;
 import org.eclipse.swt.browser.Browser;
+import org.eclipse.swt.graphics.RGB;
 import org.eclipse.swt.layout.FillLayout;
 import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Event;
 import org.eclipse.swt.widgets.Group;
+import org.eclipse.swt.widgets.Listener;
 import org.eclipse.swt.widgets.Tree;
 import org.eclipse.swt.widgets.TreeColumn;
 import org.eclipse.swt.widgets.TreeItem;
@@ -29,10 +33,12 @@ import org.eclipse.ui.IWorkbench;
 import org.eclipse.ui.IWorkbenchPreferencePage;
 import org.eclipse.ui.PlatformUI;
 
+import com.surelogic.common.eclipse.HTMLPrinter;
 import com.surelogic.common.eclipse.job.DatabaseJob;
 import com.surelogic.common.eclipse.logging.SLStatus;
 import com.surelogic.sierra.client.eclipse.Activator;
 import com.surelogic.sierra.client.eclipse.Data;
+import com.surelogic.sierra.client.eclipse.StyleSheetHelper;
 
 public class ResultFilterPreferencePage extends PreferencePage implements
 		IWorkbenchPreferencePage {
@@ -78,12 +84,11 @@ public class ResultFilterPreferencePage extends PreferencePage implements
 	private Composite f_panel = null;
 	private Tree f_findingTypes = null;
 	private Browser f_detailsText = null;
-	
 
 	@Override
 	protected Control createContents(Composite parent) {
 		f_panel = new Composite(parent, SWT.NONE);
-		f_panel.setLayout(new FillLayout());
+		f_panel.setLayout(new GridLayout());
 
 		GridData layoutData;
 		f_findingTypes = new Tree(f_panel, SWT.CHECK | SWT.FULL_SELECTION);
@@ -91,15 +96,43 @@ public class ResultFilterPreferencePage extends PreferencePage implements
 		f_findingTypes.setLinesVisible(true);
 		layoutData = new GridData(SWT.FILL, SWT.FILL, true, true);
 		f_findingTypes.setLayoutData(layoutData);
+		f_findingTypes.addListener(SWT.Selection, new Listener() {
+			public void handleEvent(Event event) {
+				boolean clearHTMLDescription = true;
+				TreeItem[] items = f_findingTypes.getSelection();
+				if (items.length > 0) {
+					final Object rawData = items[0].getData();
+					if (rawData instanceof Long) {
+						final Long findingTypeId = (Long) rawData;
+						clearHTMLDescription = false;
+						Job job = new DatabaseJob(
+								"Querying Sierra Artifact Type Description") {
+							@Override
+							protected IStatus run(IProgressMonitor monitor) {
+								monitor
+										.beginTask(
+												"Querying Sierra Artifact Type Description",
+												IProgressMonitor.UNKNOWN);
+								return queryFindingTypeHTMLDescriptionOf(findingTypeId);
+							}
+						};
+						job.schedule();
+					}
+				}
+				if (clearHTMLDescription)
+					clearHTMLDescription();
+			}
+		});
 
 		final TreeColumn treeColumn = new TreeColumn(f_findingTypes, SWT.NONE);
 		treeColumn.setText("Category/Finding Type");
 		treeColumn.setWidth(300);
 
-		final TreeColumn descriptionColumn = new TreeColumn(f_findingTypes, SWT.NONE);
+		final TreeColumn descriptionColumn = new TreeColumn(f_findingTypes,
+				SWT.NONE);
 		descriptionColumn.setText("Description");
 		descriptionColumn.setWidth(400);
-		
+
 		final Group description = new Group(f_panel, SWT.NONE);
 		description.setText("Finding Type Description");
 		description.setLayout(new FillLayout());
@@ -112,6 +145,7 @@ public class ResultFilterPreferencePage extends PreferencePage implements
 			MessageDialog.openError(Display.getCurrent().getActiveShell(),
 					"Browser Failure", "Browser cannot be initialized.");
 		}
+		clearHTMLDescription();
 
 		Job job = new DatabaseJob("Querying Sierra Artifacts") {
 			@Override
@@ -166,6 +200,40 @@ public class ResultFilterPreferencePage extends PreferencePage implements
 	}
 
 	/**
+	 * Must be called from a database job.
+	 */
+	private IStatus queryFindingTypeHTMLDescriptionOf(long findingTypeId) {
+		try {
+			final Connection c = Data.readOnlyConnection();
+			try {
+				final Statement st = c.createStatement();
+				try {
+					final String query = "select INFO from FINDING_TYPE where ID="
+							+ findingTypeId;
+					final ResultSet rs = st.executeQuery(query);
+					while (rs.next()) {
+						final String description = rs.getString(1);
+						PlatformUI.getWorkbench().getDisplay().asyncExec(
+								new Runnable() {
+									public void run() {
+										setHTMLDescription(description);
+									}
+								});
+					}
+				} finally {
+					st.close();
+				}
+			} finally {
+				c.close();
+			}
+		} catch (SQLException e) {
+			return SLStatus.createErrorStatus(
+					"Query of Sierra finding type description failed", e);
+		}
+		return Status.OK_STATUS;
+	}
+
+	/**
 	 * Must be called from the UI thread.
 	 */
 	private void fillTableContents() {
@@ -182,10 +250,30 @@ public class ResultFilterPreferencePage extends PreferencePage implements
 			item.setText(1, row.findingTypeDescription);
 			item.setData(row.id);
 		}
-//		for (TreeColumn c : f_table.getColumns()) {
-//			c.pack();
-//		}
 		f_panel.getParent().layout();
+	}
+
+	private final Display f_display = PlatformUI.getWorkbench().getDisplay();
+
+	private final RGB fBackgroundColorRGB = f_display.getSystemColor(
+			SWT.COLOR_LIST_BACKGROUND).getRGB();
+
+	/**
+	 * Must be called from the UI thread.
+	 */
+	private void clearHTMLDescription() {
+		setHTMLDescription("No finding type selected.");
+	}
+
+	/**
+	 * Must be called from the UI thread.
+	 */
+	private void setHTMLDescription(final String description) {
+		StringBuffer b = new StringBuffer();
+		HTMLPrinter.insertPageProlog(b, 0, fBackgroundColorRGB,
+				StyleSheetHelper.getInstance().getStyleSheet());
+		b.append(description);
+		f_detailsText.setText(b.toString());
 	}
 
 	public void init(IWorkbench workbench) {
