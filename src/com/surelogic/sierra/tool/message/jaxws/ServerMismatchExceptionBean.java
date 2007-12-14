0357c0c385277fbcbd41b7b447e66687c49d1eca
Nathan Boy <nathan.boy@surelogic.com>
2007-12-14 19:06:39 +0000

    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/sierra-message@3951 d02ae147-ee12-4a35-9452-91c89e8b2592

--- src/com/surelogic/sierra/tool/message/jaxws/ServerMismatchExceptionBean.java
+++ src/com/surelogic/sierra/tool/message/jaxws/ServerMismatchExceptionBean.java
@@ -1,31 +1,36 @@
 package com.surelogic.sierra.tool.message.jaxws;
 
+import java.io.Serializable;
+
 import javax.xml.bind.annotation.XmlAccessOrder;
 import javax.xml.bind.annotation.XmlAccessorOrder;
 import javax.xml.bind.annotation.XmlRootElement;
 import javax.xml.bind.annotation.XmlType;
 
-
 @XmlType(name = "ServerMismatchException")
 @XmlRootElement(name = "ServerMismatchFault", namespace = "http://message.tool.sierra.surelogic.com/")
 @XmlAccessorOrder(XmlAccessOrder.ALPHABETICAL)
-public class ServerMismatchExceptionBean {
-    private String expected;
-    private String actual;
+public class ServerMismatchExceptionBean implements Serializable {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 4600136609529425024L;
+	private String expected;
+	private String actual;
 
-    public String getExpected() {
-        return expected;
-    }
+	public String getExpected() {
+		return expected;
+	}
 
-    public void setExpected(String expected) {
-        this.expected = expected;
-    }
+	public void setExpected(String expected) {
+		this.expected = expected;
+	}
 
-    public String getActual() {
-        return actual;
-    }
+	public String getActual() {
+		return actual;
+	}
 
-    public void setActual(String actual) {
-        this.actual = actual;
-    }
+	public void setActual(String actual) {
+		this.actual = actual;
+	}
 }
