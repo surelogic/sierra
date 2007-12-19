264f5443815fec478c6fff8c8845dfdb6a1f9e74
Nathan Boy <nathan.boy@surelogic.com>
2007-12-19 18:47:32 +0000

    Changed message to run the JAXWS client with sun jax-ws ri dependencies.
    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/sierra-message@4154 d02ae147-ee12-4a35-9452-91c89e8b2592

--- src/com/surelogic/sierra/tool/message/SierraServiceClient.java
+++ src/com/surelogic/sierra/tool/message/SierraServiceClient.java
@@ -1,26 +1,25 @@
 package com.surelogic.sierra.tool.message;
 
-import com.surelogic.sierra.tool.message.axis.Axis2Client;
-
+import com.surelogic.sierra.tool.message.jaxws.JAXWSClient;
 
 /**
  * Utility class for generating proxies that call the Sierra client web service.
  * The objects returned by this class will implement the {@link SierraService}
  * interface. In addition, two unchecked exceptions may be thrown:
  * {@link SierraServiceClientException} and {@link InvalidLoginException}.
- *
+ * 
  * @author nathan
- *
+ * 
  */
 public class SierraServiceClient {
-    private SierraServiceClient() {
-    }
+	private SierraServiceClient() {
+	}
 
-    public static SierraService create() {
-        return new Axis2Client();
-    }
+	public static SierraService create() {
+		return new JAXWSClient();
+	}
 
-    public static SierraService create(SierraServerLocation location) {
-        return new Axis2Client(location);
-    }
+	public static SierraService create(SierraServerLocation location) {
+		return new JAXWSClient(location);
+	}
 }
--- src/com/surelogic/sierra/tool/message/jaxws/JAXWSClient.java
+++ src/com/surelogic/sierra/tool/message/jaxws/JAXWSClient.java
@@ -1,51 +1,160 @@
 package com.surelogic.sierra.tool.message.jaxws;
 
+import com.surelogic.sierra.tool.message.AuditTrailResponse;
+import com.surelogic.sierra.tool.message.CommitAuditTrailRequest;
+import com.surelogic.sierra.tool.message.CommitAuditTrailResponse;
+import com.surelogic.sierra.tool.message.GetAuditTrailRequest;
+import com.surelogic.sierra.tool.message.GlobalSettings;
+import com.surelogic.sierra.tool.message.GlobalSettingsRequest;
+import com.surelogic.sierra.tool.message.InvalidLoginException;
+import com.surelogic.sierra.tool.message.MergeAuditTrailRequest;
+import com.surelogic.sierra.tool.message.MergeAuditTrailResponse;
+import com.surelogic.sierra.tool.message.QualifierRequest;
+import com.surelogic.sierra.tool.message.Qualifiers;
+import com.surelogic.sierra.tool.message.Scan;
+import com.surelogic.sierra.tool.message.ServerMismatchException;
+import com.surelogic.sierra.tool.message.ServerUIDReply;
+import com.surelogic.sierra.tool.message.ServerUIDRequest;
+import com.surelogic.sierra.tool.message.SettingsReply;
+import com.surelogic.sierra.tool.message.SettingsRequest;
 import com.surelogic.sierra.tool.message.SierraServerLocation;
 import com.surelogic.sierra.tool.message.SierraService;
 import com.surelogic.sierra.tool.message.SierraServiceClientAuthenticator;
+import com.surelogic.sierra.tool.message.SierraServiceClientException;
 
 import java.net.Authenticator;
 import java.net.URL;
 
 import javax.xml.namespace.QName;
 import javax.xml.ws.Service;
+import javax.xml.ws.WebServiceException;
 
+public class JAXWSClient extends Service implements SierraService {
 
-public class JAXWSClient extends Service {
-    /**
-     * Construct a client that points to localhost:8080
-     */
-    public JAXWSClient() {
-        super(SierraServerLocation.DEFAULT.createWSDLUrl(),
-            new QName("http://services.sierra.surelogic.com/",
-                "SierraServiceBeanService"));
-    }
+	private final SierraService service;
 
-    /**
-     * Construct a client pointing to the specified host.
-     *
-     * @param host
-     *            a String of type <em>server</em> or <em>server</em>:<em>port</em>
-     */
-    public JAXWSClient(SierraServerLocation server) {
-        super((server == null) ? SierraServerLocation.DEFAULT.createWSDLUrl()
-                               : server.createWSDLUrl(),
-            new QName("http://services.sierra.surelogic.com/",
-                "SierraServiceBeanService"));
+	/**
+	 * Construct a client that points to localhost:8080
+	 */
+	public JAXWSClient() {
+		super(SierraServerLocation.DEFAULT.createWSDLUrl(), new QName(
+				"http://services.sierra.surelogic.com/",
+				"SierraServiceBeanService"));
+		this.service = getSierraServicePort();
+	}
 
-        // I need this for BASIC HTTP authenticator for connecting to the
-        // WebService
-        Authenticator.setDefault(new SierraServiceClientAuthenticator(
-                server.getUser(), server.getPass()));
-    }
+	/**
+	 * Construct a client pointing to the specified host.
+	 * 
+	 * @param host
+	 *            a String of type <em>server</em> or <em>server</em>:<em>port</em>
+	 */
+	public JAXWSClient(SierraServerLocation server) {
+		super((server == null) ? SierraServerLocation.DEFAULT.createWSDLUrl()
+				: server.createWSDLUrl(), new QName(
+				"http://services.sierra.surelogic.com/",
+				"SierraServiceBeanService"));
 
-    public JAXWSClient(URL wsdlDocumentLocation, QName serviceName) {
-        super(wsdlDocumentLocation, serviceName);
-    }
+		// I need this for BASIC HTTP authenticator for connecting to the
+		// WebService
+		Authenticator.setDefault(new SierraServiceClientAuthenticator(server
+				.getUser(), server.getPass()));
+		this.service = getSierraServicePort();
+	}
 
-    public SierraService getSierraServicePort() {
-        return super.getPort(new QName(
-                "http://services.sierra.surelogic.com/", "SierraServiceBeanPort"),
-            SierraService.class);
-    }
+	public JAXWSClient(URL wsdlDocumentLocation, QName serviceName) {
+		super(wsdlDocumentLocation, serviceName);
+		this.service = getSierraServicePort();
+	}
+
+	private SierraService getSierraServicePort() {
+		return super.getPort(new QName("http://services.sierra.surelogic.com/",
+				"SierraServiceBeanPort"), SierraService.class);
+	}
+
+	public CommitAuditTrailResponse commitAuditTrails(
+			CommitAuditTrailRequest audits) throws ServerMismatchException {
+		try {
+			return service.commitAuditTrails(audits);
+		} catch (WebServiceException e) {
+			throw handleException(e);
+		}
+
+	}
+
+	public AuditTrailResponse getAuditTrails(GetAuditTrailRequest request)
+			throws ServerMismatchException {
+		try {
+			return service.getAuditTrails(request);
+		} catch (WebServiceException e) {
+			throw handleException(e);
+		}
+	}
+
+	public GlobalSettings getGlobalSettings(GlobalSettingsRequest request) {
+		try {
+			return service.getGlobalSettings(request);
+		} catch (WebServiceException e) {
+			throw handleException(e);
+		}
+	}
+
+	public Qualifiers getQualifiers(QualifierRequest request) {
+		try {
+			return service.getQualifiers(request);
+		} catch (WebServiceException e) {
+			throw handleException(e);
+		}
+	}
+
+	public SettingsReply getSettings(SettingsRequest request)
+			throws ServerMismatchException {
+		try {
+			return service.getSettings(request);
+		} catch (WebServiceException e) {
+			throw handleException(e);
+		}
+	}
+
+	public ServerUIDReply getUid(ServerUIDRequest request) {
+		try {
+			return service.getUid(request);
+		} catch (WebServiceException e) {
+			throw handleException(e);
+		}
+	}
+
+	public MergeAuditTrailResponse mergeAuditTrails(MergeAuditTrailRequest seed)
+			throws ServerMismatchException {
+		try {
+			return service.mergeAuditTrails(seed);
+		} catch (WebServiceException e) {
+			throw handleException(e);
+		}
+	}
+
+	public void publishRun(Scan scan) {
+		try {
+			service.publishRun(scan);
+		} catch (WebServiceException e) {
+			throw handleException(e);
+		}
+	}
+
+	public void writeGlobalSettings(GlobalSettings settings) {
+		try {
+			service.writeGlobalSettings(settings);
+		} catch (WebServiceException e) {
+			throw handleException(e);
+		}
+	}
+
+	private SierraServiceClientException handleException(WebServiceException e) {
+		if (e.getMessage().startsWith(
+				"request requires HTTP authentication: Unauthorized")) {
+			return new InvalidLoginException(e);
+		} else {
+			return new SierraServiceClientException(e);
+		}
+	}
 }
