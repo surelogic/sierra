468ce8316b8552ba596ad7edbf92f7259897e396
Edwin Chan <edwin.chan@surelogic.com>
2007-12-20 16:17:19 +0000

    Refactored code to DirectoryTarget Added code to do something with the output location
    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/sierra-client-eclipse@4323 d02ae147-ee12-4a35-9452-91c89e8b2592

--- src/com/surelogic/sierra/client/eclipse/actions/NewScanAction.java
+++ src/com/surelogic/sierra/client/eclipse/actions/NewScanAction.java
@@ -1,11 +1,11 @@
 package com.surelogic.sierra.client.eclipse.actions;
 
-import java.io.File;
 import java.net.URI;
 import java.util.*;
 import java.util.logging.Logger;
 
 import org.eclipse.core.resources.IResource;
+import org.eclipse.core.resources.IWorkspaceRoot;
 import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.IStatus;
@@ -21,7 +21,8 @@ import com.surelogic.common.eclipse.logging.SLStatus;
 import com.surelogic.common.logging.SLLogger;
 import com.surelogic.sierra.client.eclipse.preferences.PreferenceConstants;
 import com.surelogic.sierra.tool.*;
-import com.surelogic.sierra.tool.pmd40.PMDTool;
+import com.surelogic.sierra.tool.findbugs.FindBugs1_3_0Tool;
+import com.surelogic.sierra.tool.pmd.PMD4_0Tool;
 import com.surelogic.sierra.tool.targets.*;
 
 /**
@@ -48,39 +49,25 @@ public class NewScanAction extends AbstractProjectSelectedMenuAction {
         protected IStatus run(IProgressMonitor monitor) {
           final SLProgressMonitor wrapper = new SLProgressMonitorWrapper(monitor);
           try {
-            ITool t = new PMDTool();
+            final IWorkspaceRoot root = ResourcesPlugin.getWorkspace().getRoot();
+            ITool t = true ? new FindBugs1_3_0Tool() : new PMD4_0Tool();
             IToolInstance ti = t.create(wrapper);  
+            
             for(IJavaProject p : selectedProjects) {
               for(IClasspathEntry cpe : p.getResolvedClasspath(true)) {
                 if (cpe.getEntryKind() == IClasspathEntry.CPE_SOURCE) {
-                  IResource res = ResourcesPlugin.getWorkspace().getRoot().findMember(cpe.getPath());
+                  IResource res = root.findMember(cpe.getPath());
                   URI loc = res.getLocationURI();
                   ti.addTarget(new DirectoryTarget(true, loc) {
                     public boolean exclude(String relativePath) {
                       // TODO Auto-generated method stub
                       return false;
                     }
-                    public Iterable<URI> getFiles() {
-                      List<URI> files = new ArrayList<URI>();
-                      findFiles(files, new File(getLocation()));
-                      return files;
-                    }            
-                    private void findFiles(List<URI> files, File here) {   
-                      if (!here.exists()) {
-                        return;
-                      }
-                      if (here.isDirectory()) {
-                        for(File f : here.listFiles()) {
-                          findFiles(files, f);
-                        }
-                      }
-                      else if (here.isFile() && here.getName().endsWith(".java")) {
-                        files.add(here.toURI());
-                      }
-                    }
                   });
                 }
               }
+              URI out = root.findMember(p.getOutputLocation()).getLocationURI();
+              ti.addTarget(new FullDirectoryTarget(false, out));
             }
             ti.run();
           } catch(Throwable ex) {
