c873f870972f0af16767911ab67646b32e50a109
Edwin Chan <edwin.chan@surelogic.com>
2008-01-14 19:46:32 +0000

    Factored out code for creating tools to ToolUtil
    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/sierra-tool@4914 d02ae147-ee12-4a35-9452-91c89e8b2592

---
+++ src/com/surelogic/sierra/tool/ToolUtil.java
@@ -0,0 +1,30 @@
+package com.surelogic.sierra.tool;
+
+import com.surelogic.sierra.tool.findbugs.*;
+import com.surelogic.sierra.tool.message.Config;
+import com.surelogic.sierra.tool.pmd.*;
+import com.surelogic.sierra.tool.reckoner.*;
+
+public class ToolUtil {
+  public static ITool create(Config config) {
+    // FIX look at config
+    return create(config, true);
+  }
+  
+  public static ITool create(Config config, boolean runRemotely) {
+    if (runRemotely) {
+      return new LocalTool();
+    }
+    final MultiTool t = new MultiTool();
+    if (!config.getExcludedToolsList().contains("findbugs")) {
+      t.addTool(new FindBugs1_3_0Tool());
+    }
+    if (!config.getExcludedToolsList().contains("pmd")) {
+      t.addTool(new PMD4_0Tool());
+    }
+    if (!config.getExcludedToolsList().contains("reckoner")) {
+      t.addTool(new Reckoner1_0Tool());
+    }
+    return t;
+  }
+}
