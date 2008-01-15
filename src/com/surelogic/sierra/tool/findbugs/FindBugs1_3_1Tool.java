947fd4e7b73bdde3f68e0438846f47a24f728b9a
Edwin Chan <edwin.chan@surelogic.com>
2008-01-15 19:32:39 +0000

    Added for FB 1.3.1
    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/sierra-tool@4947 d02ae147-ee12-4a35-9452-91c89e8b2592

---
+++ src/com/surelogic/sierra/tool/findbugs/FindBugs1_3_1Tool.java
@@ -0,0 +1,22 @@
+package com.surelogic.sierra.tool.findbugs;
+
+import java.util.Collections;
+import java.util.Set;
+
+import edu.umd.cs.findbugs.FindBugs2;
+import edu.umd.cs.findbugs.IFindBugsEngine;
+
+public class FindBugs1_3_1Tool extends AbstractFindBugsTool {
+  public FindBugs1_3_1Tool() {
+    super("1.3.1");
+  }
+
+  public Set<String> getArtifactTypes() {
+    return Collections.emptySet();
+  }
+  
+  @Override
+  protected IFindBugsEngine createEngine() {
+    return new FindBugs2();
+  }
+}
