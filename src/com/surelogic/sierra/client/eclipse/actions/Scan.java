1bd89ee37b5db5724a2a2fbb10b6b4f827391d04
Edwin Chan <edwin.chan@surelogic.com>
2008-01-16 15:12:41 +0000

    Factored out ScanProjectJobAdapter
    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/sierra-client-eclipse@5049 d02ae147-ee12-4a35-9452-91c89e8b2592

--- src/com/surelogic/sierra/client/eclipse/actions/Scan.java
+++ src/com/surelogic/sierra/client/eclipse/actions/Scan.java
@@ -10,9 +10,7 @@ import java.util.logging.Logger;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Status;
-import org.eclipse.core.runtime.jobs.IJobChangeEvent;
 import org.eclipse.core.runtime.jobs.Job;
-import org.eclipse.core.runtime.jobs.JobChangeAdapter;
 import org.eclipse.jdt.core.ICompilationUnit;
 import org.eclipse.jdt.core.IJavaProject;
 
@@ -399,62 +397,6 @@ public final class Scan {
 
 		public String getErrorMessage() {
 			return f_errorMessage;
-		}
-	}
-
-	/**
-	 * The adapter for the {@link ScanProjectJob}, handles all the possible
-	 * cases for status messages from the job and displays and logs appropriate
-	 * message.
-	 * 
-	 * @author Tanmay.Sinha
-	 * 
-	 */
-	private static class ScanProjectJobAdapter extends JobChangeAdapter {
-
-		private final String f_projectName;
-
-		/**
-		 * 
-		 * @param projectName
-		 *            the name of the project
-		 */
-		public ScanProjectJobAdapter(String projectName) {
-			super();
-			this.f_projectName = projectName;
-		}
-
-		@Override
-		public void running(IJobChangeEvent event) {
-			LOG.info("Starting scan on " + f_projectName);
-		}
-
-		@Override
-		public void done(IJobChangeEvent event) {
-			if (event.getResult().equals(Status.OK_STATUS)) {
-				LOG.info("Completed scan for " + f_projectName);
-				if (PreferenceConstants.showBalloonNotifications())
-					BalloonUtility
-							.showMessage("Sierra Scan Completed on "
-									+ f_projectName,
-									"You may now examine the results.");
-
-			} else if (event.getResult().equals(Status.CANCEL_STATUS)) {
-				LOG.info("Canceled scan on " + f_projectName);
-			} else {
-				Throwable t = event.getResult().getException();
-				LOG.log(Level.SEVERE,
-						"(top-level) Error while trying to run scan on "
-								+ f_projectName, t);
-				if (event.getResult().isMultiStatus()) {
-					for (IStatus s : event.getResult().getChildren()) {
-						Throwable t1 = s.getException();
-						LOG.log(Level.SEVERE,
-								"(multi-status) Error while trying to run scan on "
-										+ f_projectName, t1);
-					}
-				}
-			}
 		}
 	}
 }
