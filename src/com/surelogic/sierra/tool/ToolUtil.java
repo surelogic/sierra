20e7e07268f36078428a7be69b99c91feecc6ffd
Edwin Chan <edwin.chan@surelogic.com>
2008-01-21 20:04:24 +0000

    Changed to look for the FB plugin in the new location
    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/sierra-tool@5148 d02ae147-ee12-4a35-9452-91c89e8b2592

--- src/com/surelogic/sierra/tool/ToolUtil.java
+++ src/com/surelogic/sierra/tool/ToolUtil.java
@@ -21,7 +21,7 @@ public class ToolUtil {
     }
     final MultiTool t = new MultiTool();
     if (!config.getExcludedToolsList().contains("findbugs")) {
-      t.addTool(new FindBugs1_3_0Tool(config.getToolsDirectory()));
+      t.addTool(new FindBugs1_3_0Tool(config.getPluginDir(SierraToolConstants.FB_PLUGIN_ID)));
     }
     if (!config.getExcludedToolsList().contains("pmd")) {
       t.addTool(new PMD4_0Tool());
