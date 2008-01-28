f47165c7c4ae4959cef47ba6173a7477bbf8cb79
Nathan Boy <nathan.boy@surelogic.com>
2008-01-28 15:33:49 +0000

    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/sierra-message@5348 d02ae147-ee12-4a35-9452-91c89e8b2592

--- src/com/surelogic/sierra/tool/message/ArtifactGenerator.java
+++ src/com/surelogic/sierra/tool/message/ArtifactGenerator.java
@@ -2,73 +2,98 @@ package com.surelogic.sierra.tool.message;
 
 import com.surelogic.common.SLProgressMonitor;
 
-
 /**
- *
+ * 
  * @author nathan
- *
+ * 
  */
 public interface ArtifactGenerator {
-    public MetricBuilder metric();
+	public MetricBuilder metric();
 
-    public ArtifactBuilder artifact();
+	public ArtifactBuilder artifact();
 
-    public ErrorBuilder error();
+	public ErrorBuilder error();
 
-    public void finished(SLProgressMonitor monitor);
-    
-    public void rollback();
+	public void finished(SLProgressMonitor monitor);
 
-    public interface ArtifactBuilder {
-    	/**
-    	 * Add additional source locations to this artifact.
-    	 * @return
-    	 */
-        public SourceLocationBuilder sourceLocation();
+	public void rollback();
 
-        /**
-         * Call this to build the primary source location.  This method should only be called once.
-         * @return
-         */
-        public SourceLocationBuilder primarySourceLocation();
+	public interface ArtifactBuilder {
+		/**
+		 * Add additional source locations to this artifact.
+		 * 
+		 * @return
+		 */
+		public SourceLocationBuilder sourceLocation();
 
-        public ArtifactBuilder findingType(String tool, String version,
-            String mnemonic);
+		/**
+		 * Call this to build the primary source location. This method should
+		 * only be called once.
+		 * 
+		 * @return
+		 */
+		public SourceLocationBuilder primarySourceLocation();
 
-        public ArtifactBuilder priority(Priority priority);
+		public ArtifactBuilder findingType(String tool, String version,
+				String mnemonic);
 
-        public ArtifactBuilder severity(Severity severity);
+		public ArtifactBuilder priority(Priority priority);
 
-        public ArtifactBuilder message(String message);
+		public ArtifactBuilder severity(Severity severity);
 
-        public void build();
-    }
+		public ArtifactBuilder message(String message);
 
-    public interface SourceLocationBuilder {
-        SourceLocationBuilder type(IdentifierType type);
+		public void build();
+	}
 
-        SourceLocationBuilder identifier(String name);
+	public interface SourceLocationBuilder {
+		SourceLocationBuilder type(IdentifierType type);
 
-        SourceLocationBuilder compilation(String compilation);
+		SourceLocationBuilder identifier(String name);
 
-        SourceLocationBuilder className(String className);
+		/**
+		 * The unqualified name of the compilation unit. For example, if a class
+		 * name <code>Foo</code> is declared in a compilation unit located at
+		 * <code>com/surelogic/baz/Bar.java</code>, then the name of the
+		 * compilation unit is Bar.
+		 * 
+		 * @param compilation
+		 * @return
+		 */
+		SourceLocationBuilder compilation(String compilation);
 
-        SourceLocationBuilder packageName(String packageName);
+		/**
+		 * The name of the most specific class this artifact was found in. This
+		 * might not be the same as the name of the compilation unit.
+		 * 
+		 * @param className
+		 * @return
+		 */
+		SourceLocationBuilder className(String className);
 
-        SourceLocationBuilder lineOfCode(int line);
+		/**
+		 * The package name of the class, usind <code>.</code> as the
+		 * separator. (ex. com.surelogic.baz)
+		 * 
+		 * @param packageName
+		 * @return
+		 */
+		SourceLocationBuilder packageName(String packageName);
 
-        SourceLocationBuilder endLine(int line);
+		SourceLocationBuilder lineOfCode(int line);
 
-        SourceLocationBuilder hash(Long hash);
+		SourceLocationBuilder endLine(int line);
 
-        void build();
-    }
+		SourceLocationBuilder hash(Long hash);
 
-    public interface ErrorBuilder {
-        ErrorBuilder message(String message);
+		void build();
+	}
 
-        ErrorBuilder tool(String tool);
+	public interface ErrorBuilder {
+		ErrorBuilder message(String message);
 
-        void build();
-    }
+		ErrorBuilder tool(String tool);
+
+		void build();
+	}
 }
