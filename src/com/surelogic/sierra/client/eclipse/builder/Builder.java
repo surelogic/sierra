9faf0030215d77831e8a3ec4e4599628770cdfdb
Edwin Chan <edwin.chan@surelogic.com>
2008-01-29 18:44:23 +0000

    Added template for builder
    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/sierra-client-eclipse@5408 d02ae147-ee12-4a35-9452-91c89e8b2592

---
+++ src/com/surelogic/sierra/client/eclipse/builder/Builder.java
@@ -0,0 +1,21 @@
+package com.surelogic.sierra.client.eclipse.builder;
+
+import java.util.Map;
+
+import org.eclipse.core.resources.*;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IProgressMonitor;
+
+import com.surelogic.common.eclipse.builder.*;
+
+public class Builder extends AbstractJavaBuilder {
+
+  @SuppressWarnings("unchecked")
+  @Override
+  protected IProject[] build(int kind, Map args, IProgressMonitor monitor)
+      throws CoreException {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+}
