047922bbd83234a5a134db8bfd99d05eb86e23d4
Tim Halloran <tim.halloran@surelogic.com>
2008-01-30 23:03:21 +0000

    Logging work
    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/sierra-portal-servlets@5487 d02ae147-ee12-4a35-9452-91c89e8b2592

--- src/com/surelogic/sierra/lifecycle/SierraContextListener.java
+++ src/com/surelogic/sierra/lifecycle/SierraContextListener.java
@@ -1,26 +1,51 @@
 package com.surelogic.sierra.lifecycle;
 
+import java.io.File;
 import java.sql.Connection;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 
+import com.surelogic.common.logging.SLLogger;
 import com.surelogic.sierra.jdbc.server.Server;
 import com.surelogic.sierra.jdbc.server.ServerConnection;
 import com.surelogic.sierra.jdbc.server.UserTransaction;
 import com.surelogic.sierra.schema.SierraSchemaUtility;
+
 /**
  * This context listener bootstraps the database on server startup.
- * @author nathan
- *
  */
 public class SierraContextListener implements ServletContextListener {
 
-	public void contextDestroyed(ServletContextEvent arg0) {
+	public void contextDestroyed(ServletContextEvent sce) {
 		// Do nothing
 	}
 
-	public void contextInitialized(ServletContextEvent arg0) {
+	public void contextInitialized(ServletContextEvent sce) {
+		/*
+		 * TODO: this first part should probably be factored into a superclass
+		 * that can be used by both the WARs.
+		 */
+		final String loggerOption = sce.getServletContext().getInitParameter(
+				"SLLogger");
+		if ("Use Sierra Directory".equals(loggerOption)) {
+			final SimpleDateFormat dateFormat = new SimpleDateFormat(
+					"-yyyy.MM.dd-'at'-HH.mm.ss.SSS");
+			String logFileName = System.getProperty("user.home")
+					+ File.separator + "Sierra" + File.separator + "Server"
+					+ File.separator
+					+ sce.getServletContext().getServletContextName()
+					+ dateFormat.format(new Date()) + ".txt";
+			System.setProperty(SLLogger.LOG_FILE_NAME_PROPERTY, logFileName);
+		} else if ("No File Output".equals(loggerOption)) {
+			System.setProperty(SLLogger.LOG_FILE_NAME_PROPERTY,
+					SLLogger.NO_FILE_OUTPUT);
+		}
+		/*
+		 * Boot up the database if necessary.
+		 */
 		ServerConnection.withTransaction(new UserTransaction<Void>() {
 
 			public String getUserName() {
@@ -35,5 +60,4 @@ public class SierraContextListener implements ServletContextListener {
 		});
 
 	}
-
 }
