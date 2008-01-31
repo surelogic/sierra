9a510f47e2c8457fc47ef3a6b318693849954402
Tim Halloran <tim.halloran@surelogic.com>
2008-01-31 02:12:20 +0000

    Logging fix for the server
    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/sierra-jdbc@5497 d02ae147-ee12-4a35-9452-91c89e8b2592

--- src/com/surelogic/sierra/server/lifecycle/LogServletContextListener.java
+++ src/com/surelogic/sierra/server/lifecycle/LogServletContextListener.java
@@ -3,10 +3,13 @@ package com.surelogic.sierra.server.lifecycle;
 import java.io.File;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.logging.FileHandler;
+import java.util.logging.Level;
 
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 
+import com.surelogic.common.i18n.I18N;
 import com.surelogic.common.logging.SLLogger;
 
 /**
@@ -14,8 +17,9 @@ import com.surelogic.common.logging.SLLogger;
  * context parameters on server startup.
  * <p>
  * The parameter <tt>SLLogger</tt> may be set to <tt>Use Sierra Directory</tt>
- * to cause the logger to log under the ~/Sierra/Server directory or
- * <tt>No File Output</tt> to cause only console logging.
+ * to cause the logger to log under the ~/Sierra/Server directory. If
+ * <tt>SLLogger</tt> is set to <tt>No File Output</tt> only console logging
+ * will be done. The default (no value) is to log into the temporary directory.
  */
 public class LogServletContextListener implements ServletContextListener {
 
@@ -29,18 +33,26 @@ public class LogServletContextListener implements ServletContextListener {
 		 */
 		final String loggerOption = sce.getServletContext().getInitParameter(
 				"SLLogger");
+		if ("No File Output".equals(loggerOption))
+			return;
+		final SimpleDateFormat dateFormat = new SimpleDateFormat(
+				"-yyyy.MM.dd-'at'-HH.mm.ss.SSS");
+		final String logFileName;
 		if ("Use Sierra Directory".equals(loggerOption)) {
-			final SimpleDateFormat dateFormat = new SimpleDateFormat(
-					"-yyyy.MM.dd-'at'-HH.mm.ss.SSS");
-			String logFileName = System.getProperty("user.home")
-					+ File.separator + "Sierra" + File.separator + "Server"
-					+ File.separator
+			logFileName = System.getProperty("user.home") + File.separator
+					+ "Sierra" + File.separator + "Server" + File.separator
 					+ sce.getServletContext().getServletContextName()
 					+ dateFormat.format(new Date()) + ".txt";
-			System.setProperty(SLLogger.LOG_FILE_NAME_PROPERTY, logFileName);
-		} else if ("No File Output".equals(loggerOption)) {
-			System.setProperty(SLLogger.LOG_FILE_NAME_PROPERTY,
-					SLLogger.NO_FILE_OUTPUT);
+		} else {
+			logFileName = System.getProperty("java.io.tmpdir") + File.separator
+					+ sce.getServletContext().getServletContextName()
+					+ dateFormat.format(new Date()) + ".txt";
+		}
+		try {
+			final FileHandler fh = new FileHandler(logFileName);
+			SLLogger.addHandler(fh);
+		} catch (Exception e) {
+			SLLogger.getLogger().log(Level.SEVERE, I18N.err(29, logFileName));
 		}
 	}
 }
