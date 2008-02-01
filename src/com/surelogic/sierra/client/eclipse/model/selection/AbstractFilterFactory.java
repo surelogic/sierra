ba1ccc6b98b2f80c31aa6395f5d80d12aac0ea21
Edwin Chan <edwin.chan@surelogic.com>
2008-02-01 14:43:39 +0000

    Changed to implement equals()
    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/sierra-client-eclipse@5552 d02ae147-ee12-4a35-9452-91c89e8b2592

--- src/com/surelogic/sierra/client/eclipse/model/selection/AbstractFilterFactory.java
+++ src/com/surelogic/sierra/client/eclipse/model/selection/AbstractFilterFactory.java
@@ -2,10 +2,19 @@ package com.surelogic.sierra.client.eclipse.model.selection;
 
 public abstract class AbstractFilterFactory implements ISelectionFilterFactory {
 
-	public int compareTo(ISelectionFilterFactory o) {
+	public final int compareTo(ISelectionFilterFactory o) {
 		return getFilterLabel().compareTo(o.getFilterLabel());
 	}
 
+	@Override
+	public final boolean equals(Object o) {
+	  if (o instanceof ISelectionFilterFactory) {
+	    ISelectionFilterFactory f = (ISelectionFilterFactory) o;
+	    return getFilterLabel().equals(f.getFilterLabel());
+	  }
+	  return false;
+	}
+	
 	@Override
 	public String toString() {
 		return getFilterLabel();
