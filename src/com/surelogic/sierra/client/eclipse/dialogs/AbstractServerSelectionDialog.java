d4398c9ae89356ce82e35b1171051027422d8539
Edwin Chan <edwin.chan@surelogic.com>
2008-02-04 18:37:27 +0000

    ASSIGNED - bug 1329: Add buttons to Scan Filter preferences to send/get settings from a server https://fluid.surelogic.com/bugzilla/show_bug.cgi?id=1329
    Factored out existing code for server selection
    
    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/sierra-client-eclipse@5583 d02ae147-ee12-4a35-9452-91c89e8b2592

---
+++ src/com/surelogic/sierra/client/eclipse/dialogs/AbstractServerSelectionDialog.java
@@ -0,0 +1,102 @@
+package com.surelogic.sierra.client.eclipse.dialogs;
+
+import org.eclipse.jface.dialogs.Dialog;
+import org.eclipse.jface.dialogs.IDialogConstants;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.*;
+
+import com.surelogic.common.eclipse.SLImages;
+import com.surelogic.sierra.client.eclipse.model.SierraServer;
+import com.surelogic.sierra.client.eclipse.model.SierraServerManager;
+
+public abstract class AbstractServerSelectionDialog extends Dialog {
+
+	private final SierraServerManager f_manager = SierraServerManager
+			.getInstance();
+
+	private Table f_serverTable;
+
+	private SierraServer f_server = null;
+
+	private final String label;
+	
+	public final SierraServer getServer() {
+		return f_server;
+	}
+
+	protected AbstractServerSelectionDialog(Shell parentShell, String label) {
+		super(parentShell);
+		this.label = label;
+		setShellStyle(getShellStyle() | SWT.RESIZE | SWT.MAX);
+	}
+
+	@Override
+	protected final void configureShell(Shell newShell) {
+		super.configureShell(newShell);
+		newShell.setImage(SLImages.getImage(SLImages.IMG_SIERRA_SERVER));
+		newShell.setText("Select Sierra Server");
+	}
+
+	@Override
+	protected Control createDialogArea(Composite parent) {
+		Composite panel = (Composite) super.createDialogArea(parent);
+		GridLayout gridLayout = new GridLayout();
+		gridLayout.numColumns = 2;
+		panel.setLayout(gridLayout);
+
+		Label banner = new Label(panel, SWT.NONE);
+		banner.setLayoutData(new GridData(SWT.CENTER, SWT.TOP, false, true, 1,
+				1));
+		banner.setImage(SLImages
+				.getImage(SLImages.IMG_SIERRA_POWERED_BY_SURELOGIC_GLOBE));
+
+		final Composite entryPanel = new Composite(panel, SWT.NONE);
+		entryPanel.setLayoutData(new GridData(GridData.FILL_BOTH));
+		gridLayout = new GridLayout();
+		entryPanel.setLayout(gridLayout);
+
+		final Label l = new Label(entryPanel, SWT.WRAP);
+		l.setText(label);
+
+		f_serverTable = new Table(entryPanel, SWT.FULL_SELECTION);
+		f_serverTable.setLayoutData(new GridData(GridData.FILL_BOTH));
+
+		for (SierraServer server : f_manager.getServers()) {
+			TableItem item = new TableItem(f_serverTable, SWT.NONE);
+			item.setText(server.getLabel());
+			item.setImage(SLImages.getImage(SLImages.IMG_SIERRA_SERVER));
+			item.setData(server);
+		}
+
+		addToEntryPanel(entryPanel);
+
+		f_serverTable.addListener(SWT.Selection, new Listener() {
+			public void handleEvent(Event event) {
+				final TableItem[] sa = f_serverTable.getSelection();
+				if (sa.length > 0) {
+					final TableItem selection = sa[0];
+					f_server = (SierraServer) selection.getData();
+				}
+				setOKState();
+			}
+		});
+
+		// add controls to composite as necessary
+		return panel;
+	}
+
+	protected abstract void addToEntryPanel(Composite entryPanel);
+
+  @Override
+	protected final Control createContents(Composite parent) {
+		final Control contents = super.createContents(parent);
+		setOKState();
+		return contents;
+	}
+
+	private final void setOKState() {
+		getButton(IDialogConstants.OK_ID).setEnabled(f_server != null);
+	}
+}
--- src/com/surelogic/sierra/client/eclipse/dialogs/ServerSelectionDialog.java
+++ src/com/surelogic/sierra/client/eclipse/dialogs/ServerSelectionDialog.java
@@ -1,39 +1,9 @@
 package com.surelogic.sierra.client.eclipse.dialogs;
 
-import org.eclipse.jface.dialogs.Dialog;
-import org.eclipse.jface.dialogs.IDialogConstants;
 import org.eclipse.swt.SWT;
-import org.eclipse.swt.layout.GridData;
-import org.eclipse.swt.layout.GridLayout;
-import org.eclipse.swt.widgets.Button;
-import org.eclipse.swt.widgets.Composite;
-import org.eclipse.swt.widgets.Control;
-import org.eclipse.swt.widgets.Event;
-import org.eclipse.swt.widgets.Label;
-import org.eclipse.swt.widgets.Listener;
-import org.eclipse.swt.widgets.Shell;
-import org.eclipse.swt.widgets.Table;
-import org.eclipse.swt.widgets.TableItem;
+import org.eclipse.swt.widgets.*;
 
-import com.surelogic.common.eclipse.SLImages;
-import com.surelogic.sierra.client.eclipse.model.SierraServer;
-import com.surelogic.sierra.client.eclipse.model.SierraServerManager;
-
-public final class ServerSelectionDialog extends Dialog {
-
-	private final SierraServerManager f_manager = SierraServerManager
-			.getInstance();
-
-	private final String f_projectName;
-
-	private Table f_serverTable;
-
-	private SierraServer f_server = null;
-
-	public SierraServer getServer() {
-		return f_server;
-	}
-
+public final class ServerSelectionDialog extends AbstractServerSelectionDialog {
 	private boolean f_useForAllUnconnectedProjects = false;
 
 	public boolean useForAllUnconnectedProjects() {
@@ -41,81 +11,18 @@ public final class ServerSelectionDialog extends Dialog {
 	}
 
 	public ServerSelectionDialog(Shell parentShell, String projectName) {
-		super(parentShell);
-		setShellStyle(getShellStyle() | SWT.RESIZE | SWT.MAX);
-		f_projectName = projectName;
+		super(parentShell, "Select the server to connect '" + projectName + "' to:");
 	}
 
-	@Override
-	protected void configureShell(Shell newShell) {
-		super.configureShell(newShell);
-		newShell.setImage(SLImages.getImage(SLImages.IMG_SIERRA_SERVER));
-		newShell.setText("Select Sierra Server");
-	}
-
-	@Override
-	protected Control createDialogArea(Composite parent) {
-		Composite panel = (Composite) super.createDialogArea(parent);
-		GridLayout gridLayout = new GridLayout();
-		gridLayout.numColumns = 2;
-		panel.setLayout(gridLayout);
-
-		Label banner = new Label(panel, SWT.NONE);
-		banner.setLayoutData(new GridData(SWT.CENTER, SWT.TOP, false, true, 1,
-				1));
-		banner.setImage(SLImages
-				.getImage(SLImages.IMG_SIERRA_POWERED_BY_SURELOGIC_GLOBE));
-
-		final Composite entryPanel = new Composite(panel, SWT.NONE);
-		entryPanel.setLayoutData(new GridData(GridData.FILL_BOTH));
-		gridLayout = new GridLayout();
-		entryPanel.setLayout(gridLayout);
-
-		final Label l = new Label(entryPanel, SWT.WRAP);
-		l.setText("Select the server to connect '" + f_projectName + "' to:");
-
-		f_serverTable = new Table(entryPanel, SWT.FULL_SELECTION);
-		f_serverTable.setLayoutData(new GridData(GridData.FILL_BOTH));
-
-		for (SierraServer server : f_manager.getServers()) {
-			TableItem item = new TableItem(f_serverTable, SWT.NONE);
-			item.setText(server.getLabel());
-			item.setImage(SLImages.getImage(SLImages.IMG_SIERRA_SERVER));
-			item.setData(server);
-		}
-
-		final Button useForAll = new Button(entryPanel, SWT.CHECK);
-		useForAll.setText("Use this server for all unconnected projects");
-		useForAll.setSelection(f_useForAllUnconnectedProjects);
-		useForAll.addListener(SWT.Selection, new Listener() {
-			public void handleEvent(Event event) {
-				f_useForAllUnconnectedProjects = useForAll.getSelection();
-			}
-		});
-
-		f_serverTable.addListener(SWT.Selection, new Listener() {
-			public void handleEvent(Event event) {
-				final TableItem[] sa = f_serverTable.getSelection();
-				if (sa.length > 0) {
-					final TableItem selection = sa[0];
-					f_server = (SierraServer) selection.getData();
-				}
-				setOKState();
-			}
-		});
-
-		// add controls to composite as necessary
-		return panel;
-	}
-
-	@Override
-	protected Control createContents(Composite parent) {
-		final Control contents = super.createContents(parent);
-		setOKState();
-		return contents;
-	}
-
-	private void setOKState() {
-		getButton(IDialogConstants.OK_ID).setEnabled(f_server != null);
-	}
+  @Override
+  protected void addToEntryPanel(Composite entryPanel) {
+    final Button useForAll = new Button(entryPanel, SWT.CHECK);
+    useForAll.setText("Use this server for all unconnected projects");
+    useForAll.setSelection(f_useForAllUnconnectedProjects);
+    useForAll.addListener(SWT.Selection, new Listener() {
+      public void handleEvent(Event event) {
+        f_useForAllUnconnectedProjects = useForAll.getSelection();
+      }
+    });
+  }
 }
