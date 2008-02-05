d1f1052edf34c8e41f290bcc64dc89c1eb966740
Aaron Silinskas <aaron.silinskas@surelogic.com>
2008-02-05 15:34:01 +0000

    cleaned out some old code/files
    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/sierra-portal@5623 d02ae147-ee12-4a35-9452-91c89e8b2592

--- src/com/surelogic/sierra/gwt/client/test/TestService.java
+++
@@ -1,9 +0,0 @@
-package com.surelogic.sierra.gwt.client.test;
-
-import com.google.gwt.user.client.rpc.RemoteService;
-
-public interface TestService extends RemoteService {
-
-	public String toUpperCase(String text);
-
-}
