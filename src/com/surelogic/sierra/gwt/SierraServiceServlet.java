c329dfae5a3f5bf33f63b8b7198d96d3bf32f190
Aaron Silinskas <aaron.silinskas@surelogic.com>
2008-02-06 06:12:54 +0000

    made the SecurityHelper class and USER field public, until a better way can be found
    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/sierra-portal-servlets@5667 d02ae147-ee12-4a35-9452-91c89e8b2592

--- src/com/surelogic/sierra/gwt/SierraServiceServlet.java
+++ src/com/surelogic/sierra/gwt/SierraServiceServlet.java
@@ -7,6 +7,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.google.gwt.user.server.rpc.RemoteServiceServlet;
+import com.surelogic.sierra.jdbc.server.SecurityHelper;
 import com.surelogic.sierra.jdbc.server.UserContext;
 import com.surelogic.sierra.jdbc.user.User;
 
@@ -21,7 +22,7 @@ public abstract class SierraServiceServlet extends RemoteServiceServlet {
 	protected void service(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException {
 		try {
-			UserContext.set((User) req.getSession().getAttribute("SierraUser"));
+			UserContext.set((User) req.getSession().getAttribute(SecurityHelper.USER));
 			super.service(req, resp);
 		} finally {
 			UserContext.remove();
