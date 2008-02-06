7e18b0b8e934f0dc999c23938a342b3587e95384
Aaron Silinskas <aaron.silinskas@surelogic.com>
2008-02-06 06:13:03 +0000

    created a single-entry-point setup for GWT that handles login and authentication instead of the servlet filter created the ContentPanel to manage switching content panes, etc still lots to do to get Admin working again
    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/sierra-portal-servlets@5668 d02ae147-ee12-4a35-9452-91c89e8b2592

---
+++ src/com/surelogic/sierra/gwt/server/SessionServiceImpl.java
@@ -0,0 +1,46 @@
+package com.surelogic.sierra.gwt.server;
+
+import java.sql.Connection;
+
+import com.surelogic.sierra.gwt.SierraServiceServlet;
+import com.surelogic.sierra.gwt.client.SessionService;
+import com.surelogic.sierra.jdbc.server.ConnectionFactory;
+import com.surelogic.sierra.jdbc.server.Server;
+import com.surelogic.sierra.jdbc.server.ServerTransaction;
+import com.surelogic.sierra.jdbc.server.UserContext;
+import com.surelogic.sierra.jdbc.user.ServerUserManager;
+import com.surelogic.sierra.jdbc.user.User;
+
+public class SessionServiceImpl extends SierraServiceServlet implements
+		SessionService {
+	private static final long serialVersionUID = -6665683550116264375L;
+
+	public boolean isValidSession() {
+		return UserContext.peek() != null;
+	}
+
+	public String login(final String userName, final String password) {
+		if (userName != null && password != null) {
+			final User u = ConnectionFactory
+					.withReadOnly(new ServerTransaction<User>() {
+
+						public User perform(Connection conn, Server server)
+								throws Exception {
+							return ServerUserManager.getInstance(conn).login(
+									userName, password);
+						}
+					});
+			if (u != null) {
+				getThreadLocalRequest().getSession().setAttribute("SierraUser",
+						u);
+				UserContext.set(u);
+				return null;
+			} else {
+				return "Invalid username or password";
+			}
+		} else {
+			return "Missing username or password";
+		}
+	}
+
+}
