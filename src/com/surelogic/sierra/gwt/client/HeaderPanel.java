f4b34724a6bd6f3fd209bb6a3d92874888d1b815
Aaron Silinskas <aaron.silinskas@surelogic.com>
2008-02-07 16:20:16 +0000

    set up more of the header's user panel
    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/sierra-portal@5713 d02ae147-ee12-4a35-9452-91c89e8b2592

--- src/com/surelogic/sierra/gwt/client/HeaderPanel.java
+++ src/com/surelogic/sierra/gwt/client/HeaderPanel.java
@@ -1,12 +1,14 @@
 package com.surelogic.sierra.gwt.client;
 
 import com.google.gwt.core.client.GWT;
+import com.google.gwt.user.client.ui.ClickListener;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.DockPanel;
 import com.google.gwt.user.client.ui.HorizontalPanel;
 import com.google.gwt.user.client.ui.Image;
 import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.RootPanel;
+import com.google.gwt.user.client.ui.Widget;
 import com.surelogic.sierra.gwt.client.data.UserAccount;
 
 public class HeaderPanel extends Composite {
@@ -28,16 +30,47 @@ public class HeaderPanel extends Composite {
 				+ "images/header-sierra-logo.gif");
 		rootPanel.add(sierraLogo, DockPanel.WEST);
 
+		
 		rootPanel.add(userPanel, DockPanel.EAST);
+		rootPanel.setCellHorizontalAlignment(userPanel, DockPanel.ALIGN_RIGHT);
 	}
 
 	public void updateAccountPanel(UserAccount user) {
 		if (user != null) {
-			userPanel.add(new Label("Logged In: " + user.getUserName()));
-			// TODO show preferences, logout, admin, etc (user panel)
+			userPanel.add(createUserLabel("Logged in as " + user.getUserName(), null));
+			userPanel.add(createUserLabel("|", null));
+			userPanel.add(createUserLabel("Preferences", new PreferencesListener()));
+			userPanel.add(createUserLabel("|", null));
+			userPanel.add(createUserLabel("Log out", new LogoutListener()));
 		} else {
 			userPanel.clear();
-			// TODO hide user panel
 		}
+	}
+	
+	private Label createUserLabel(String text, ClickListener clickListener) {
+		final Label lbl = new Label(text);
+		lbl.addStyleName("header-user-label");
+		if (clickListener != null) {
+			lbl.addStyleName("header-user-clickable");
+			lbl.addClickListener(clickListener);
+		}
+		return lbl;
+	}
+	
+	private static class PreferencesListener implements ClickListener {
+
+		public void onClick(Widget sender) {
+			// TODO Auto-generated method stub
+			
+		}
+		
+	}
+	private static class LogoutListener implements ClickListener {
+
+		public void onClick(Widget sender) {
+			// TODO Auto-generated method stub
+			
+		}
+		
 	}
 }
