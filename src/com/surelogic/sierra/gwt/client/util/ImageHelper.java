e59e0c6d7d57bc7f0d0669948885b6408d978737
Aaron Silinskas <aaron.silinskas@surelogic.com>
2008-02-17 22:32:34 +0000

    fixed bug, 16x16 image was actually 14x14
    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/sierra-portal@5959 d02ae147-ee12-4a35-9452-91c89e8b2592

--- src/com/surelogic/sierra/gwt/client/util/ImageHelper.java
+++ src/com/surelogic/sierra/gwt/client/util/ImageHelper.java
@@ -9,9 +9,9 @@ public final class ImageHelper {
 
 	public static Image getWaitImage(int size) {
 		int adjustedSize;
-		if (size <= 16) {
-			adjustedSize = 16;
-		} else if (size > 16 && size <= 24) {
+		if (size <= 14) {
+			adjustedSize = 14;
+		} else if (size > 14 && size <= 24) {
 			adjustedSize = 24;
 		} else {
 			adjustedSize = 32;
