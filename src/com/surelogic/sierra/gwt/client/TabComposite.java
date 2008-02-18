65442e83443f1cacbffc3d441f817165ce44e7a3
Aaron Silinskas <aaron.silinskas@surelogic.com>
2008-02-18 06:19:07 +0000

    just added a TODO
    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/sierra-portal@5960 d02ae147-ee12-4a35-9452-91c89e8b2592

--- src/com/surelogic/sierra/gwt/client/TabComposite.java
+++ src/com/surelogic/sierra/gwt/client/TabComposite.java
@@ -3,6 +3,7 @@ package com.surelogic.sierra.gwt.client;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.DockPanel;
 
+// TODO convert so TabComposite has on* methods for UI creation, etc like ContentComposite
 public abstract class TabComposite extends Composite {
 	private final DockPanel rootPanel = new DockPanel();
 
