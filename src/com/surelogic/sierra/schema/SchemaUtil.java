d317252185350327d240c7a46308b0e19cc6dd32
Nathan Boy <nathan.boy@surelogic.com>
2008-02-22 15:38:00 +0000

    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/sierra-jdbc@6143 d02ae147-ee12-4a35-9452-91c89e8b2592

--- src/com/surelogic/sierra/schema/SchemaUtil.java
+++ src/com/surelogic/sierra/schema/SchemaUtil.java
@@ -11,26 +11,29 @@ import com.surelogic.sierra.jdbc.tool.FindingTypeManager;
 import com.surelogic.sierra.tool.message.*;
 
 public class SchemaUtil {
-  static void updateFindingTypes(Connection conn) throws SQLException {
-    FindingTypeManager ftMan = FindingTypeManager.getInstance(conn);
-    List<FindingTypes> types = new ArrayList<FindingTypes>(3);
+	static void updateFindingTypes(Connection conn) throws SQLException {
+		FindingTypeManager ftMan = FindingTypeManager.getInstance(conn);
+		List<FindingTypes> types = new ArrayList<FindingTypes>(3);
 
-    types.add(getFindingTypes("findbugs.xml"));
-    types.add(getFindingTypes("pmd.xml"));
-    types.add(getFindingTypes("cpd.xml"));
-    types.add(getFindingTypes("checkstyle.xml"));
-    ftMan.updateFindingTypes(types, 0);
-  }
-  
-  private static FindingTypes getFindingTypes(String file) {
-    InputStream in = Thread.currentThread().getContextClassLoader()
-    .getResourceAsStream("com/surelogic/sierra/tool/message/"+file);
-    return MessageWarehouse.getInstance().fetchFindingTypes(in);
-  }
-  
-  static void setupFilters(Connection c) throws SQLException {
-    final SettingsManager sMan = SettingsManager.getInstance(c);
-    sMan.writeGlobalSettingsUUID(new ArrayList<String>(SettingsManager
-        .getSureLogicDefaultFilterSet()));
-  }
+		types.add(getFindingTypes("findbugs.xml"));
+		types.add(getFindingTypes("pmd.xml"));
+		types.add(getFindingTypes("cpd.xml"));
+		types.add(getFindingTypes("checkstyle.xml"));
+		ftMan.updateFindingTypes(types, 0);
+	}
+
+	private static FindingTypes getFindingTypes(String file) {
+		InputStream in = Thread.currentThread().getContextClassLoader()
+				.getResourceAsStream(
+						"com/surelogic/sierra/tool/message/" + file);
+		return MessageWarehouse.getInstance().fetchFindingTypes(in);
+	}
+
+	static void setupFilters(Connection c) throws SQLException {
+		final SettingsManager sMan = SettingsManager.getInstance(c);
+		if (sMan.getGlobalSettings().isEmpty()) {
+			sMan.writeGlobalSettingsUUID(new ArrayList<String>(SettingsManager
+					.getSureLogicDefaultFilterSet()));
+		}
+	}
 }
