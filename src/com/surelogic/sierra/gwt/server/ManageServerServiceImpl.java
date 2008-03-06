17cdf43e9ab91c2b74959dd4b6036a2fce4da08e
Tim Halloran <tim.halloran@surelogic.com>
2008-03-06 15:15:53 +0000

    working
    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/sierra-portal-servlets@6568 d02ae147-ee12-4a35-9452-91c89e8b2592

--- src/com/surelogic/sierra/gwt/server/ManageServerServiceImpl.java
+++ src/com/surelogic/sierra/gwt/server/ManageServerServiceImpl.java
@@ -3,7 +3,7 @@ package com.surelogic.sierra.gwt.server;
 import java.sql.Connection;
 import java.sql.SQLException;
 
-import com.sun.imageio.plugins.common.I18N;
+import com.surelogic.common.i18n.I18N;
 import com.surelogic.common.jdbc.FutureDatabaseException;
 import com.surelogic.sierra.gwt.SierraServiceServlet;
 import com.surelogic.sierra.gwt.client.data.EmailInfo;
@@ -79,7 +79,7 @@ public class ManageServerServiceImpl extends SierraServiceServlet implements
 
 	private ServerInfo readServerInfo(Server server) {
 		ServerInfo info = new ServerInfo();
-		info.setProductVersion(I18N.getString("sierra.teamserver.version"));
+		info.setProductVersion(I18N.msg("sierra.teamserver.version"));
 		info.setAvailableVersion(server.getAvailableSchemaVersion());
 		try {
 			info.setCurrentVersion(server.getSchemaVersion());
