d071679925c46de7a726b5c5bb23030644ce5fad
Edwin Chan <edwin.chan@surelogic.com>
2008-03-06 20:04:37 +0000

    ASSIGNED - bug 1411: An internal error occurred during: "Loading scan document for LocationManager". https://fluid.surelogic.com/bugzilla/show_bug.cgi?id=1411
    Added code to handle **, *, ? in include/exclude patterns
    
    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/sierra-message@6597 d02ae147-ee12-4a35-9452-91c89e8b2592

--- src/com/surelogic/sierra/tool/targets/FilteredDirectoryTarget.java
+++ src/com/surelogic/sierra/tool/targets/FilteredDirectoryTarget.java
@@ -1,6 +1,9 @@
 package com.surelogic.sierra.tool.targets;
 
 import java.net.URI;
+import java.util.StringTokenizer;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import javax.xml.bind.annotation.XmlAccessOrder;
 import javax.xml.bind.annotation.XmlAccessorOrder;
@@ -21,20 +24,99 @@ public class FilteredDirectoryTarget extends DirectoryTarget {
     this.inclusions = inclusions;
     this.exclusions = exclusions;
   }
-
+  
+  private boolean initialized = false;
+  private IPattern[] includePatterns = null;
+  private IPattern[] excludePatterns = null;
+  
+  private synchronized void initPatterns() {
+	  if (initialized) {
+		  return;
+	  }
+	  initialized = true;
+	  if (inclusions != null) {
+		  includePatterns = createPatterns(inclusions);
+	  }
+      if (exclusions != null) {
+		  excludePatterns = createPatterns(exclusions);
+      }
+  }
+	  /*
+	          if (relativePath.startsWith(ex)) {
+	            break include; // now check exclusions
+	          }
+	        }      
+	        return true; // Not included
+	      }
+	          if (relativePath.startsWith(ex)) {
+	            return true;
+	          }
+	        }
+	      }  
+	      */
+  
+  private interface IPattern {
+	  boolean matches(String path);
+  }
+  
+  private static IPattern[] createPatterns(String[] patterns) {
+	  IPattern[] result = new IPattern[patterns.length];
+	  int i = 0;
+	  StringBuilder sb = new StringBuilder();
+	  for(final String p : patterns) {
+		  sb.setLength(0);
+		  if (p.startsWith("**/")) {
+			  sb.append(".*"); // Match any prefix
+			  addToPattern(sb, p.substring(2));
+			  sb.append('$');  // Match the end
+		  } else {
+			  sb.append('^');  // Match the beginning
+			  addToPattern(sb, p);
+			  sb.append(".*"); // Match any suffix
+		  }
+		  final Pattern regex = Pattern.compile(sb.toString());
+		  final Matcher match = regex.matcher("");
+		  result[i] = new IPattern() {
+			public boolean matches(String path) {
+				match.reset(path);
+				return match.matches();
+			}			  
+		  };
+		  i++;
+	  }
+	  return result;
+  }
+  
+  private static void addToPattern(StringBuilder sb, String p) {
+	  StringTokenizer st = new StringTokenizer(p, "?*", true);
+	  while (st.hasMoreTokens()) {
+		  String frag = st.nextToken();
+		  if (frag.equals("?")) {
+			  sb.append('.');
+		  }
+		  else if (frag.equals("*")) {
+			  sb.append("[^/]*");
+		  }
+		  else {
+			  sb.append(Pattern.quote(frag));
+		  }
+	  }
+  }
+  
   public boolean exclude(String relativePath) {
+	initPatterns();
    include:
-    if (inclusions != null) {
-      for(String ex : inclusions) {
-        if (relativePath.startsWith(ex)) {
+    if (includePatterns != null) {
+      for(IPattern p : excludePatterns) {
+        if (p.matches(relativePath)) {
           break include; // now check exclusions
         }
       }      
       return true; // Not included
     }
-    if (exclusions != null) {
-      for(String ex : exclusions) {
-        if (relativePath.startsWith(ex)) {
+    if (excludePatterns != null) {
+      for(IPattern p : excludePatterns) {
+        if (p.matches(relativePath)) {
           return true;
         }
       }
