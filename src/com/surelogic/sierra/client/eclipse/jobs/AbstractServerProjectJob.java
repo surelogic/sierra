93bfb2cef5a6c70fa00f0820ae6e28643b8fe325
Edwin Chan <edwin.chan@surelogic.com>
2008-03-14 15:35:05 +0000

    ASSIGNED - bug 1455: (SureLogic #51) Unable to synchronize the project util.concurrent to the Sierra team server Nathans machine is http://192.168.11.187:13376/ user="edwin" . https://fluid.surelogic.com/bugzilla/show_bug.cgi?id=1455
    Added code to put up an error dialog
    
    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/sierra-client-eclipse@6831 d02ae147-ee12-4a35-9452-91c89e8b2592

--- src/com/surelogic/sierra/client/eclipse/jobs/AbstractServerProjectJob.java
+++ src/com/surelogic/sierra/client/eclipse/jobs/AbstractServerProjectJob.java
@@ -1,10 +1,18 @@
 package com.surelogic.sierra.client.eclipse.jobs;
 
-import org.eclipse.core.runtime.IStatus;
+import java.util.logging.Level;
 
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.core.runtime.jobs.Job;
+
+import com.surelogic.common.eclipse.dialogs.ErrorDialogUtility;
 import com.surelogic.common.eclipse.jobs.DatabaseJob;
+import com.surelogic.common.eclipse.jobs.SLUIJob;
 import com.surelogic.common.eclipse.logging.SLStatus;
 import com.surelogic.common.i18n.I18N;
+import com.surelogic.common.logging.SLLogger;
 import com.surelogic.sierra.client.eclipse.actions.*;
 import com.surelogic.sierra.client.eclipse.model.SierraServer;
 import com.surelogic.sierra.tool.message.InvalidLoginException;
@@ -37,6 +45,20 @@ public abstract class AbstractServerProjectJob extends DatabaseJob {
   
   protected final IStatus createWarningStatus(final int errNo, Throwable t) {
     final String msg = I18N.err(errNo, f_projectName, f_server);
-    return SLStatus.createWarningStatus(errNo, msg, t);
+    final IStatus s = SLStatus.createWarningStatus(errNo, msg, t);
+    final String title = "Problem while "+getName();
+    SLLogger.log(Level.INFO, msg, t);
+    Job job = new SLUIJob() {
+		@Override
+		public IStatus runInUIThread(IProgressMonitor monitor) {
+			// Note this will be automatically logged by Eclipse
+			// when the original job returns 
+		    ErrorDialogUtility.open(null, title, s, false);
+			return Status.OK_STATUS;
+		} 
+    	
+    };
+    job.schedule();
+    return s;
   }
 }
