2763dafc695b69a38a56ec887828d349c27683ca
Nathan Boy <nathan.boy@surelogic.com>
2008-03-17 17:45:37 +0000

    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/sierra-portal-servlets@6856 d02ae147-ee12-4a35-9452-91c89e8b2592

---
+++ src/com/surelogic/sierra/gwt/server/FindingService.java
@@ -0,0 +1,19 @@
+package com.surelogic.sierra.gwt.server;
+
+import com.google.gwt.user.server.rpc.RemoteServiceServlet;
+import com.surelogic.sierra.gwt.client.data.FindingOverview;
+
+public class FindingService extends RemoteServiceServlet implements
+		com.surelogic.sierra.gwt.client.service.FindingService {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -5522046767503450943L;
+
+	public FindingOverview getFinding(String id) {
+		
+		return null;
+	}
+
+}
