7c81b2e06c8cc99a5ce238ad28a64a4b5045c289
Tim Halloran <tim.halloran@surelogic.com>
2008-03-17 13:44:23 +0000

    Removed unused import
    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/sierra-client-eclipse@6846 d02ae147-ee12-4a35-9452-91c89e8b2592

--- src/com/surelogic/sierra/client/eclipse/views/FindingDetailsMediator.java
+++ src/com/surelogic/sierra/client/eclipse/views/FindingDetailsMediator.java
@@ -8,7 +8,6 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.concurrent.atomic.AtomicLong;
 import java.util.logging.Level;
 
 import org.eclipse.core.runtime.IProgressMonitor;
