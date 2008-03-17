4e9c1d2ff977cf9292f764689931865991407162
Tim Halloran <tim.halloran@surelogic.com>
2008-03-17 18:11:20 +0000

    Better logging
    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/sierra-jdbc@6860 d02ae147-ee12-4a35-9452-91c89e8b2592

--- src/com/surelogic/sierra/server/lifecycle/LogServletContextListener.java
+++ src/com/surelogic/sierra/server/lifecycle/LogServletContextListener.java
@@ -1,6 +1,7 @@
 package com.surelogic.sierra.server.lifecycle;
 
 import java.io.File;
+import java.sql.Connection;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.logging.FileHandler;
@@ -12,10 +13,15 @@ import javax.servlet.ServletContextListener;
 import com.surelogic.common.FileUtility;
 import com.surelogic.common.i18n.I18N;
 import com.surelogic.common.logging.SLLogger;
+import com.surelogic.sierra.jdbc.server.ConnectionFactory;
+import com.surelogic.sierra.jdbc.server.Server;
+import com.surelogic.sierra.jdbc.server.ServerTransaction;
+import com.surelogic.sierra.schema.SierraSchemaUtility;
 
 /**
- * This servlet context listener configures SLLogger based upon the web.xml
- * context parameters on server startup.
+ * This servlet context listener gets the Sierra team server code ready to run.
+ * It sets up SLLogger based upon the web.xml context parameters on server
+ * startup. It also checks that the database schema is up to date with the code.
  * <p>
  * The parameter <tt>SLLogger</tt> may be set to <tt>Use Sierra Directory</tt>
  * to cause the logger to log under the ~/Sierra/Server directory. If
@@ -34,20 +40,19 @@ public class LogServletContextListener implements ServletContextListener {
 		 */
 		final String loggerOption = sce.getServletContext().getInitParameter(
 				"SLLogger");
+		final String contextName = sce.getServletContext()
+				.getServletContextName();
+		bootLogging(loggerOption, contextName);
+		bootDatabase();
+	}
+
+	private void bootLogging(String loggerOption, String contextName) {
 		if ("No File Output".equals(loggerOption))
 			return;
-		final SimpleDateFormat dateFormat = new SimpleDateFormat("-yyyy_MM_dd");
-		String servletContextName = sce.getServletContext()
-				.getServletContextName();
-		if (servletContextName.length() < 2) {
-			servletContextName = "root";
-		}
-		if (servletContextName.startsWith("/")) {
-			servletContextName = servletContextName.substring(1);
-		}
+		final SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy_MM_dd");
 
 		final String logFileName;
-		final String tail = File.separator + "log-" + servletContextName
+		final String tail = File.separator + "log-team-server-"
 				+ dateFormat.format(new Date()) + ".txt";
 		if ("Use Sierra Directory".equals(loggerOption)) {
 			final String serverDirectory = FileUtility.getSierraDataDirectory()
@@ -69,11 +74,28 @@ public class LogServletContextListener implements ServletContextListener {
 		final long totalMemoryMB = rt.totalMemory() / 1024L / 1024L;
 		final long freeMemoryMB = rt.freeMemory() / 1024L / 1024L;
 		SLLogger.getLogger().info(
-				sce.getServletContext().getServletContextName()
-						+ " logging initialized : Java runtime: maxMemory="
+				contextName + " logging to '" + logFileName
+						+ "' initialized : Java runtime: maxMemory="
 						+ maxMemoryMB + " MB; totalMemory=" + totalMemoryMB
 						+ " MB; freeMemory=" + freeMemoryMB
 						+ " MB; availableProcessors="
 						+ rt.availableProcessors());
+	}
+
+	private void bootDatabase() {
+		/*
+		 * Bootstrap or update up the database as necessary.
+		 */
+		ConnectionFactory.withTransaction(new ServerTransaction<Void>() {
+			public Void perform(Connection conn, Server server)
+					throws Exception {
+				SierraSchemaUtility.checkAndUpdate(conn, true);
+				return null;
+			}
+		});
+		SLLogger.log(Level.INFO, "Derby booted with derby.storage.pageSize="
+				+ System.getProperty("derby.storage.pageSize")
+				+ " and derby.storage.pageCacheSize="
+				+ System.getProperty("derby.storage.pageCacheSize") + ".");
 	}
 }
