34b89740889940ff13deee4fc62a1aec5beec7c9
Edwin Chan <edwin.chan@surelogic.com>
2008-03-18 15:03:36 +0000

    ASSIGNED - bug 1458: Resurrect Ant task https://fluid.surelogic.com/bugzilla/show_bug.cgi?id=1458
    Moved SierraScan to sierra-ant
    
    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/sierra-ant@6901 d02ae147-ee12-4a35-9452-91c89e8b2592

---
+++ src/com/surelogic/ant/sierra/SierraScan.java
@@ -0,0 +1,145 @@
+/*
+ * Created on Jan 17, 2008
+ */
+package com.surelogic.ant.sierra;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.tools.ant.BuildException;
+import org.apache.tools.ant.Project;
+import org.apache.tools.ant.taskdefs.Javac;
+import org.apache.tools.ant.taskdefs.compilers.CompilerAdapter;
+
+// import org.apache.tools.ant.util.GlobPatternMapper;
+// import org.apache.tools.ant.util.SourceFileScanner;
+
+public class SierraScan extends Javac {
+	/***************************************************************************
+	 * Ant Task Attributes
+	 **************************************************************************/
+
+	// Optional attribute, if present, we send the scan document will be sent to
+	// this server
+	private String server = null;
+
+	// Optional attribute, required when we send the scan document will be sent
+	// to this server.
+	private String user;
+	// Optional attribute, required when we send the scan document will be sent
+	// to this server.
+	private String password;
+
+	// Optional, but req'd if URL is set. Comma-separated list of timeseries.
+	private final List<String> timeseries = new ArrayList<String>();
+
+	@Override
+	protected void scanDir(File srcDir, File destDir, String[] files) {
+		/*
+		 * GlobPatternMapper m = new GlobPatternMapper(); m.setFrom("*.java");
+		 * m.setTo("*.class"); SourceFileScanner sfs = new
+		 * SourceFileScanner(this); File[] newFiles = sfs.restrictAsFiles(files,
+		 * srcDir, destDir, m);
+		 */
+		File[] newFiles = new File[files.length];
+		int i = 0;
+		for (String name : files) {
+			newFiles[i] = new File(srcDir, name);
+			i++;
+		}
+
+		if (newFiles.length > 0) {
+			File[] newCompileList = new File[compileList.length
+					+ newFiles.length];
+			System.arraycopy(compileList, 0, newCompileList, 0,
+					compileList.length);
+			System.arraycopy(newFiles, 0, newCompileList, compileList.length,
+					newFiles.length);
+			compileList = newCompileList;
+		}
+	}
+
+	/**
+	 * Modified from Javac.compile()
+	 */
+	@Override
+	protected void compile() {
+		File destDir = this.getDestdir();
+		System.out.println(destDir.getAbsolutePath());
+
+		if (compileList.length > 0) {
+			log("Scanning " + compileList.length + " source file"
+					+ (compileList.length == 1 ? "" : "s")
+			// + (destDir != null ? " to " + destDir : "")
+			);
+
+			if (listFiles) {
+				for (int i = 0; i < compileList.length; i++) {
+					String filename = compileList[i].getAbsolutePath();
+					log(filename);
+				}
+			}
+
+			CompilerAdapter adapter = new SierraJavacAdapter(this);
+
+			// now we need to populate the compiler adapter
+			adapter.setJavac(this);
+
+			// finally, lets execute the compiler!!
+			if (!adapter.execute()) {
+				if (failOnError) {
+					throw new BuildException("Failed", getLocation());
+				} else {
+					log("Failed", Project.MSG_ERR);
+				}
+			}
+		}
+	}
+
+	/***************************************************************************
+	 * Getters and Setters for attributes
+	 **************************************************************************/
+
+	public void setServer(String server) {
+		this.server = server;
+	}
+
+	public String getServer() {
+		return server;
+	}
+
+	public String getUser() {
+		return user;
+	}
+
+	public void setUser(String user) {
+		this.user = user;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	/**
+	 * @return the server timeseries.
+	 */
+	public final List<String> getTimeseries() {
+		return timeseries;
+	}
+
+	/**
+	 * @param timeseries
+	 *            the server timeseries to set.
+	 */
+	public final void setTimeseries(String timeseries) {
+		String[] strings = timeseries.split(",");
+		for (String timeseriesName : strings) {
+			this.timeseries.add(timeseriesName.trim());
+		}
+	}
+}
