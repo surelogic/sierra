030dd63817b780aa40a5000bf1f433421b4e3ff6
Nathan Boy <nathan.boy@surelogic.com>
2008-03-25 17:27:49 +0000

    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/sierra-portal-servlets@7016 d02ae147-ee12-4a35-9452-91c89e8b2592

--- src/com/surelogic/sierra/gwt/server/SettingsServiceImpl.java
+++ src/com/surelogic/sierra/gwt/server/SettingsServiceImpl.java
@@ -13,9 +13,9 @@ import com.surelogic.sierra.gwt.client.service.SettingsService;
 import com.surelogic.sierra.jdbc.server.ConnectionFactory;
 import com.surelogic.sierra.jdbc.server.Server;
 import com.surelogic.sierra.jdbc.server.UserTransaction;
-import com.surelogic.sierra.jdbc.settings.FilterEntryDetail;
-import com.surelogic.sierra.jdbc.settings.FilterSetDetail;
-import com.surelogic.sierra.jdbc.settings.ParentDetail;
+import com.surelogic.sierra.jdbc.settings.FilterEntryDO;
+import com.surelogic.sierra.jdbc.settings.FilterSetDO;
+import com.surelogic.sierra.jdbc.settings.ParentDO;
 import com.surelogic.sierra.jdbc.settings.SettingsManager;
 import com.surelogic.sierra.jdbc.user.User;
 import com.surelogic.sierra.tool.message.FindingType;
@@ -35,19 +35,19 @@ public class SettingsServiceImpl extends SierraServiceServlet implements
 					public List<FilterSet> perform(Connection conn,
 							Server server, User user) throws Exception {
 						final Map<String, FilterSet> sets = new HashMap<String, FilterSet>();
-						for (FilterSetDetail detail : SettingsManager
+						for (FilterSetDO detail : SettingsManager
 								.getInstance(conn).listFilterSets()) {
 							final FilterSet set = getOrCreateSet(detail
 									.getUid(), sets);
 							set.setName(detail.getName());
 							final List<FilterSet> parents = new ArrayList<FilterSet>();
-							for (ParentDetail parent : detail.getParents()) {
+							for (ParentDO parent : detail.getParents()) {
 								parents.add(getOrCreateSet(parent.getUid(),
 										sets));
 							}
 							set.setParents(parents);
 							final List<FilterEntry> filters = new ArrayList<FilterEntry>();
-							for (FilterEntryDetail fDetail : detail
+							for (FilterEntryDO fDetail : detail
 									.getFilters()) {
 								final FilterEntry filter = new FilterEntry();
 								filter.setFiltered(fDetail.isFiltered());
