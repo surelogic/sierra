134fc2ca7cf335c9d605c8f446ae160805116738
Nathan Boy <nathan.boy@surelogic.com>
2008-03-26 18:53:38 +0000

    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/sierra-portal-servlets@7046 d02ae147-ee12-4a35-9452-91c89e8b2592

--- src/com/surelogic/sierra/gwt/server/SettingsServiceImpl.java
+++ src/com/surelogic/sierra/gwt/server/SettingsServiceImpl.java
@@ -1,7 +1,9 @@
 package com.surelogic.sierra.gwt.server;
 
-import java.sql.Connection;
+import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -9,14 +11,17 @@ import java.util.Map;
 import com.surelogic.sierra.gwt.SierraServiceServlet;
 import com.surelogic.sierra.gwt.client.data.FilterEntry;
 import com.surelogic.sierra.gwt.client.data.FilterSet;
+import com.surelogic.sierra.gwt.client.data.Status;
 import com.surelogic.sierra.gwt.client.service.SettingsService;
+import com.surelogic.sierra.jdbc.Query;
 import com.surelogic.sierra.jdbc.server.ConnectionFactory;
 import com.surelogic.sierra.jdbc.server.Server;
-import com.surelogic.sierra.jdbc.server.UserTransaction;
+import com.surelogic.sierra.jdbc.server.TransactionException;
+import com.surelogic.sierra.jdbc.server.UserQuery;
 import com.surelogic.sierra.jdbc.settings.FilterEntryDO;
 import com.surelogic.sierra.jdbc.settings.FilterSetDO;
-import com.surelogic.sierra.jdbc.settings.ParentDO;
-import com.surelogic.sierra.jdbc.settings.SettingsManager;
+import com.surelogic.sierra.jdbc.settings.FilterSets;
+import com.surelogic.sierra.jdbc.tool.FindingTypeManager;
 import com.surelogic.sierra.jdbc.user.User;
 import com.surelogic.sierra.tool.message.FindingType;
 
@@ -30,37 +35,55 @@ public class SettingsServiceImpl extends SierraServiceServlet implements
 
 	public List<FilterSet> getFilterSets() {
 		return ConnectionFactory
-				.withUserReadOnly(new UserTransaction<List<FilterSet>>() {
+				.withUserReadOnly(new UserQuery<List<FilterSet>>() {
 
-					public List<FilterSet> perform(Connection conn,
-							Server server, User user) throws Exception {
+					public List<FilterSet> perform(Query q, Server server,
+							User user) {
 						final Map<String, FilterSet> sets = new HashMap<String, FilterSet>();
-						for (FilterSetDO detail : SettingsManager
-								.getInstance(conn).listFilterSets()) {
-							final FilterSet set = getOrCreateSet(detail
-									.getUid(), sets);
-							set.setName(detail.getName());
-							final List<FilterSet> parents = new ArrayList<FilterSet>();
-							for (ParentDO parent : detail.getParents()) {
-								parents.add(getOrCreateSet(parent.getUid(),
-										sets));
+						FindingTypeManager ftMan;
+						try {
+							ftMan = FindingTypeManager.getInstance(q
+									.getConnection());
+							final FilterSets fs = new FilterSets(q);
+							for (final FilterSetDO detail : fs.listFilterSets()) {
+								final FilterSet set = getOrCreateSet(detail
+										.getUid(), sets);
+								set.setName(detail.getName());
+								final List<FilterSet> parents = new ArrayList<FilterSet>();
+								for (final String parent : detail.getParents()) {
+									parents.add(getOrCreateSet(parent, sets));
+								}
+								set.setParents(parents);
+								final List<FilterEntry> filters = new ArrayList<FilterEntry>();
+								for (final FilterEntryDO fDetail : detail
+										.getFilters()) {
+									final FilterEntry filter = new FilterEntry();
+									filter.setFiltered(fDetail.isFiltered());
+									final FindingType type = ftMan
+											.getFindingType(fDetail
+													.getFindingType());
+									filter.setName(type.getName());
+									filter.setId(type.getId());
+									filter.setShortMessage(type
+											.getShortMessage());
+									filters.add(filter);
+								}
+								set.setEntries(filters);
 							}
-							set.setParents(parents);
-							final List<FilterEntry> filters = new ArrayList<FilterEntry>();
-							for (FilterEntryDO fDetail : detail
-									.getFilters()) {
-								final FilterEntry filter = new FilterEntry();
-								filter.setFiltered(fDetail.isFiltered());
-								final FindingType type = fDetail
-										.getFindingType();
-								filter.setName(type.getName());
-								filter.setId(type.getId());
-								filter.setShortMessage(type.getShortMessage());
-								filters.add(filter);
-							}
-							set.setEntries(filters);
+							final List<FilterSet> values = new ArrayList<FilterSet>(
+									sets.values());
+							Collections.sort(values,
+									new Comparator<FilterSet>() {
+										public int compare(FilterSet o1,
+												FilterSet o2) {
+											return o1.getName().compareTo(
+													o2.getName());
+										}
+									});
+							return values;
+						} catch (final SQLException e) {
+							throw new TransactionException(e);
 						}
-						return new ArrayList<FilterSet>(sets.values());
 					}
 				});
 	}
@@ -75,4 +98,28 @@ public class SettingsServiceImpl extends SierraServiceServlet implements
 		}
 		return set;
 	}
+
+	@SuppressWarnings("unchecked")
+	public Status createFilterSet(final String name, final List entries,
+			final List parents) {
+		return ConnectionFactory.withUserTransaction(new UserQuery<Status>() {
+			public Status perform(Query q, Server s, User u) {
+				final FilterSets sets = new FilterSets(q);
+				final long revision = s.nextRevision();
+				final FilterSetDO set = sets.createFilterSet(name, null,
+						revision);
+				set.getParents().addAll(parents);
+				final List doEntries = set.getFilters();
+				final List<FilterEntry> moEntries = entries;
+				for (final FilterEntry entry : moEntries) {
+					doEntries.add(new FilterEntryDO(entry.getId(), entry
+							.isFiltered()));
+				}
+				sets.updateFilterSet(set, revision);
+				return Status.success("Filter set " + name + " created.");
+			}
+		});
+
+	}
+
 }
