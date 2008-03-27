8a28554b662990604b68e44e19840823ddbc3206
Edwin Chan <edwin.chan@surelogic.com>
2008-03-27 17:16:34 +0000

    Changed to default to checking the server for updates
    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/sierra-client-eclipse@7064 d02ae147-ee12-4a35-9452-91c89e8b2592

--- src/com/surelogic/sierra/client/eclipse/preferences/PreferenceInitializer.java
+++ src/com/surelogic/sierra/client/eclipse/preferences/PreferenceInitializer.java
@@ -40,7 +40,7 @@ public class PreferenceInitializer extends AbstractPreferenceInitializer {
 		store.setDefault(PreferenceConstants.P_SERVER_STATUS_SORT,
 				ServerStatusSort.BY_SERVER.toString());
 		store.setDefault(PreferenceConstants.P_SERVER_INTERACTION_SETTING,
-				ServerInteractionSetting.NEVER.toString());
+				ServerInteractionSetting.CHECK.toString());
 		store.setDefault(
 				PreferenceConstants.P_SERVER_INTERACTION_PERIOD_IN_MINUTES, 5);
 		store.setDefault(
