e0c9c925a8b379b6cd89630c48e0d2b4bf392af5
Nathan Boy <nathan.boy@surelogic.com>
2008-04-01 14:23:32 +0000

    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/sierra-jdbc@7098 d02ae147-ee12-4a35-9452-91c89e8b2592

--- src/com/surelogic/sierra/schema/Schema_0025.java
+++ src/com/surelogic/sierra/schema/Schema_0025.java
@@ -2,12 +2,24 @@ package com.surelogic.sierra.schema;
 
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 import com.surelogic.common.jdbc.SchemaAction;
 import com.surelogic.sierra.findbugs1_3_3.FindBugs1_3_3ToolInfoGenerator;
+import com.surelogic.sierra.jdbc.JDBCUtils;
 
 public class Schema_0025 implements SchemaAction {
 	public void run(Connection conn) throws SQLException {
+		// Alter synch table client-side only
+		if (!JDBCUtils.isServer(conn)) {
+			final Statement st = conn.createStatement();
+			try {
+				st.execute("ALTER TABLE SYNCH ADD COLUMN COMMIT_COUNT INTEGER");
+				st.execute("ALTER TABLE SYNCH ADD COLUMN UPDATE_COUNT INTEGER");
+			} finally {
+				st.close();
+			}
+		}
 		FindBugs1_3_3ToolInfoGenerator.generateTool(conn);
 		SchemaUtil.updateFindingTypes(conn);
 		SchemaUtil.setupFilters(conn);
