aaa2b193264e4e9b7c2153896e20f14479a7c435
Nathan Boy <nathan.boy@surelogic.com>
2008-04-02 15:33:18 +0000

    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/sierra-portal-servlets@7129 d02ae147-ee12-4a35-9452-91c89e8b2592

--- src/com/surelogic/sierra/gwt/server/SettingsServiceImpl.java
+++ src/com/surelogic/sierra/gwt/server/SettingsServiceImpl.java
@@ -1,6 +1,5 @@
 package com.surelogic.sierra.gwt.server;
 
-import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
@@ -17,14 +16,13 @@ import com.surelogic.sierra.gwt.client.service.SettingsService;
 import com.surelogic.sierra.jdbc.Query;
 import com.surelogic.sierra.jdbc.server.ConnectionFactory;
 import com.surelogic.sierra.jdbc.server.Server;
-import com.surelogic.sierra.jdbc.server.TransactionException;
 import com.surelogic.sierra.jdbc.server.UserQuery;
 import com.surelogic.sierra.jdbc.settings.FilterEntryDO;
 import com.surelogic.sierra.jdbc.settings.FilterSetDO;
 import com.surelogic.sierra.jdbc.settings.FilterSets;
-import com.surelogic.sierra.jdbc.tool.FindingTypeManager;
+import com.surelogic.sierra.jdbc.tool.FindingTypeDO;
+import com.surelogic.sierra.jdbc.tool.FindingTypes;
 import com.surelogic.sierra.jdbc.user.User;
-import com.surelogic.sierra.tool.message.FindingType;
 
 public class SettingsServiceImpl extends SierraServiceServlet implements
 		SettingsService {
@@ -41,50 +39,40 @@ public class SettingsServiceImpl extends SierraServiceServlet implements
 					public List<FilterSet> perform(Query q, Server server,
 							User user) {
 						final Map<String, FilterSet> sets = new HashMap<String, FilterSet>();
-						FindingTypeManager ftMan;
-						try {
-							ftMan = FindingTypeManager.getInstance(q
-									.getConnection());
-							final FilterSets fs = new FilterSets(q);
-							for (final FilterSetDO detail : fs.listFilterSets()) {
-								final FilterSet set = getOrCreateSet(detail
-										.getUid(), sets);
-								set.setName(detail.getName());
-								final List<FilterSet> parents = new ArrayList<FilterSet>();
-								for (final String parent : detail.getParents()) {
-									parents.add(getOrCreateSet(parent, sets));
-								}
-								set.setParents(parents);
-								final List<FilterEntry> filters = new ArrayList<FilterEntry>();
-								for (final FilterEntryDO fDetail : detail
-										.getFilters()) {
-									final FilterEntry filter = new FilterEntry();
-									filter.setFiltered(fDetail.isFiltered());
-									final FindingType type = ftMan
-											.getFindingType(fDetail
-													.getFindingType());
-									filter.setName(type.getName());
-									filter.setId(type.getId());
-									filter.setShortMessage(type
-											.getShortMessage());
-									filters.add(filter);
-								}
-								set.setEntries(filters);
+						final FindingTypes types = new FindingTypes(q);
+						final FilterSets fs = new FilterSets(q);
+						for (final FilterSetDO detail : fs.listFilterSets()) {
+							final FilterSet set = getOrCreateSet(detail
+									.getUid(), sets);
+							set.setName(detail.getName());
+							final List<FilterSet> parents = new ArrayList<FilterSet>();
+							for (final String parent : detail.getParents()) {
+								parents.add(getOrCreateSet(parent, sets));
 							}
-							final List<FilterSet> values = new ArrayList<FilterSet>(
-									sets.values());
-							Collections.sort(values,
-									new Comparator<FilterSet>() {
-										public int compare(FilterSet o1,
-												FilterSet o2) {
-											return o1.getName().compareTo(
-													o2.getName());
-										}
-									});
-							return values;
-						} catch (final SQLException e) {
-							throw new TransactionException(e);
+							set.setParents(parents);
+							final List<FilterEntry> filters = new ArrayList<FilterEntry>();
+							for (final FilterEntryDO fDetail : detail
+									.getFilters()) {
+								final FilterEntry filter = new FilterEntry();
+								filter.setFiltered(fDetail.isFiltered());
+								final FindingTypeDO type = types
+										.getFindingType(fDetail
+												.getFindingType());
+								filter.setName(type.getName());
+								filter.setUid(type.getUid());
+								filter.setShortMessage(type.getShortMessage());
+								filters.add(filter);
+							}
+							set.setEntries(filters);
 						}
+						final List<FilterSet> values = new ArrayList<FilterSet>(
+								sets.values());
+						Collections.sort(values, new Comparator<FilterSet>() {
+							public int compare(FilterSet o1, FilterSet o2) {
+								return o1.getName().compareTo(o2.getName());
+							}
+						});
+						return values;
 					}
 				});
 	}
@@ -113,7 +101,7 @@ public class SettingsServiceImpl extends SierraServiceServlet implements
 				final Set<FilterEntryDO> doEntries = set.getFilters();
 				final List<FilterEntry> moEntries = entries;
 				for (final FilterEntry entry : moEntries) {
-					doEntries.add(new FilterEntryDO(entry.getId(), entry
+					doEntries.add(new FilterEntryDO(entry.getUid(), entry
 							.isFiltered()));
 				}
 				sets.updateFilterSet(set, revision);
