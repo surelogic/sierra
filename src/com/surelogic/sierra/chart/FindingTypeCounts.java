eb2f5ac5c598339682a9f5eb5de9e44927a415ee
Nathan Boy <nathan.boy@surelogic.com>
2008-04-02 20:34:19 +0000

    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/sierra-portal-servlets@7141 d02ae147-ee12-4a35-9452-91c89e8b2592

---
+++ src/com/surelogic/sierra/chart/FindingTypeCounts.java
@@ -0,0 +1,48 @@
+package com.surelogic.sierra.chart;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.Map;
+
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.data.category.DefaultCategoryDataset;
+
+import com.surelogic.sierra.jdbc.ConnectionQuery;
+import com.surelogic.sierra.jdbc.Result;
+import com.surelogic.sierra.jdbc.ResultHandler;
+import com.surelogic.sierra.jdbc.Row;
+
+public class FindingTypeCounts implements IDatabasePlot {
+
+	public JFreeChart plot(PlotSize mutableSize,
+			Map<String, String> parameters, Connection c) throws SQLException,
+			IOException {
+		c.setTransactionIsolation(Connection.TRANSACTION_READ_UNCOMMITTED);
+		final String uid = parameters.get("uid");
+		if (uid != null) {
+			final DefaultCategoryDataset set = new ConnectionQuery(c).prepared(
+					"Plots.FindingType.count",
+					new ResultHandler<DefaultCategoryDataset>() {
+
+						public DefaultCategoryDataset handle(Result r) {
+							DefaultCategoryDataset set = new DefaultCategoryDataset();
+							for (Row row : r) {
+								set.setValue(row.nextInt(), "Findings", row
+										.nextString());
+							}
+							return set;
+						}
+					}).call(uid);
+			final JFreeChart chart = ChartFactory.createBarChart("Frequency",
+					null, "# in the latest scan", set,
+					PlotOrientation.HORIZONTAL, true, false, false);
+			return chart;
+		}
+
+		return null;
+	}
+
+}
