01f89559d278a96a5ade1298c6fe76028c2f4aef
Aaron Silinskas <aaron.silinskas@surelogic.com>
2008-04-08 15:12:14 +0000

    the .equals method now compares object type
    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/sierra-portal@7245 d02ae147-ee12-4a35-9452-91c89e8b2592

--- src/com/surelogic/sierra/gwt/client/util/LangUtil.java
+++ src/com/surelogic/sierra/gwt/client/util/LangUtil.java
@@ -1,10 +1,25 @@
 package com.surelogic.sierra.gwt.client.util;
 
+import com.google.gwt.core.client.GWT;
+
 public final class LangUtil {
 
+	/**
+	 * Compares two objects to determine equality of type and value.
+	 * 
+	 * @param o1
+	 *            first object to compare
+	 * @param o2
+	 *            second object to compare
+	 * @return true if the objects are of the same type and equal in value
+	 */
 	public static boolean equals(Object o1, Object o2) {
 		if (o1 == o2) {
 			return true;
+		}
+		if (o1 != null && o2 != null
+				&& !GWT.getTypeName(o1).equals(GWT.getTypeName(o2))) {
+			return false;
 		}
 		return o1 != null && o1.equals(o2);
 	}
