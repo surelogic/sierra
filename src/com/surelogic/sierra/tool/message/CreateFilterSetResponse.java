ea2e3c591d7d101c732e09740b3ee7ddcb4a97a9
Nathan Boy <nathan.boy@surelogic.com>
2008-04-08 20:05:58 +0000

    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/sierra-message@7265 d02ae147-ee12-4a35-9452-91c89e8b2592

--- src/com/surelogic/sierra/tool/message/CreateFilterSetResponse.java
+++ src/com/surelogic/sierra/tool/message/CreateFilterSetResponse.java
@@ -1,8 +1,8 @@
 package com.surelogic.sierra.tool.message;
 
-import com.sun.xml.internal.txw2.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
 
-@XmlElement
+@XmlRootElement
 public class CreateFilterSetResponse {
 
 	protected FilterSet set;
