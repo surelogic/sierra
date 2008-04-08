2b0583bb4696cd3d7e9a2fa9e92a255502cfa74d
Nathan Boy <nathan.boy@surelogic.com>
2008-04-08 17:51:40 +0000

    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/sierra-portal@7252 d02ae147-ee12-4a35-9452-91c89e8b2592

--- src/com/surelogic/sierra/gwt/client/RulesContent.java
+++ src/com/surelogic/sierra/gwt/client/RulesContent.java
@@ -121,28 +121,23 @@ public class RulesContent extends ContentComposite {
 		clearSearch();
 		searchHeaderGrid.setWidget(1, 1, ImageHelper.getWaitImage(16));
 		final StringBuffer queryBuf = new StringBuffer();
-		queryBuf.append(".*");
 		for (int i = 0; i < text.length(); i++) {
 			final char ch = text.charAt(i);
 			if (Character.isLetterOrDigit(ch)) {
-				queryBuf.append("[");
 				queryBuf.append(Character.toLowerCase(ch));
-				queryBuf.append(Character.toUpperCase(ch));
-				queryBuf.append("]");
 			}
 		}
-		queryBuf.append(".*");
 		final String query = queryBuf.toString();
 		for (final Iterator it = categories.iterator(); it.hasNext();) {
 			final Category cat = (Category) it.next();
-			if (cat.getName().matches(query)) {
+			if (cat.getName().toLowerCase().indexOf(query) >= 0) {
 				addSearchCategory(cat);
 			} else if (!"".equals(text)) {
 				boolean categoryAdded = false;
 				for (final Iterator i = cat.getEntries().iterator(); i
 						.hasNext();) {
 					final FilterEntry e = (FilterEntry) i.next();
-					if (e.getName().matches(query)) {
+					if (e.getName().toLowerCase().indexOf(query) >= 0) {
 						if (!categoryAdded) {
 							addSearchCategory(cat);
 							categoryAdded = true;
