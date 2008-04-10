d0a1567b452bf1412c268a889407a4579fa424f7
Aaron Silinskas <aaron.silinskas@surelogic.com>
2008-04-10 20:45:35 +0000

    charts now display a wait image while loading
    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/sierra-portal@7345 d02ae147-ee12-4a35-9452-91c89e8b2592

--- src/com/surelogic/sierra/gwt/client/ui/Chart.java
+++ src/com/surelogic/sierra/gwt/client/ui/Chart.java
@@ -1,35 +1,69 @@
 package com.surelogic.sierra.gwt.client.ui;
 
 import com.google.gwt.user.client.ui.Composite;
-import com.google.gwt.user.client.ui.HTML;
+import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.LoadListener;
+import com.google.gwt.user.client.ui.VerticalPanel;
+import com.google.gwt.user.client.ui.Widget;
 import com.surelogic.sierra.gwt.client.data.ImageMapData;
 import com.surelogic.sierra.gwt.client.data.Ticket;
 import com.surelogic.sierra.gwt.client.service.Callback;
 import com.surelogic.sierra.gwt.client.service.ServiceHelper;
+import com.surelogic.sierra.gwt.client.util.ImageHelper;
 
 public class Chart extends Composite {
-
-	private final HTML html = new HTML();
+	private final VerticalPanel rootPanel = new VerticalPanel();
 
 	public Chart() {
-		initWidget(html);
+		initWidget(rootPanel);
+		rootPanel.add(ImageHelper.getWaitImage(32));
 	}
 
 	public void setChartTicket(final Ticket ticket) {
 		if (ticket == null) {
-			html.setHTML("No Chart Available");
+			rootPanel.clear();
+			rootPanel.add(new Label("No Chart Available"));
 		}
 		ServiceHelper.getTicketService().getImageMap(ticket, new Callback() {
 
 			protected void onFailure(String message, Object result) {
-				// TODO failure
+				loadFailed(message);
 			}
 
 			protected void onSuccess(String message, Object result) {
-				html.setHTML(((ImageMapData) result).getData()
-						+ "<img src=\"chart/png?ticket=" + ticket.getUUID()
-						+ "\" usemap=\"#map" + ticket.getUUID() + "\" />");
+				loadChart(ticket, (ImageMapData) result);
+			}
+
+		});
+	}
+
+	private void loadChart(final Ticket ticket, ImageMapData mapData) {
+		final String chartId = ticket.getUUID();
+		final String url = "chart/png?ticket=" + chartId;
+		final String map = "#map" + chartId;
+
+		final MappedImage chartImg = new MappedImage(url, map);
+		chartImg.addLoadListener(new LoadListener() {
+
+			public void onError(Widget sender) {
+				loadFailed(null);
+			}
+
+			public void onLoad(Widget sender) {
+				while (rootPanel.getWidgetCount() > 1) {
+					rootPanel.remove(1);
+				}
 			}
 		});
+
+		rootPanel.insert(chartImg, 0);
+	}
+
+	private void loadFailed(String message) {
+		rootPanel.clear();
+		rootPanel.add(new Label("Unable To Load Chart"));
+		if (message != null && !"".equals(message)) {
+			rootPanel.add(new Label(message));
+		}
 	}
 }
