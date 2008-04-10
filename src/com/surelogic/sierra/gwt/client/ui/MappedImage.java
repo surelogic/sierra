42733bb729df1615f4569cdbaf9a5438092d5531
Aaron Silinskas <aaron.silinskas@surelogic.com>
2008-04-10 20:45:04 +0000

    created to provide easy access to the usemap attribute of an image
    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/sierra-portal@7344 d02ae147-ee12-4a35-9452-91c89e8b2592

---
+++ src/com/surelogic/sierra/gwt/client/ui/MappedImage.java
@@ -0,0 +1,30 @@
+package com.surelogic.sierra.gwt.client.ui;
+
+import com.google.gwt.user.client.DOM;
+import com.google.gwt.user.client.ui.Image;
+
+public class MappedImage extends Image {
+
+	public MappedImage() {
+		super();
+	}
+
+	public MappedImage(String url, String map) {
+		super(url);
+		setMap(map);
+	}
+
+	public MappedImage(String url, String map, int left, int top, int width,
+			int height) {
+		super(url, left, top, width, height);
+		setMap(map);
+	}
+
+	public String getMap() {
+		return DOM.getElementAttribute(getElement(), "usemap");
+	}
+
+	public void setMap(String map) {
+		DOM.setElementAttribute(getElement(), "usemap", map);
+	}
+}
