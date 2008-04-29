8faf695fc4ea0dac906f947bf5818764bcea8916
Edwin Chan <edwin.chan@surelogic.com>
2008-04-29 14:41:33 +0000

    Updated to match IAdHoc
    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/sierra-client-eclipse@7626 d02ae147-ee12-4a35-9452-91c89e8b2592

--- src/com/surelogic/sierra/client/eclipse/AdHocGlue.java
+++ src/com/surelogic/sierra/client/eclipse/AdHocGlue.java
@@ -3,6 +3,9 @@ package com.surelogic.sierra.client.eclipse;
 import java.io.File;
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 import org.eclipse.core.runtime.IPath;
 
@@ -10,7 +13,13 @@ import com.surelogic.adhoc.AbstractAdHocGlue;
 import com.surelogic.sierra.client.eclipse.preferences.PreferenceConstants;
 
 public final class AdHocGlue extends AbstractAdHocGlue {
-
+	private static final ExecutorService exec = 
+        Executors.newSingleThreadExecutor();
+	
+	public Executor getExecutor() {
+		return exec;
+	}
+	
 	public Connection getConnection() throws SQLException {
 		return Data.getConnection();
 	}
