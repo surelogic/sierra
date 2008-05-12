5b5956ec47cb696812417952e83c7886b1903622
Edwin Chan <edwin.chan@surelogic.com>
2008-05-12 18:22:34 +0000

    Updated for FB 1.3.4 and PMD 4.2.1
    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/sierra-jdbc@7822 d02ae147-ee12-4a35-9452-91c89e8b2592

---
+++ src/com/surelogic/sierra/pmd4_2_1/PMD4_2_1ToolInfoGenerator.java
@@ -0,0 +1,46 @@
+package com.surelogic.sierra.pmd4_2_1;
+
+import java.sql.Connection;
+import java.util.StringTokenizer;
+import java.util.logging.Logger;
+
+import javax.xml.parsers.SAXParser;
+
+import com.surelogic.common.xml.XMLUtil;
+import com.surelogic.sierra.pmd.AbstractPMDToolInfoGenerator;
+
+/**
+ * Generates the tool and finding type information for pmd. The rulesets were
+ * taken from CVS HEAD on 06/18/2007.
+ * 
+ * @author nathan
+ * 
+ */
+public final class PMD4_2_1ToolInfoGenerator extends AbstractPMDToolInfoGenerator {
+	private static final Logger log = Logger
+			.getLogger(PMD4_2_1ToolInfoGenerator.class.getName());
+
+	/**
+	 * Load the rulesets and persist them to the embedded database.
+	 * 
+	 */
+	public static void generateTool(Connection conn) {
+		PMD4_2_1ToolInfoGenerator handler = new PMD4_2_1ToolInfoGenerator(conn);
+    SAXParser sp = XMLUtil.createSAXParser();
+    ClassLoader cl = Thread.currentThread().getContextClassLoader();
+    String rulesetFilenames = getRulesetNames(log, cl.getResourceAsStream(
+                                                   "com/surelogic/sierra/pmd4_2_1/rulesets/rulesets.properties"));
+
+		for (StringTokenizer st = new StringTokenizer(rulesetFilenames, ","); st
+				.hasMoreTokens();) {
+			String fileName = st.nextToken();
+			XMLUtil.parseResource(log, sp, 
+			                      cl.getResourceAsStream("com/surelogic/sierra/pmd4_2_1/" + fileName), 
+			                      handler, "Could not parse a PMD ruleset");
+		}
+	}
+
+	private PMD4_2_1ToolInfoGenerator(Connection conn) {
+    super(conn, "4.2.1");
+	}
+}
