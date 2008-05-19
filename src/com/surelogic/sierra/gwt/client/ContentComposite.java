27ea26f4a8105a7b6be800fcf9de4f2646750dad
Aaron Silinskas <aaron.silinskas@surelogic.com>
2008-05-19 16:56:56 +0000

    merged LifecycleComposite back into ContentComposite, will not be re-used
    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/sierra-portal@7962 d02ae147-ee12-4a35-9452-91c89e8b2592

--- src/com/surelogic/sierra/gwt/client/ContentComposite.java
+++ src/com/surelogic/sierra/gwt/client/ContentComposite.java
@@ -1,9 +1,12 @@
 package com.surelogic.sierra.gwt.client;
 
+import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.DockPanel;
 
-public abstract class ContentComposite extends LifecycleComposite {
+public abstract class ContentComposite extends Composite implements Lifecycle {
 	private final DockPanel rootPanel = new DockPanel();
+	private boolean initialized;
+	private boolean active;
 
 	public ContentComposite() {
 		super();
@@ -13,6 +16,29 @@ public abstract class ContentComposite extends LifecycleComposite {
 
 	public final void show() {
 		ContextManager.setContent(this);
+	}
+
+	public final void update(Context context) {
+		if (!initialized) {
+			initialized = true;
+			onInitialize();
+		}
+
+		onUpdate(context);
+		active = true;
+	}
+
+	public final void deactivate() {
+		active = false;
+		onDeactivate();
+	}
+
+	public boolean isInitialized() {
+		return initialized;
+	}
+
+	public boolean isActive() {
+		return active;
 	}
 
 	protected final void onInitialize() {
@@ -25,4 +51,7 @@ public abstract class ContentComposite extends LifecycleComposite {
 
 	protected abstract void onInitialize(DockPanel rootPanel);
 
+	protected abstract void onUpdate(Context context);
+
+	protected abstract void onDeactivate();
 }
