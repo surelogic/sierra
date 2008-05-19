eca5377d156092789eb2024aac2899c086ecbd95
Nathan Boy <nathan.boy@surelogic.com>
2008-05-19 21:09:53 +0000

    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/sierra-services@7972 d02ae147-ee12-4a35-9452-91c89e8b2592

--- src/com/surelogic/sierra/tool/message/ServerInfoServiceImpl.java
+++ src/com/surelogic/sierra/tool/message/ServerInfoServiceImpl.java
@@ -2,7 +2,6 @@ package com.surelogic.sierra.tool.message;
 
 import java.sql.Connection;
 import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.List;
 
 import javax.servlet.ServletConfig;
@@ -17,27 +16,18 @@ public class ServerInfoServiceImpl extends SRPCServlet implements
 		ServerInfoService {
 	private static final long serialVersionUID = 557394723869102797L;
 
-	private final List<Services> services = new ArrayList<Services>();
+	private final ServerInfoReply reply = new ServerInfoReply();
 
 	public ServerInfoReply getServerInfo(ServerInfoRequest request) {
-		final ServerInfoReply reply = new ServerInfoReply();
-		synchronized (services) {
-			reply.getServices().addAll(services);
+		synchronized (reply) {
+			return reply;
 		}
-		reply.setUid(ConnectionFactory
-				.withReadOnly(new ServerTransaction<String>() {
-
-					public String perform(Connection conn, Server server)
-							throws SQLException {
-						return server.getUid();
-					}
-				}));
-		return reply;
 	}
 
 	@Override
 	public void init(ServletConfig config) throws ServletException {
-		synchronized (services) {
+		synchronized (reply) {
+			final List<Services> services = reply.getServices();
 			if ("on".equals(config.getServletContext().getInitParameter(
 					"teamserver"))) {
 				services.add(Services.TEAMSERVER);
@@ -46,6 +36,14 @@ public class ServerInfoServiceImpl extends SRPCServlet implements
 					"buglink"))) {
 				services.add(Services.BUGLINK);
 			}
+			reply.setUid(ConnectionFactory
+					.withReadOnly(new ServerTransaction<String>() {
+
+						public String perform(Connection conn, Server server)
+								throws SQLException {
+							return server.getUid();
+						}
+					}));
 		}
 	}
 
