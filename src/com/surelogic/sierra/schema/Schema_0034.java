8d6172a35aad6547affa8a845700dca388a6e529
Edwin Chan <edwin.chan@surelogic.com>
2008-06-03 16:39:36 +0000

    Added artifact/finding types for JSure
    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/sierra-jdbc@8105 d02ae147-ee12-4a35-9452-91c89e8b2592

--- src/com/surelogic/sierra/schema/Schema_0034.java
+++ src/com/surelogic/sierra/schema/Schema_0034.java
@@ -10,8 +10,10 @@ public class Schema_0034 implements SchemaAction {
 
 	public void run(Connection conn) throws SQLException {
 		JSure0_9ToolInfoGenerator.generateTool(conn);
+		/*
 		SchemaUtil.updateFindingTypes(conn);
 		SchemaUtil.setupScanFilters(conn);
 		SchemaUtil.setupCategories(conn);
+		*/
 	}
 }
