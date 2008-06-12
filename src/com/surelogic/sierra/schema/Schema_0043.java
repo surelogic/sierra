1f601f17fe48af4de69b0bc1c0bcb6098df2bbec
Edwin Chan <edwin.chan@surelogic.com>
2008-06-12 21:01:36 +0000

    Added info for effects assurance
    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/sierra-jdbc@8265 d02ae147-ee12-4a35-9452-91c89e8b2592

--- src/com/surelogic/sierra/schema/Schema_0043.java
+++ src/com/surelogic/sierra/schema/Schema_0043.java
@@ -6,14 +6,12 @@ import java.sql.SQLException;
 import com.surelogic.common.jdbc.SchemaAction;
 import com.surelogic.sierra.jsure.*;
 
-public class Schema_0035 implements SchemaAction {
+public class Schema_0043 implements SchemaAction {
 
 	public void run(Connection conn) throws SQLException {
-		JSure1_0ToolInfoGenerator.generateTool(conn);
-		/*
+		JSure1_1ToolInfoGenerator.generateTool(conn);
 		SchemaUtil.updateFindingTypes(conn);
 		SchemaUtil.setupScanFilters(conn);
 		SchemaUtil.setupCategories(conn);
-		*/
 	}
 }
