36c54374f9135b74780b73baf3650e82147bc38d
Nathan Boy <nathan.boy@surelogic.com>
2008-06-12 19:53:13 +0000

    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/sierra-portal@8253 d02ae147-ee12-4a35-9452-91c89e8b2592

--- src/com/surelogic/sierra/gwt/client/service/TicketServiceAsync.java
+++ src/com/surelogic/sierra/gwt/client/service/TicketServiceAsync.java
@@ -4,6 +4,7 @@ import java.util.Map;
 
 import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.surelogic.sierra.gwt.client.data.ImageMapData;
+import com.surelogic.sierra.gwt.client.data.ReportTable;
 import com.surelogic.sierra.gwt.client.data.Result;
 import com.surelogic.sierra.gwt.client.data.Ticket;
 
@@ -12,4 +13,7 @@ public interface TicketServiceAsync {
 			AsyncCallback<Result<Ticket>> callback);
 
 	void getImageMap(Ticket ticket, AsyncCallback<Result<ImageMapData>> callback);
+
+	void getTableData(Ticket ticket, AsyncCallback<Result<ReportTable>> callback);
+
 }
