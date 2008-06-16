95b86d3e57ae722dfab1baf2ec500953eccd73ab
Aaron Silinskas <aaron.silinskas@surelogic.com>
2008-06-16 21:26:28 +0000

    worked on UI
    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/sierra-portal@8338 d02ae147-ee12-4a35-9452-91c89e8b2592

--- src/com/surelogic/sierra/gwt/client/content/findingtypes/FindingTypeView.java
+++ src/com/surelogic/sierra/gwt/client/content/findingtypes/FindingTypeView.java
@@ -1,36 +1,37 @@
 package com.surelogic.sierra.gwt.client.content.findingtypes;
 
 import com.google.gwt.user.client.ui.HTML;
+import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.VerticalPanel;
 import com.surelogic.sierra.gwt.client.data.FindingType;
 import com.surelogic.sierra.gwt.client.ui.BlockPanel;
 import com.surelogic.sierra.gwt.client.util.LangUtil;
 
 public class FindingTypeView extends BlockPanel {
-	private final HTML summary = new HTML();
 	private final HTML description = new HTML();
 	private final VerticalPanel chart = new VerticalPanel();
 
 	@Override
 	protected void onInitialize(VerticalPanel contentPanel) {
-		contentPanel.add(new HTML("<h3>Summary</h3>"));
-		contentPanel.add(summary);
-		contentPanel.add(new HTML("<h3>Description</h3>"));
+		description.addStyleName("padded");
 		contentPanel.add(description);
-		contentPanel.add(new HTML("<h3>Found In</h3>"));
+		contentPanel.add(new Label("Found In:"));
 		contentPanel.add(chart);
 	}
 
 	public void setSelection(FindingType findingType) {
 		if (findingType != null) {
 			setSummary(findingType.getName());
-
-			setText(summary, findingType.getShortMessage(),
-					"No summary information.");
-			setText(description, findingType.getInfo(), "No description.");
+			final String info = findingType.getInfo();
+			if (LangUtil.notEmpty(info)) {
+				description.setHTML(info);
+				description.removeStyleName("font-italic");
+			} else {
+				description.setHTML(info);
+				description.addStyleName("font-italic");
+			}
 		} else {
 			setSummary("Select a finding type");
-			summary.setText(null);
 			description.setText(null);
 		}
 
@@ -39,16 +40,6 @@ public class FindingTypeView extends BlockPanel {
 			// FIXME throws an exception right now
 			// chart.add(ChartBuilder.name("FindingTypeCounts").prop("uid",
 			// findingType.getUuid()).build());
-		}
-	}
-
-	private void setText(HTML textUI, String text, String emptyText) {
-		if (LangUtil.notEmpty(text)) {
-			description.setHTML(text);
-			description.removeStyleName("font-italic");
-		} else {
-			description.setHTML(emptyText);
-			description.addStyleName("font-italic");
 		}
 	}
 
