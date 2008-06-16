883b4dfdbe2025ef818e6362410c4e3e96f10f03
Aaron Silinskas <aaron.silinskas@surelogic.com>
2008-06-16 15:07:33 +0000

    added set and clear wait status
    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/sierra-portal@8327 d02ae147-ee12-4a35-9452-91c89e8b2592

--- src/com/surelogic/sierra/gwt/client/ui/ActionPanel.java
+++ src/com/surelogic/sierra/gwt/client/ui/ActionPanel.java
@@ -3,23 +3,43 @@ package com.surelogic.sierra.gwt.client.ui;
 import com.google.gwt.user.client.ui.ClickListener;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.HorizontalPanel;
+import com.google.gwt.user.client.ui.Image;
 import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.VerticalPanel;
+import com.surelogic.sierra.gwt.client.util.ImageHelper;
 
 public class ActionPanel extends Composite {
 	private static final String STYLE = "sl-ActionPanel";
-
-	private final HorizontalPanel rootPanel = new HorizontalPanel();
+	private final Image waitImage = ImageHelper.getWaitImage(16);
+	private final VerticalPanel rootPanel = new VerticalPanel();
+	private final HorizontalPanel actionPanel = new HorizontalPanel();
 
 	public ActionPanel() {
 		super();
 		initWidget(rootPanel);
 		rootPanel.addStyleName(STYLE);
+
+		rootPanel.add(actionPanel);
 	}
 
 	public void addAction(String text, ClickListener actionListener) {
 		final Label action = new Label(text);
 		action.addStyleName(STYLE + "-item");
 		action.addClickListener(actionListener);
-		rootPanel.add(action);
+		actionPanel.add(action);
+	}
+
+	public void setWaitStatus() {
+		rootPanel.remove(actionPanel);
+		if (rootPanel.getWidgetIndex(waitImage) == -1) {
+			rootPanel.add(waitImage);
+		}
+	}
+
+	public void clearWaitStatus() {
+		rootPanel.remove(waitImage);
+		if (rootPanel.getWidgetIndex(actionPanel) == -1) {
+			rootPanel.add(actionPanel);
+		}
 	}
 }
