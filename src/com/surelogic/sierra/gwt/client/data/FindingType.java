d9ec8808d8d9e8ad76ea473a0eb8d40b7424dd7a
Nathan Boy <nathan.boy@surelogic.com>
2008-06-18 14:52:20 +0000

    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/sierra-portal@8377 d02ae147-ee12-4a35-9452-91c89e8b2592

--- src/com/surelogic/sierra/gwt/client/data/FindingType.java
+++ src/com/surelogic/sierra/gwt/client/data/FindingType.java
@@ -12,13 +12,16 @@ public class FindingType implements Cacheable, Serializable {
 	private String shortMessage;
 	private String info;
 	private List<String> reportedBy;
+	private List<CategoryInfo> categoriesIncluding;
+	private List<CategoryInfo> categoriesExcluding;
+	private List<ScanFilterInfo> scanFiltersIncluding;
 
 	public String getUuid() {
 		return uuid;
 	}
 
 	public void setUuid(String uid) {
-		this.uuid = uid;
+		uuid = uid;
 	}
 
 	public String getName() {
@@ -50,6 +53,108 @@ public class FindingType implements Cacheable, Serializable {
 			reportedBy = new ArrayList<String>();
 		}
 		return reportedBy;
+	}
+
+	public List<CategoryInfo> getCategoriesIncluding() {
+		if (categoriesIncluding == null) {
+			categoriesIncluding = new ArrayList<CategoryInfo>();
+		}
+		return categoriesIncluding;
+	}
+
+	public List<CategoryInfo> getCategoriesExcluding() {
+		if (categoriesExcluding == null) {
+			categoriesExcluding = new ArrayList<CategoryInfo>();
+		}
+		return categoriesExcluding;
+	}
+
+	public List<ScanFilterInfo> getScanFiltersIncluding() {
+		if (scanFiltersIncluding == null) {
+			scanFiltersIncluding = new ArrayList<ScanFilterInfo>();
+		}
+		return scanFiltersIncluding;
+	}
+
+	public static class ScanFilterInfo implements Serializable {
+		/**
+		 * 
+		 */
+		private static final long serialVersionUID = -5289740701941214936L;
+		private String uuid;
+		private String name;
+
+		public ScanFilterInfo(String uuid, String name) {
+			this.uuid = uuid;
+			this.name = name;
+		}
+
+		public ScanFilterInfo() {
+
+		}
+
+		public String getUuid() {
+			return uuid;
+		}
+
+		public void setUuid(String uuid) {
+			this.uuid = uuid;
+		}
+
+		public String getName() {
+			return name;
+		}
+
+		public void setName(String name) {
+			this.name = name;
+		}
+
+	}
+
+	public static class CategoryInfo implements Serializable {
+
+		/**
+		 * 
+		 */
+		private static final long serialVersionUID = -9024044811648450240L;
+		private String uuid;
+		private String name;
+		private String description;
+
+		public CategoryInfo() {
+
+		}
+
+		public CategoryInfo(String uuid, String name, String description) {
+			this.uuid = uuid;
+			this.name = name;
+			this.description = description;
+		}
+
+		public String getUuid() {
+			return uuid;
+		}
+
+		public void setUuid(String uuid) {
+			this.uuid = uuid;
+		}
+
+		public String getName() {
+			return name;
+		}
+
+		public void setName(String name) {
+			this.name = name;
+		}
+
+		public String getDescription() {
+			return description;
+		}
+
+		public void setDescription(String description) {
+			this.description = description;
+		}
+
 	}
 
 }
