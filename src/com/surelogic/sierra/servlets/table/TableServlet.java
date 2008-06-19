1d8856f3724df0f54439a599900a4b0ef112da7c
Alex DeMarco <alex.demarco@surelogic.com>
2008-06-19 20:55:41 +0000

    made some changes in order to write the Content-length header (basically buffered the output before writing response)
    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/sierra-portal-servlets@8422 d02ae147-ee12-4a35-9452-91c89e8b2592

--- src/com/surelogic/sierra/servlets/table/TableServlet.java
+++ src/com/surelogic/sierra/servlets/table/TableServlet.java
@@ -12,9 +12,9 @@ import javax.servlet.http.HttpServletResponse;
 import com.surelogic.sierra.cache.Attendant;
 import com.surelogic.sierra.cache.TableCache;
 import com.surelogic.sierra.cache.Ticket;
-import com.surelogic.sierra.gwt.client.data.ColumnData;
 import com.surelogic.sierra.gwt.client.data.Report;
 import com.surelogic.sierra.gwt.client.data.ReportTable;
+import com.surelogic.sierra.gwt.client.data.ReportTable.ColumnData;
 import com.surelogic.sierra.servlets.ServletUtility;
 
 public class TableServlet extends HttpServlet {
@@ -22,9 +22,9 @@ public class TableServlet extends HttpServlet {
 	public static final String OUTPUT_FORMAT_XML = "xml";
 	public static final String OUTPUT_FORMAT_HTML = "html";
 	public static final String OUTPUT_FORMAT_JSON = "json";
-
-	private final String outputFormat = OUTPUT_FORMAT_XML;
-
+	
+	private String outputFormat = OUTPUT_FORMAT_XML;
+		
 	/**
 	 * 
 	 */
@@ -33,32 +33,40 @@ public class TableServlet extends HttpServlet {
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException {
-
-		final PrintWriter pw = resp.getWriter();
-
+		
+		final StringBuffer buffer = new StringBuffer(256);
+		 
 		try {
 			final Report report = ServletUtility
 					.launderRequestParametersAsReport(req);
 			final Ticket ticket = Attendant.getInstance().getTicket(report,
 					req.getSession());
-			final ReportTable r = TableCache.getInstance().getReportTable(
-					ticket);
-			if (OUTPUT_FORMAT_HTML.equals(outputFormat)) {
+			final ReportTable r = TableCache.getInstance().getReportTable(ticket);			
+			if(OUTPUT_FORMAT_HTML.equals(outputFormat)) {
 				resp.setContentType("text/html");
-				writeHtml(pw, r);
-			} else if (OUTPUT_FORMAT_JSON.equals(outputFormat)) {
-				resp.setContentType("text/plain");
-				writeJson(pw, r);
-			} else {
-				resp.setContentType("text/xml");
-				pw.write("<?xml version=\"1.0\" encoding=\"UTF-8\"?>");
-				writeXml(pw, r);
+				writeHtml(buffer, r);
 			}
-			pw.close();
-		} catch (final Exception e) {
+			else if(OUTPUT_FORMAT_JSON.equals(outputFormat)) {
+				resp.setContentType("text/plain");
+				writeJson(buffer, r);
+			}
+			else {
+				resp.setContentType("text/xml");
+				buffer.append("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n");
+				writeXml(buffer, r);
+			}
+
+			// I need to set this content length for some clients to work properly;
+			// an alternative is to say: Transfer-Encoding: chunked, but I don't want to do that
+			resp.setContentLength(buffer.length());
+
+			final PrintWriter out = new PrintWriter(resp.getOutputStream());
+			out.write(buffer.toString());
+			out.close();
+			
+		}
+		catch(Exception e) {
 			throw new ServletException(e);
-		} finally {
-			pw.close();
 		}
 	}
 
@@ -68,53 +76,56 @@ public class TableServlet extends HttpServlet {
 		doGet(req, resp);
 	}
 
-	private void writeXml(final PrintWriter pw, final ReportTable r)
-			throws Exception {
+	private void writeXml(final StringBuffer buffer, final ReportTable r) throws Exception {
 		final List<String> cols = r.getHeaders();
-		pw.write("<table>");
-		int i = 0;
+		buffer.append("<table>");		
+		int i=0;
 		for (final List<String> row : r.getData()) {
-			pw.append("<row id=\"row").append(Integer.toString(i++)).append(
-					"\">");
-			int j = 0;
+			buffer.append("<row id=\"row").append(Integer.toString(i++)).append("\">");
+			int j=0;
 			for (final String data : row) {
 				final String col = cols.get(j++).replaceAll("\\s", "");
-				pw.append("<").append(col).append(">").append(data)
-						.append("</").append(col).append(">");
+				buffer.append("<").append(col).append(">").append(data).append("</").append(col).append(">");
 			}
-			pw.write("</row>");
+			buffer.append("</row>");
 		}
-		pw.write("</table>");
+		buffer.append("</table>");
 	}
-
-	private void writeJson(final PrintWriter pw, final ReportTable r)
-			throws Exception {
+	
+	private void writeJson(final StringBuffer buffer, final ReportTable r) throws Exception {
 		throw new Exception("writeJson() is not supported yet");
 	}
-
-	private void writeHtml(final PrintWriter pw, final ReportTable r)
-			throws Exception {
-		pw.write("<table>");
-		pw.write("<tr>");
+	
+	private void writeHtml(final StringBuffer buffer, final ReportTable r) throws Exception {
+		buffer.append("<table>");
+		buffer.append("<tr>");
 		final List<ColumnData> cols = r.getColumns();
 		if (!cols.isEmpty()) {
-			pw.write("<colgroup>");
+			buffer.append("<colgroup>");
 			for (final ColumnData col : cols) {
-				pw.write("<col class=\"" + col.getCSS() + "\" />");
+				buffer.append("<col class=\"" + col.getCSS() + "\" />");
 			}
-			pw.write("</colgroup>");
+			buffer.append("</colgroup>");
 		}
 		for (final String header : r.getHeaders()) {
-			pw.write("<th>" + header + "</th>");
+			buffer.append("<th>" + header + "</th>");
 		}
-		pw.write("</tr>");
+		buffer.append("</tr>");
 		for (final List<String> row : r.getData()) {
-			pw.write("<tr>");
+			buffer.append("<tr>");
 			for (final String col : row) {
-				pw.write("<td>" + col + "</td>");
+				buffer.append("<td>" + col + "</td>");
 			}
-			pw.write("</tr>");
+			buffer.append("</tr>");
 		}
-		pw.write("</table>");
+		buffer.append("</table>");
+	}
+
+	public String getOutputFormat() {
+		return outputFormat;
+	}
+
+	public void setOutputFormat(String outputFormat) {
+		this.outputFormat = outputFormat;
 	}
 }
