af1785c3f8a28b0df1325a675410e71736844a92
Nathan Boy <nathan.boy@surelogic.com>
2008-06-19 21:37:10 +0000

    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/sierra-portal@8428 d02ae147-ee12-4a35-9452-91c89e8b2592

--- src/com/surelogic/sierra/gwt/client/content/projects/ProjectView.java
+++ src/com/surelogic/sierra/gwt/client/content/projects/ProjectView.java
@@ -1,15 +1,19 @@
 package com.surelogic.sierra.gwt.client.content.projects;
 
+import com.google.gwt.user.client.ui.HorizontalPanel;
 import com.google.gwt.user.client.ui.VerticalPanel;
 import com.surelogic.sierra.gwt.client.data.Project;
 import com.surelogic.sierra.gwt.client.ui.BlockPanel;
+import com.surelogic.sierra.gwt.client.util.ChartBuilder;
 
 public class ProjectView extends BlockPanel {
+
+	private final HorizontalPanel chart = new HorizontalPanel();
 
 	@Override
 	protected void onInitialize(VerticalPanel contentPanel) {
 		setTitle("Project");
-
+		contentPanel.add(chart);
 	}
 
 	public void setSelection(Project project) {
@@ -17,8 +21,12 @@ public class ProjectView extends BlockPanel {
 			setSummary("Select a project");
 		} else {
 			setSummary(project.getName());
+			chart.clear();
+			chart.add(ChartBuilder.name("ProjectFindingsChart").prop(
+					"projectName", project.getName()).build());
+			chart.add(ChartBuilder.name("ProjectCompilationsChart").prop(
+					"projectName", project.getName()).build());
 		}
 
 	}
-
 }
