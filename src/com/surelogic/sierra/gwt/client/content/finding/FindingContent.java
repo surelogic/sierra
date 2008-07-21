71c95d6dfebb97ae4f7a58e32b915de85c025f3b
Nathan Boy <nathan.boy@surelogic.com>
2008-07-21 20:00:53 +0000

    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/sierra-portal@8826 d02ae147-ee12-4a35-9452-91c89e8b2592

--- src/com/surelogic/sierra/gwt/client/content/finding/FindingContent.java
+++ src/com/surelogic/sierra/gwt/client/content/finding/FindingContent.java
@@ -5,6 +5,7 @@ import com.google.gwt.user.client.ui.HTML;
 import com.google.gwt.user.client.ui.VerticalPanel;
 import com.surelogic.sierra.gwt.client.Context;
 import com.surelogic.sierra.gwt.client.content.ContentComposite;
+import com.surelogic.sierra.gwt.client.data.ArtifactOverview;
 import com.surelogic.sierra.gwt.client.data.AuditOverview;
 import com.surelogic.sierra.gwt.client.data.FindingOverview;
 import com.surelogic.sierra.gwt.client.service.Callback;
@@ -16,15 +17,15 @@ public final class FindingContent extends ContentComposite {
 
 	private static final FindingContent instance = new FindingContent();
 
-	private HTML synopsis = new HTML();
+	private final HTML synopsis = new HTML();
 
-	private HTML location = new HTML();
+	private final HTML location = new HTML();
 
-	private HTML description = new HTML();
+	private final HTML description = new HTML();
 
-	private VerticalPanel audits = new VerticalPanel();
+	private final VerticalPanel audits = new VerticalPanel();
 
-	private VerticalPanel artifacts = new VerticalPanel();
+	private final VerticalPanel artifacts = new VerticalPanel();
 
 	public static FindingContent getInstance() {
 		return instance;
@@ -35,7 +36,7 @@ public final class FindingContent extends ContentComposite {
 	}
 
 	@Override
-	protected void onInitialize(DockPanel rootPanel) {
+	protected void onInitialize(final DockPanel rootPanel) {
 		final VerticalPanel panel = new VerticalPanel();
 		panel.add(UI.h3("Synopsis"));
 		panel.add(synopsis);
@@ -43,29 +44,31 @@ public final class FindingContent extends ContentComposite {
 		panel.add(location);
 		panel.add(UI.h3("Description"));
 		panel.add(description);
+		panel.add(UI.h3("Artifacts"));
+		panel.add(artifacts);
 		panel.add(UI.h3("Audits"));
 		panel.add(audits);
 		getRootPanel().add(panel, DockPanel.CENTER);
 	}
 
 	@Override
-	protected void onUpdate(Context context) {
+	protected void onUpdate(final Context context) {
 		final String findingType = context.getParameter(PARAM_FINDING);
-		if (findingType == null || findingType.length() == 0) {
+		if ((findingType == null) || (findingType.length() == 0)) {
 			setEmpty();
 		} else {
 			ServiceHelper.getFindingService().getFinding(findingType,
 					new Callback<FindingOverview>() {
 
 						@Override
-						protected void onFailure(String message,
-								FindingOverview result) {
+						protected void onFailure(final String message,
+								final FindingOverview result) {
 							setEmpty();
 						}
 
 						@Override
-						protected void onSuccess(String message,
-								FindingOverview result) {
+						protected void onSuccess(final String message,
+								final FindingOverview result) {
 							setFinding(result);
 						}
 
@@ -91,18 +94,30 @@ public final class FindingContent extends ContentComposite {
 		synopsis.setText("No Finding");
 	}
 
-	private void setFinding(FindingOverview f) {
+	private void setFinding(final FindingOverview f) {
 		clear();
 		String firstReported = null;
 		String firstReportedBy = null;
-		for (AuditOverview audit : f.getAudits()) {
-			if (firstReported == null) {
-				firstReported = audit.getTime();
-				firstReportedBy = audit.getUser();
+		if (f.getAudits().isEmpty()) {
+			audits.add(UI.p("No comments"));
+		} else {
+			for (final AuditOverview audit : f.getAudits()) {
+				if (firstReported == null) {
+					firstReported = audit.getTime();
+					firstReportedBy = audit.getUser();
+				}
+				audits.add(new HTML(audit.getTime()
+						+ " by <span class=\"user\">" + audit.getUser()
+						+ "</span>:<p class=\"audit\">" + audit.getText()
+						+ "</p>"));
 			}
-			audits.add(new HTML(audit.getTime() + " by <span class=\"user\">"
-					+ audit.getUser() + "</span>:<p class=\"audit\">"
-					+ audit.getText() + "</p>"));
+		}
+		for (final ArtifactOverview artifact : f.getArtifacts()) {
+			artifacts.add(new HTML(artifact.getTime()
+					+ " by <span class=\"artifact\">" + artifact.getTool()
+					+ " - " + artifact.getType()
+					+ "</span>:<p class=\"artifact\">" + artifact.getSummary()
+					+ "</p>"));
 		}
 		final String s = "This <em>"
 				+ f.getFindingType()
