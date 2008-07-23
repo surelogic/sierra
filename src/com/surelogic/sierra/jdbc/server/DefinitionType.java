3d99d695b526d2606069ed61febb3459d9dec391
Nathan Boy <nathan.boy@surelogic.com>
2008-07-23 15:54:18 +0000

    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/sierra-jdbc@8888 d02ae147-ee12-4a35-9452-91c89e8b2592

--- src/com/surelogic/sierra/jdbc/server/DefinitionType.java
+++
@@ -1,5 +0,0 @@
-package com.surelogic.sierra.jdbc.server;
-
-public enum DefinitionType {
-	FILTER_SET
-}
--- src/com/surelogic/sierra/jdbc/server/UserQuery.java
+++
@@ -1,8 +0,0 @@
-package com.surelogic.sierra.jdbc.server;
-
-import com.surelogic.common.jdbc.Query;
-import com.surelogic.sierra.jdbc.user.User;
-
-public interface UserQuery<T> {
-	T perform(Query query, Server server, User user);
-}
