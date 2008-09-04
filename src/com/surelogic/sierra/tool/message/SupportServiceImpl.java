5811bbf3c56c4480f54631f450bead52efa6480c
Aaron Silinskas <aaron.silinskas@surelogic.com>
2008-09-04 14:58:48 +0000

    added the required activation jar
    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/sierra-services@9468 d02ae147-ee12-4a35-9452-91c89e8b2592

--- src/com/surelogic/sierra/tool/message/SupportServiceImpl.java
+++ src/com/surelogic/sierra/tool/message/SupportServiceImpl.java
@@ -1,8 +1,6 @@
 package com.surelogic.sierra.tool.message;
 
-import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -11,9 +9,21 @@ import java.util.Map;
 import java.util.Properties;
 import java.util.logging.Level;
 
-import javax.mail.*;
-import javax.mail.internet.*;
-import javax.activation.*;
+import javax.activation.DataHandler;
+import javax.activation.DataSource;
+import javax.activation.FileDataSource;
+import javax.mail.Authenticator;
+import javax.mail.BodyPart;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Multipart;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
+import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
 
 import com.surelogic.common.logging.SLLogger;
 import com.surelogic.sierra.message.srpc.SRPCServlet;
@@ -23,18 +33,18 @@ public class SupportServiceImpl extends SRPCServlet implements SupportService {
 	static {
 		SLLogger.getLogger().warning("Starting SupportService");
 	}
-	
-	public SupportReply request(SupportRequest r, File... files) {
+
+	public SupportReply request(final SupportRequest r, final File... files) {
 		SLLogger.getLogger().warning("SupportService.request()");
 		if (r.getType() == null) {
 			return new SupportReply("Null type");
-		}				
+		}
 		switch (r.getType()) {
 		case REGISTER:
 		case UPDATE:
 		case USAGE:
 		case ERROR:
-			File props = writeProps(r.getPairs());
+			final File props = writeProps(r.getPairs());
 			notifyAdmin("Test", "Testing attachments", prepend(props, files));
 			break;
 		default:
@@ -43,18 +53,18 @@ public class SupportServiceImpl extends SRPCServlet implements SupportService {
 		return new SupportReply("OK");
 	}
 
-	private File writeProps(Map<String, String> pairs) {
+	private File writeProps(final Map<String, String> pairs) {
 		PrintWriter pw = null;
 		try {
-			File f        = File.createTempFile("Test", ".tmp");
-			FileWriter fw = new FileWriter(f);
-			pw = new PrintWriter(fw);		
-			for (Map.Entry<String, String> e : pairs.entrySet()) {
-				pw.println(e.getKey()+"="+e.getValue());
+			final File f = File.createTempFile("Test", ".tmp");
+			final FileWriter fw = new FileWriter(f);
+			pw = new PrintWriter(fw);
+			for (final Map.Entry<String, String> e : pairs.entrySet()) {
+				pw.println(e.getKey() + "=" + e.getValue());
 			}
 			pw.close();
-			return f;		
-		} catch (IOException e) {
+			return f;
+		} catch (final IOException e) {
 			if (pw != null) {
 				e.printStackTrace(pw);
 			} else {
@@ -64,7 +74,7 @@ public class SupportServiceImpl extends SRPCServlet implements SupportService {
 		return null;
 	}
 
-	private static File[] prepend(File f, File[] files) {
+	private static File[] prepend(final File f, final File[] files) {
 		if (f == null || !f.exists()) {
 			return files;
 		}
@@ -75,14 +85,15 @@ public class SupportServiceImpl extends SRPCServlet implements SupportService {
 		files[0] = f;
 		return files;
 	}
-	
-	private void notifyAdmin(final String subject, final String message, File... files) {
-		final String host      = "smtp.gmail.com"; 
-		final String port      = "587"; 
-		final String user      = "changedIn99@gmail.com"; 
-		final String pass      = "nocertainty"; 
-		final String from      = "changedIn99@gmail.com"; 
-		final String to        = "edwin.chan@surelogic.com"; 
+
+	private void notifyAdmin(final String subject, final String message,
+			final File... files) {
+		final String host = "smtp.gmail.com";
+		final String port = "587";
+		final String user = "changedIn99@gmail.com";
+		final String pass = "nocertainty";
+		final String from = "changedIn99@gmail.com";
+		final String to = "edwin.chan@surelogic.com";
 		final Properties props = new Properties();
 		props.setProperty("mail.transport.protocol", "smtp");
 		props.setProperty("mail.smtp.host", host);
@@ -105,53 +116,55 @@ public class SupportServiceImpl extends SRPCServlet implements SupportService {
 		final Session session = Session.getInstance(props, auth);
 		try {
 			final MimeMessage msg = new MimeMessage(session);
-			msg.setSender(new InternetAddress(((from == null) || (from
-					.length() == 0)) ? to : from));
-			msg.setRecipient(Message.RecipientType.TO,
-					new InternetAddress(to));
+			msg.setSender(new InternetAddress(
+					((from == null) || (from.length() == 0)) ? to : from));
+			msg.setRecipient(Message.RecipientType.TO, new InternetAddress(to));
 			msg.setSubject(subject);
 			msg.setSentDate(new Date());
-			
-		    Multipart multipart = new MimeMultipart();
-		    setupMessage(multipart, message);
 
-		    for(File f : files) {
-		    	addAttachment(multipart, f);
-		    }
+			final Multipart multipart = new MimeMultipart();
+			setupMessage(multipart, message);
 
-		    // Put parts in message
-		    msg.setContent(multipart);
+			for (final File f : files) {
+				addAttachment(multipart, f);
+			}
+
+			// Put parts in message
+			msg.setContent(multipart);
 			Transport.send(msg);
 		} catch (final MessagingException mex) {
-			log.log(Level.SEVERE,
-					"Mail notification of exception failed.", mex);
+			log
+					.log(Level.SEVERE,
+							"Mail notification of exception failed.", mex);
 		}
 	}
-	
-	private static void setupMessage(Multipart multipart, String message) throws MessagingException {
-		// Create the message part 
-	    BodyPart messageBodyPart = new MimeBodyPart();
 
-	    // Fill the message
-	    messageBodyPart.setText(message);
-		//msg.setContent(message, "text/plain");
-	    
-	    multipart.addBodyPart(messageBodyPart);
+	private static void setupMessage(final Multipart multipart,
+			final String message) throws MessagingException {
+		// Create the message part
+		final BodyPart messageBodyPart = new MimeBodyPart();
+
+		// Fill the message
+		messageBodyPart.setText(message);
+		// msg.setContent(message, "text/plain");
+
+		multipart.addBodyPart(messageBodyPart);
 	}
-	
-	private static void addAttachment(Multipart multipart, File file) throws MessagingException {
-		BodyPart messageBodyPart = new MimeBodyPart();
 
-	    // Get the attachment
-	    DataSource source = new FileDataSource(file);
+	private static void addAttachment(final Multipart multipart, final File file)
+			throws MessagingException {
+		final BodyPart messageBodyPart = new MimeBodyPart();
 
-	    // Set the data handler to the attachment
-	    messageBodyPart.setDataHandler(new DataHandler(source));
+		// Get the attachment
+		final DataSource source = new FileDataSource(file);
 
-	    // Set the filename
-	    // FIX modify to eliminate temp name?
-	    messageBodyPart.setFileName(file.getName());
+		// Set the data handler to the attachment
+		messageBodyPart.setDataHandler(new DataHandler(source));
 
-	    multipart.addBodyPart(messageBodyPart);
+		// Set the filename
+		// FIX modify to eliminate temp name?
+		messageBodyPart.setFileName(file.getName());
+
+		multipart.addBodyPart(messageBodyPart);
 	}
 }
