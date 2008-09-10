fd7f863dc693908038d70a8d613b49542e2b9336
Nathan Boy <nathan.boy@surelogic.com>
2008-09-10 18:48:52 +0000

    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/sierra-portal@9535 d02ae147-ee12-4a35-9452-91c89e8b2592

---
+++ src/com/surelogic/sierra/gwt/client/ui/ScanChoice.java
@@ -0,0 +1,29 @@
+package com.surelogic.sierra.gwt.client.ui;
+
+import java.util.List;
+
+import com.google.gwt.user.client.ui.ListBox;
+import com.surelogic.sierra.gwt.client.data.Scan;
+import com.surelogic.sierra.gwt.client.service.ServiceHelper;
+import com.surelogic.sierra.gwt.client.service.callback.StandardCallback;
+
+public class ScanChoice extends ListBox {
+
+	public ScanChoice() {
+		super(false);
+	}
+
+	public void displayProjectScans(final String project) {
+		ServiceHelper.getFindingService().getScans(project,
+				new StandardCallback<List<Scan>>() {
+					@Override
+					protected void doSuccess(final List<Scan> result) {
+
+						for (final Scan scan : result) {
+							addItem(scan.getScanTimeDisplay(), scan.getUuid());
+						}
+					}
+				});
+	}
+
+}
