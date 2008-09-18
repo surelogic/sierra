9f696dfbd1e464b14a4b0f8f7fd8d2cbed4d6796
Aaron Silinskas <aaron.silinskas@surelogic.com>
2008-09-18 15:17:42 +0000

    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/sierra-portal@9618 d02ae147-ee12-4a35-9452-91c89e8b2592

--- src/com/surelogic/sierra/gwt/client/content/overview/DashboardBlock.java
+++ src/com/surelogic/sierra/gwt/client/content/overview/DashboardBlock.java
@@ -1,14 +1,65 @@
 package com.surelogic.sierra.gwt.client.content.overview;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.google.gwt.user.client.ui.ClickListener;
+import com.google.gwt.user.client.ui.HorizontalPanel;
+import com.google.gwt.user.client.ui.Image;
 import com.google.gwt.user.client.ui.VerticalPanel;
+import com.google.gwt.user.client.ui.Widget;
+import com.surelogic.sierra.gwt.client.ui.Direction;
+import com.surelogic.sierra.gwt.client.ui.ImageHelper;
 import com.surelogic.sierra.gwt.client.ui.panel.BlockPanel;
 
 public class DashboardBlock extends BlockPanel {
+	private final HorizontalPanel movementActions = new HorizontalPanel();
+	private final List<DashboardListener> listeners = new ArrayList<DashboardListener>();
 
 	@Override
-	protected void onInitialize(VerticalPanel contentPanel) {
-		// TODO Auto-generated method stub
-
+	protected void onInitialize(final VerticalPanel contentPanel) {
+		movementActions.add(createArrowImage(Direction.UP));
+		movementActions.add(createArrowImage(Direction.DOWN));
+		movementActions.add(createArrowImage(Direction.LEFT));
+		movementActions.add(createArrowImage(Direction.RIGHT));
 	}
 
+	public void setEditMode(final boolean editing) {
+		removeActions();
+		if (editing) {
+			addAction(movementActions);
+			addAction("Remove", new ClickListener() {
+
+				public void onClick(final Widget sender) {
+					for (final DashboardListener listener : listeners) {
+						listener.onRemove(DashboardBlock.this);
+					}
+				}
+			});
+		}
+		for (final DashboardListener listener : listeners) {
+			listener.onEditingChange(this, editing);
+		}
+	}
+
+	private Image createArrowImage(final Direction direction) {
+		return ImageHelper.getArrowImage(direction, new ClickListener() {
+
+			public void onClick(final Widget sender) {
+				for (final DashboardListener listener : listeners) {
+					listener.onMove(DashboardBlock.this, direction);
+				}
+			}
+		});
+	}
+
+	public static interface DashboardListener {
+
+		void onMove(DashboardBlock block, Direction direction);
+
+		void onRemove(DashboardBlock block);
+
+		void onEditingChange(DashboardBlock block, boolean editing);
+
+	}
 }
