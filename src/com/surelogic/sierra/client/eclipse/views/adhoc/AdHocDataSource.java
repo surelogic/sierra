789fb99b9c93e72d2ae1c8c0fab69564e0e81257
Nathan Boy <nathan.boy@surelogic.com>
2008-10-14 15:22:25 +0000

    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/sierra-client-eclipse@10026 d02ae147-ee12-4a35-9452-91c89e8b2592

--- src/com/surelogic/sierra/client/eclipse/views/adhoc/AdHocDataSource.java
+++ src/com/surelogic/sierra/client/eclipse/views/adhoc/AdHocDataSource.java
@@ -2,14 +2,13 @@ package com.surelogic.sierra.client.eclipse.views.adhoc;
 
 import java.io.File;
 import java.net.URL;
-import java.sql.Connection;
-import java.sql.SQLException;
 import java.util.logging.Level;
 
 import org.eclipse.core.runtime.IPath;
 
 import com.surelogic.common.adhoc.IAdHocDataSource;
 import com.surelogic.common.i18n.I18N;
+import com.surelogic.common.jdbc.DBConnection;
 import com.surelogic.common.logging.SLLogger;
 import com.surelogic.sierra.client.eclipse.Activator;
 import com.surelogic.sierra.client.eclipse.Data;
@@ -17,8 +16,8 @@ import com.surelogic.sierra.client.eclipse.preferences.PreferenceConstants;
 
 public final class AdHocDataSource implements IAdHocDataSource {
 
-	public Connection getConnection() throws SQLException {
-		return Data.getInstance().getConnection();
+	public DBConnection getDB() {
+		return Data.getInstance();
 	}
 
 	public int getMaxRowsPerQuery() {
@@ -26,12 +25,12 @@ public final class AdHocDataSource implements IAdHocDataSource {
 	}
 
 	public File getQuerySaveFile() {
-		IPath pluginState = Activator.getDefault().getStateLocation();
+		final IPath pluginState = Activator.getDefault().getStateLocation();
 		return new File(pluginState.toOSString()
 				+ System.getProperty("file.separator") + "queries.xml");
 	}
 
-	public void badQuerySaveFileNotification(Exception e) {
+	public void badQuerySaveFileNotification(final Exception e) {
 		SLLogger.getLogger().log(Level.SEVERE,
 				I18N.err(4, getQuerySaveFile().getAbsolutePath()), e);
 	}
