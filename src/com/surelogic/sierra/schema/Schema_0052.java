9dfb72a2d78e4c49253dad98448e362285ce75ea
Edwin Chan <edwin.chan@surelogic.com>
2008-12-09 18:18:51 +0000

    Upgraded FB to v1.3.6
    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/sierra-jdbc@10668 d02ae147-ee12-4a35-9452-91c89e8b2592

--- src/com/surelogic/sierra/schema/Schema_0052.java
+++ src/com/surelogic/sierra/schema/Schema_0052.java
@@ -4,16 +4,14 @@ import java.sql.Connection;
 import java.sql.SQLException;
 
 import com.surelogic.common.jdbc.SchemaAction;
-import com.surelogic.sierra.jsure.*;
+import com.surelogic.sierra.findbugs1_3_6.FindBugs1_3_6ToolInfoGenerator;
 
-public class Schema_0043 implements SchemaAction {
+public class Schema_0052 implements SchemaAction {
 
 	public void run(Connection conn) throws SQLException {
-		JSure1_1ToolInfoGenerator.generateTool(conn);
-		/*
+		FindBugs1_3_6ToolInfoGenerator.generateTool(conn);
 		SchemaUtil.updateFindingTypes(conn);
 		SchemaUtil.setupScanFilters(conn);
 		SchemaUtil.setupCategories(conn);
-		*/
 	}
 }
