1cc709879fdf436b9566e6c35815c653369ab2b8
Edwin Chan <edwin.chan@surelogic.com>
2008-12-09 19:27:29 +0000

    Updated to use PMD 4.2.4
    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/sierra-jdbc@10672 d02ae147-ee12-4a35-9452-91c89e8b2592

--- src/com/surelogic/sierra/schema/Schema_0053.java
+++ src/com/surelogic/sierra/schema/Schema_0053.java
@@ -4,16 +4,14 @@ import java.sql.Connection;
 import java.sql.SQLException;
 
 import com.surelogic.common.jdbc.SchemaAction;
-import com.surelogic.sierra.findbugs1_3_6.FindBugs1_3_6ToolInfoGenerator;
+import com.surelogic.sierra.pmd4_2_4.PMD4_2_4ToolInfoGenerator;
 
-public class Schema_0052 implements SchemaAction {
+public class Schema_0053 implements SchemaAction {
 
 	public void run(Connection conn) throws SQLException {
-		FindBugs1_3_6ToolInfoGenerator.generateTool(conn);
-		/*
+		PMD4_2_4ToolInfoGenerator.generateTool(conn);
 		SchemaUtil.updateFindingTypes(conn);
 		SchemaUtil.setupScanFilters(conn);
 		SchemaUtil.setupCategories(conn);
-		*/
 	}
 }
