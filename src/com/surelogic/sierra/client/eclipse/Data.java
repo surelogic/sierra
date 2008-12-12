4bc2a5214122728780c6390c797ba3d568092192
Nathan Boy <nathan.boy@surelogic.com>
2008-12-12 16:32:17 +0000

    Schema Reloaded
    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/sierra-client-eclipse@10689 d02ae147-ee12-4a35-9452-91c89e8b2592

--- src/com/surelogic/sierra/client/eclipse/Data.java
+++ src/com/surelogic/sierra/client/eclipse/Data.java
@@ -42,7 +42,7 @@ public final class Data extends DerbyConnection {
 
 	@Override
 	protected String getSchemaName() {
-		return "SIERRA";
+		return "SIERRAV1";
 	}
 
 	public SchemaData getSchemaLoader() {
