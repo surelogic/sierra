f9d9a4422d7ddf0c9692066e90f37006dfcbe9c8
Nathan Boy <nathan.boy@surelogic.com>
2008-12-19 18:17:44 +0000

    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/sierra-message@10875 d02ae147-ee12-4a35-9452-91c89e8b2592

--- src/com/surelogic/sierra/tool/message/ServerIdentity.java
+++ src/com/surelogic/sierra/tool/message/ServerIdentity.java
@@ -2,4 +2,42 @@ package com.surelogic.sierra.tool.message;
 
 public class ServerIdentity {
 
+	private String server;
+	private String name;
+	private long revision;
+
+	public ServerIdentity() {
+	};
+
+	public ServerIdentity(final String server, final String name,
+			final long revision) {
+		this.server = server;
+		this.name = name;
+		this.revision = revision;
+	}
+
+	public String getServer() {
+		return server;
+	}
+
+	public void setServer(final String server) {
+		this.server = server;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(final String name) {
+		this.name = name;
+	}
+
+	public long getRevision() {
+		return revision;
+	}
+
+	public void setRevision(final long revision) {
+		this.revision = revision;
+	}
+
 }
