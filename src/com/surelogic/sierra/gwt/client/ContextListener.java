4ec9f19d0f7237e1d778a0f821e48f94ff594a66
Aaron Silinskas <aaron.silinskas@surelogic.com>
2008-12-30 21:34:38 +0000

    javadocs
    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/sierra-portal@10912 d02ae147-ee12-4a35-9452-91c89e8b2592

--- src/com/surelogic/sierra/gwt/client/ContextListener.java
+++ src/com/surelogic/sierra/gwt/client/ContextListener.java
@@ -1,7 +1,17 @@
 package com.surelogic.sierra.gwt.client;
 
+/**
+ * Listens for changes in the web client context (a.k.a. browser url).
+ * 
+ */
 public interface ContextListener {
 
+	/**
+	 * Called when the web client context has changed.
+	 * 
+	 * @param context
+	 *            the new context
+	 */
 	void onChange(Context context);
 
 }
--- src/com/surelogic/sierra/gwt/client/ContextManager.java
+++ src/com/surelogic/sierra/gwt/client/ContextManager.java
@@ -11,36 +11,104 @@ import com.surelogic.sierra.gwt.client.content.ContentComposite;
 import com.surelogic.sierra.gwt.client.data.Status;
 import com.surelogic.sierra.gwt.client.ui.dialog.MessageDialog;
 
+/**
+ * This singleton manages all interaction with the web client context. It
+ * handles browser navigation events, user interaction, and can optionally block
+ * navigation until the user confirms a prompt. Navigation blocking is generally
+ * used when in an "edit" mode that requires a save or cancel confirmation.
+ * 
+ * @see Context
+ * @see ContextListener
+ */
 public final class ContextManager {
+	/**
+	 * A list of context listeners that will be notified of context changes.
+	 */
 	private static List<ContextListener> contextListeners = new ArrayList<ContextListener>();
+
+	/**
+	 * If not null, this context will remain the current context until cleared
+	 * via {@link #unlockContext()} or user confirmation.
+	 */
 	private static Context lockedContext;
+
+	/**
+	 * The prompt to show the user if the current context is locked via
+	 * {@link #lockContext(String)}.
+	 */
 	private static String lockedMessage;
 
+	/**
+	 * Singleton
+	 */
 	private ContextManager() {
 		// Not instantiable
 	}
 
+	/**
+	 * Called immediately on GWT module loading to start browser history
+	 * monitoring.
+	 */
 	public static void initialize() {
-		History.addHistoryListener(new ContextHistoryListener());
+		History.addHistoryListener(new HistoryListener() {
+
+			public void onHistoryChanged(final String historyToken) {
+				notifyContextListeners();
+			}
+		});
 	}
 
+	/**
+	 * Returns true if the current context matches a given context.
+	 * 
+	 * @param context
+	 *            the context to compare
+	 * @return true if the context is current
+	 */
 	public static boolean isContext(final String context) {
 		final Context ctx = new Context(context);
 		return ctx.equals(getContext());
 	}
 
+	/**
+	 * Returns true if the current content matches a given content.
+	 * 
+	 * @param content
+	 *            the content to compare
+	 * @return true if the content is current
+	 */
 	public static boolean isContent(final ContentComposite content) {
 		return content == getContext().getContent();
 	}
 
+	/**
+	 * Returns the current web client context as a string.
+	 * 
+	 * @return the current context
+	 */
 	public static String getContextAsString() {
 		return History.getToken();
 	}
 
+	/**
+	 * Returns the current web client context.
+	 * 
+	 * @return the current context
+	 */
 	public static Context getContext() {
 		return new Context(getContextAsString());
 	}
 
+	/**
+	 * Attempts to update the current context to a new value. This will trigger
+	 * a notification to context listeners if the current context has not been
+	 * locked, or the user confirms that the lock should be released.
+	 * 
+	 * @param context
+	 *            the new context
+	 * @see #addContextListener(ContextListener)
+	 * @see #lockContext(String)
+	 */
 	public static void setContext(final Context context) {
 		final String token = context.toString();
 		// Note: newItem calls onHistoryChanged, which calls
@@ -52,14 +120,36 @@ public final class ContextManager {
 		}
 	}
 
+	/**
+	 * Forces a notification to all context listeners if the current context is
+	 * not locked, or if the user confirms that the lock should be released.
+	 * 
+	 * @see #addContextListener(ContextListener)
+	 * @see #lockContext(String)
+	 */
 	public static void refreshContext() {
 		notifyContextListeners();
 	}
 
+	/**
+	 * Registers a context listener to receive context change notifications.
+	 * 
+	 * @param listener
+	 *            the listener to notify of context changes.
+	 * @see #removeContextListener(ContextListener)
+	 */
 	public static void addContextListener(final ContextListener listener) {
 		contextListeners.add(listener);
 	}
 
+	/**
+	 * Removes a context listener so it will no longer be notified of context
+	 * changes.
+	 * 
+	 * @param listener
+	 *            the listener to remove
+	 * @see #addContextListener(ContextListener)
+	 */
 	public static void removeContextListener(final ContextListener listener) {
 		contextListeners.remove(listener);
 	}
@@ -89,21 +179,43 @@ public final class ContextManager {
 		lockedMessage = null;
 	}
 
+	/**
+	 * Notifies context listeners of a change or forced refresh of the current
+	 * web client context. If the current context is locked, this method will
+	 * prompt the user to confirm or cancel navigation away from the current
+	 * context. If the user confirms the navigation, the context will be
+	 * unlocked and update to the new value.
+	 * 
+	 * @see #lockContext(String)
+	 * @see #unlockContext()
+	 * @see #addContextListener(ContextListener)
+	 * @see #removeContextListener(ContextListener)
+	 */
 	private static void notifyContextListeners() {
 		final Context context = getContext();
+
+		// if lockedContext isn't null, the current context is locked
 		if (lockedContext != null) {
+			// only prompt the user if trying to change from the current context
 			if (!lockedContext.equals(context)) {
+				// build the prompt dialog
 				final MessageDialog dlg = new MessageDialog("Warning", null,
 						lockedMessage);
 				dlg.addPopupListener(new PopupListener() {
 
 					public void onPopupClosed(final PopupPanel sender,
 							final boolean autoClosed) {
+						// the prompt dialog has closed
 						final Status s = dlg.getStatus();
 						if (s != null && s.isSuccess()) {
+							// the user has confirmed the navigation away from
+							// the current context
+							// unlock the context and set the new value
 							unlockContext();
 							setContext(context);
 						} else {
+							// the user has cancelled the navigation, revert the
+							// current context back to the locked value
 							setContext(lockedContext);
 						}
 					}
@@ -115,14 +227,6 @@ public final class ContextManager {
 				listener.onChange(context);
 			}
 		}
-	}
-
-	private static class ContextHistoryListener implements HistoryListener {
-
-		public void onHistoryChanged(final String historyToken) {
-			notifyContextListeners();
-		}
-
 	}
 
 }
