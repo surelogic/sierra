054272b99e10002713861b94ae6fbf45108c776e
Aaron Silinskas <aaron.silinskas@surelogic.com>
2009-01-07 21:36:56 +0000

    javadocs
    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/sierra-portal@10973 d02ae147-ee12-4a35-9452-91c89e8b2592

--- src/com/surelogic/sierra/gwt/client/SessionManager.java
+++ src/com/surelogic/sierra/gwt/client/SessionManager.java
@@ -9,19 +9,59 @@ import com.surelogic.sierra.gwt.client.service.ServiceHelper;
 import com.surelogic.sierra.gwt.client.service.SessionServiceAsync;
 import com.surelogic.sierra.gwt.client.service.callback.ResultCallback;
 
+/**
+ * This singleton provides login and logout functionality, along with access to
+ * the current user. It also manages a list of session listeners, so
+ * notifications can be received when the session state is changed.
+ * 
+ * @see UserAccount
+ * @see SessionListener
+ */
 public final class SessionManager {
+	/**
+	 * The current user's account information.
+	 */
 	private static UserAccount userAccount;
+
+	/**
+	 * A list of listeners that will be notified when the session state changes.
+	 */
 	private static final List<SessionListener> sessionListeners = new ArrayList<SessionListener>();
 
+	/**
+	 * Returns the user information for the current session. This method can
+	 * will return null if a login has not been attempted, or has failed.
+	 * 
+	 * @return the current user information, or null
+	 */
 	public static UserAccount getUser() {
 		return userAccount;
 	}
 
+	/**
+	 * Returns true if a successful login operation has occurred.
+	 * 
+	 * @return true if the user is logged in.
+	 * @see #login(String, String)
+	 */
 	public static boolean isLoggedIn() {
 		return userAccount != null;
 	}
 
+	/**
+	 * Authenticates the given user and password with the server. When a result
+	 * is received, the session listeners are notified and the web site context
+	 * is refreshed.
+	 * 
+	 * @param username
+	 *            the user to authenticate
+	 * @param password
+	 *            the user's password to verify
+	 * @see SessionListener
+	 * @see ContextManager#refreshContext()
+	 */
 	public static void login(final String username, final String password) {
+		// make the login RPC call
 		final SessionServiceAsync sessionService = ServiceHelper
 				.getSessionService();
 		sessionService.login(username, password,
@@ -30,6 +70,7 @@ public final class SessionManager {
 					@Override
 					public void doFailure(final String message,
 							final UserAccount result) {
+						// login failed
 						userAccount = null;
 						for (final SessionListener listener : sessionListeners) {
 							listener.onLoginFailure(message);
@@ -40,6 +81,7 @@ public final class SessionManager {
 					@Override
 					public void doSuccess(final String message,
 							final UserAccount result) {
+						// login succeeded
 						userAccount = result;
 						for (final SessionListener listener : sessionListeners) {
 							listener.onLogin(userAccount);
@@ -49,6 +91,13 @@ public final class SessionManager {
 				});
 	}
 
+	/**
+	 * Updates the user account manually. This is mainly used when an already
+	 * authenticated user opens a new browser window.
+	 * 
+	 * @param user
+	 *            the user's account information
+	 */
 	public static void updateUser(final UserAccount user) {
 		userAccount = user;
 		for (final SessionListener listener : sessionListeners) {
@@ -57,12 +106,23 @@ public final class SessionManager {
 		ContextManager.refreshContext();
 	}
 
+	/**
+	 * Ends the current user session. This method attempts to notify the server
+	 * of the logout, and will refresh the web site context when a result is
+	 * received or the server can not be contacted.
+	 * 
+	 * @param errorMessage
+	 *            the optional error message to show on the login web page
+	 */
 	public static void logout(final String errorMessage) {
+		// make the logout RPC call
 		final SessionServiceAsync svc = ServiceHelper.getSessionService();
 		svc.logout(new ResultCallback<String>() {
 
 			@Override
 			protected void doException(final Throwable caught) {
+				// an exception occurred, just clear out the user info and
+				// notify listeners
 				final UserAccount oldUser = userAccount;
 				userAccount = null;
 				for (final SessionListener listener : sessionListeners) {
@@ -74,6 +134,7 @@ public final class SessionManager {
 
 			@Override
 			protected void doFailure(String message, final String result) {
+				// the logout operation failed? This really shouldn't happen.
 				if (errorMessage != null && !errorMessage.equals("")) {
 					message += " (" + errorMessage + ")";
 				}
@@ -85,6 +146,7 @@ public final class SessionManager {
 
 			@Override
 			protected void doSuccess(final String message, final String result) {
+				// logout was successful, notify listeners
 				final UserAccount oldUser = userAccount;
 				userAccount = null;
 				for (final SessionListener listener : sessionListeners) {
@@ -96,10 +158,25 @@ public final class SessionManager {
 		});
 	}
 
+	/**
+	 * Registers a session listener to receive session change notifications.
+	 * 
+	 * @param listener
+	 *            the listener to notify of session changes.
+	 * @see #removeSessionListener(SessionListener)
+	 */
 	public static void addSessionListener(final SessionListener listener) {
 		sessionListeners.add(listener);
 	}
 
+	/**
+	 * Removes a session listener so it will no longer be notified of session
+	 * changes.
+	 * 
+	 * @param listener
+	 *            the listener to remove
+	 * @see #addSessionListener(SessionListener)
+	 */
 	public static void removeSessionListener(final SessionListener listener) {
 		sessionListeners.remove(listener);
 	}
