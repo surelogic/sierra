7627a66d6926c0786039b0f26da97a1c06d0f6a0
Edwin Chan <edwin.chan@surelogic.com>
2009-01-12 21:57:19 +0000

    Added constant BY_SERVER_SETTINGS
    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/sierra-client-eclipse@10996 d02ae147-ee12-4a35-9452-91c89e8b2592

--- src/com/surelogic/sierra/client/eclipse/model/ServerSyncType.java
+++ src/com/surelogic/sierra/client/eclipse/model/ServerSyncType.java
@@ -1,13 +1,23 @@
 package com.surelogic.sierra.client.eclipse.model;
 
 public enum ServerSyncType {
-  BUGLINK, ALL;
+  BUGLINK, BY_SERVER_SETTINGS, ALL;
   
   public boolean syncBugLink() {
 	  return true;
   }
   
+  /**
+   * @return if supposed to sync project (ScanLink) data
+   */
   public boolean syncProjects() {
-	  return this.equals(ALL);
+	  return !this.equals(BUGLINK);
+  }
+  
+  /**  
+   * @return if supposed to synchronize, based on server settings
+   */
+  public boolean syncByServerSettings() {
+	  return this.equals(BY_SERVER_SETTINGS);
   }
 }
