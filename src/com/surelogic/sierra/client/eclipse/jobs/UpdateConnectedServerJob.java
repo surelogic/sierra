263363ce3bdd27b1d7e2ece4fad6847285497aa0
Nathan Boy <nathan.boy@surelogic.com>
2009-01-20 14:55:34 +0000

    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/sierra-client-eclipse@11144 d02ae147-ee12-4a35-9452-91c89e8b2592

--- src/com/surelogic/sierra/client/eclipse/jobs/UpdateConnectedServerJob.java
+++ src/com/surelogic/sierra/client/eclipse/jobs/UpdateConnectedServerJob.java
@@ -2,23 +2,34 @@ package com.surelogic.sierra.client.eclipse.jobs;
 
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
 
+import com.surelogic.sierra.client.eclipse.Data;
 import com.surelogic.sierra.client.eclipse.preferences.ServerFailureReport;
 import com.surelogic.sierra.jdbc.settings.ConnectedServer;
+import com.surelogic.sierra.jdbc.settings.SettingQueries;
 
 public class UpdateConnectedServerJob extends AbstractServerProjectJob {
 
-	public UpdateConnectedServerJob(final ServerProjectGroupJob family,
-			final String name, final ConnectedServer server,
-			final String project, final ServerFailureReport method) {
-		super(family, name, server, project, method);
+	private final boolean f_savePassword;
 
+	public UpdateConnectedServerJob(final ConnectedServer server,
+			final boolean savePassword, final ServerFailureReport method) {
+		super(null, "Updating connection parameters to " + server.getName(),
+				server, null, method);
+		f_savePassword = savePassword;
 	}
 
 	@Override
 	protected IStatus run(final IProgressMonitor monitor) {
-		// TODO Auto-generated method stub
-		return null;
+		try {
+			Data.getInstance().withTransaction(
+					SettingQueries.updateServerLocation(getServer(),
+							f_savePassword));
+			return Status.OK_STATUS;
+		} catch (final Exception e) {
+			return createErrorStatus(51, e);
+		}
 	}
 
 }
