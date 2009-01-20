52ce118b29c313ca8521f108e16df27a02c985bf
Tim Halloran <tim.halloran@surelogic.com>
2009-01-20 18:44:59 +0000

    Updated JavaDoc
    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/sierra-client-eclipse@11157 d02ae147-ee12-4a35-9452-91c89e8b2592

--- src/com/surelogic/sierra/client/eclipse/model/IBuglinkDataObserver.java
+++ src/com/surelogic/sierra/client/eclipse/model/IBuglinkDataObserver.java
@@ -1,11 +1,13 @@
 package com.surelogic.sierra.client.eclipse.model;
 
+/**
+ * Observes changes to BugLink data in the client.
+ */
 public interface IBuglinkDataObserver {
 	/**
-	 * Notifies an observer that Buglink data has been modified in the
-	 * database.
+	 * Notifies an observer that BugLink data has been modified in the database.
 	 * <p>
-	 * Note that this call is <b>not</b> be made from a UI thread.
+	 * Note that this call may not be made from a UI thread.
 	 */
 	void notify(BuglinkData bd);
 }
