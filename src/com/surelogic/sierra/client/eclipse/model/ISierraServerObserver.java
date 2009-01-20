d76a43f317b8a3e18d377fd31fdf80a7f2d30ea3
Tim Halloran <tim.halloran@surelogic.com>
2009-01-20 18:44:01 +0000

    Updated JavaDoc
    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/sierra-client-eclipse@11156 d02ae147-ee12-4a35-9452-91c89e8b2592

--- src/com/surelogic/sierra/client/eclipse/model/ISierraServerObserver.java
+++ src/com/surelogic/sierra/client/eclipse/model/ISierraServerObserver.java
@@ -1,7 +1,17 @@
 package com.surelogic.sierra.client.eclipse.model;
 
+/**
+ * Observes changes to sierra server connections in the client.
+ */
 public interface ISierraServerObserver {
 
+	/**
+	 * Clients of this method should never assume they are in a particular
+	 * thread.
+	 * 
+	 * @param manager
+	 *            the server manager.
+	 */
 	void notify(ConnectedServerManager manager);
 
 }
