326777474bd69a7dbd054391e60c99620d2f82bd
Nathan Boy <nathan.boy@surelogic.com>
2009-02-02 18:39:41 +0000

    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/sierra-message@11300 d02ae147-ee12-4a35-9452-91c89e8b2592

--- src/com/surelogic/sierra/tool/message/Audit.java
+++ src/com/surelogic/sierra/tool/message/Audit.java
@@ -6,78 +6,89 @@ import javax.xml.bind.annotation.XmlAccessOrder;
 import javax.xml.bind.annotation.XmlAccessorOrder;
 import javax.xml.bind.annotation.XmlType;
 
-
 @XmlAccessorOrder(XmlAccessOrder.ALPHABETICAL)
 @XmlType
 public class Audit {
-    private Date timestamp;
-    private String value;
-    private AuditEvent event;
-    private String user;
-    private Long revision;
+	private String uuid;
+	private Date timestamp;
+	private String value;
+	private AuditEvent event;
+	private String user;
+	private Long revision;
 
-    public Audit() {
-        // Do nothing
-    }
+	public Audit() {
+		// Do nothing
+	}
 
-    public Audit(Date timestamp, AuditEvent event, String value) {
-        this.timestamp = timestamp;
-        this.value = value;
-        this.event = event;
-    }
+	public Audit(final String uuid, final Date timestamp,
+			final AuditEvent event, final String value) {
+		this.uuid = uuid;
+		this.timestamp = timestamp;
+		this.value = value;
+		this.event = event;
+	}
 
-    public Audit(String user, Date timestamp, AuditEvent event, String value,
-        Long revision) {
-        this.user = user;
-        this.timestamp = timestamp;
-        this.value = value;
-        this.event = event;
-        this.revision = revision;
-    }
+	public Audit(final String uuid, final String user, final Date timestamp,
+			final AuditEvent event, final String value, final Long revision) {
+		this.uuid = uuid;
+		this.user = user;
+		this.timestamp = timestamp;
+		this.value = value;
+		this.event = event;
+		this.revision = revision;
+	}
 
-    public Date getTimestamp() {
-        return timestamp;
-    }
+	public String getUuid() {
+		return uuid;
+	}
 
-    public void setTimestamp(Date timestamp) {
-        this.timestamp = timestamp;
-    }
+	public void setUuid(final String uuid) {
+		this.uuid = uuid;
+	}
 
-    public String getValue() {
-        return value;
-    }
+	public Date getTimestamp() {
+		return timestamp;
+	}
 
-    public void setValue(String comment) {
-        this.value = comment;
-    }
+	public void setTimestamp(final Date timestamp) {
+		this.timestamp = timestamp;
+	}
 
-    public AuditEvent getEvent() {
-        return event;
-    }
+	public String getValue() {
+		return value;
+	}
 
-    public void setEvent(AuditEvent event) {
-        this.event = event;
-    }
+	public void setValue(final String comment) {
+		this.value = comment;
+	}
 
-    public String getUser() {
-        return user;
-    }
+	public AuditEvent getEvent() {
+		return event;
+	}
 
-    public void setUser(String user) {
-        this.user = user;
-    }
+	public void setEvent(final AuditEvent event) {
+		this.event = event;
+	}
 
-    public Long getRevision() {
-        return revision;
-    }
+	public String getUser() {
+		return user;
+	}
 
-    public void setRevision(Long revision) {
-        this.revision = revision;
-    }
+	public void setUser(final String user) {
+		this.user = user;
+	}
 
-    @Override
-    public String toString() {
-        return "Audit(" + event + ", " + timestamp + ", " + value + ", " +
-        user + ")";
-    }
+	public Long getRevision() {
+		return revision;
+	}
+
+	public void setRevision(final Long revision) {
+		this.revision = revision;
+	}
+
+	@Override
+	public String toString() {
+		return "Audit(" + event + ", " + timestamp + ", " + value + ", " + user
+				+ ")";
+	}
 }
