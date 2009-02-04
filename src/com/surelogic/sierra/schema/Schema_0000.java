3b04c600aad4a508b4a6627b694282dc9110dd03
Nathan Boy <nathan.boy@surelogic.com>
2009-02-04 21:39:44 +0000

    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/sierra-jdbc@11333 d02ae147-ee12-4a35-9452-91c89e8b2592

--- src/com/surelogic/sierra/schema/Schema_0000.java
+++ src/com/surelogic/sierra/schema/Schema_0000.java
@@ -6,14 +6,12 @@ import java.sql.SQLException;
 import com.surelogic.common.jdbc.SchemaAction;
 import com.surelogic.sierra.cpd.CPD4_1ToolInfoGenerator;
 import com.surelogic.sierra.findbugs1_3_7.FindBugs1_3_7ToolInfoGenerator;
-import com.surelogic.sierra.jsure.JSure1_1ToolInfoGenerator;
 import com.surelogic.sierra.pmd4_2_4.PMD4_2_4ToolInfoGenerator;
 
 public class Schema_0000 implements SchemaAction {
 
 	public void run(final Connection conn) throws SQLException {
 		CPD4_1ToolInfoGenerator.generateTool(conn);
-		JSure1_1ToolInfoGenerator.generateTool(conn);
 		FindBugs1_3_7ToolInfoGenerator.generateTool(conn);
 		PMD4_2_4ToolInfoGenerator.generateTool(conn);
 		SchemaUtil.updateFindingTypes(conn);
