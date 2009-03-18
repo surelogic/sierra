251986d8bcb1b929a3e7218c9fa9114c682adb29
Edwin Chan <edwin.chan@surelogic.com>
2009-03-18 21:20:37 +0000

    Added code to handle PMD plugin jars
    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/sierra-tool@11947 d02ae147-ee12-4a35-9452-91c89e8b2592

--- src/com/surelogic/sierra/tool/pmd/AbstractPMDTool.java
+++ src/com/surelogic/sierra/tool/pmd/AbstractPMDTool.java
@@ -1,13 +1,20 @@
 package com.surelogic.sierra.tool.pmd;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FilenameFilter;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.io.Writer;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLClassLoader;
 import java.nio.charset.Charset;
 import java.util.*;
 import java.util.logging.Level;
+import java.util.zip.*;
 
 import net.sourceforge.pmd.*;
 import net.sourceforge.pmd.Report.ProcessingError;
@@ -16,6 +23,7 @@ import net.sourceforge.pmd.renderers.Renderer;
 import com.surelogic.common.*;
 import com.surelogic.common.jobs.SLProgressMonitor;
 import com.surelogic.common.jobs.SLStatus;
+import com.surelogic.common.xml.Entities;
 import com.surelogic.sierra.tool.*;
 import com.surelogic.sierra.tool.ArtifactType;
 import com.surelogic.sierra.tool.message.*;
@@ -23,26 +31,305 @@ import com.surelogic.sierra.tool.message.ArtifactGenerator.*;
 import com.surelogic.sierra.tool.targets.IToolTarget;
 
 public class AbstractPMDTool extends AbstractTool {
+	private static final String PMD_LIB = "pmd-lib";	
 	private static final String rulesets = "all.xml"; // location of the XML rule file
 	
 	public AbstractPMDTool(boolean debug) {
-		super("PMD", PMD.VERSION, "PMD", "", debug);
+		super("PMD", "4.2.4"/*PMD.VERSION*/, "PMD", "", debug);
 	}
 
-	public final Set<ArtifactType> getArtifactTypes() {
-		Set<ArtifactType> types = new HashSet<ArtifactType>();
+	static List<RuleSet> getDefaultRuleSets() throws RuleSetNotFoundException {
+		List<RuleSet> sets = new ArrayList<RuleSet>();
+		
 		// only the default rules
 		final RuleSetFactory ruleSetFactory = new RuleSetFactory(); 
-		try {
-			RuleSet ruleset = ruleSetFactory.createSingleRuleSet(rulesets);
+		final Iterator<RuleSet> it = ruleSetFactory.getRegisteredRuleSets();
+		while (it.hasNext()) {
+			final RuleSet ruleset = it.next();
+			final Language lang = ruleset.getLanguage();
+			if ("Android Rules".equals(ruleset.getName())) {
+				continue;
+			}
+			if (lang == null || Language.JAVA.equals(lang)) {
+				//System.out.println("Found "+ruleset.getName()+" in "+ruleset.getFileName());
+				sets.add(ruleset);
+			}
+		}
+		return sets;
+	}
+	
+	static List<RuleSet> getRuleSets() throws RuleSetNotFoundException {
+		List<File> plugins = findPluginJars(false);
+		URL[] urls         = new URL[plugins.size()];
+		int i = 0;
+		for(File jar : plugins) {
+			try {
+				urls[i] = jar.toURI().toURL();
+			} catch (MalformedURLException e) {
+				LOG.log(Level.WARNING, "Problem converting "+jar.getAbsolutePath()+" to URL", e);
+			}
+			i++;
+		}
+		final URLClassLoader cl = new URLClassLoader(urls, AbstractPMDTool.class.getClassLoader());
+		final RuleSetFactory ruleSetFactory = new RuleSetFactory();
+		final List<RuleSet> sets = getDefaultRuleSets();
+
+		// Add in plugin rulesets
+		for(File jar : findPluginJars(false)) {
+			try {
+				for(RulePair pair : findRuleSetsInJar(jar)) {
+					if (pair != null) {
+						RuleSet set = ruleSetFactory.createRuleSet(pair.stream, cl);
+						if (!containsRuleSet(sets, set)) {
+							if (set.getFileName() == null) {
+								set.setFileName(pair.name);
+							}
+							sets.add(set);
+						}
+					}
+				}
+			} catch (IOException e) {
+				LOG.log(Level.WARNING, "Problem while processing "+jar.getAbsolutePath(), e);
+			}
+		}
+		//RuleSet ruleset = ruleSetFactory.createSingleRuleSet(rulesets);	
+		return sets;
+	}
+	
+	private static boolean containsRuleSet(List<RuleSet> sets, RuleSet set) {
+		for(RuleSet inSet : sets) {
+			if (inSet.getFileName().equals(set.getFileName())) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	Set<ArtifactType> extractArtifactTypes(List<RuleSet> rulesets) {
+		Set<ArtifactType> types = new HashSet<ArtifactType>();	
+		for(RuleSet ruleset : rulesets) {
 			for(Rule r : ruleset.getRules()) {
 				types.add(new ArtifactType(getName(), getVersion(), 
-						                   r.getRuleSetName(), r.getName(), r.getRuleSetName()));				
+						ruleset.getFileName(), r.getName(), r.getRuleSetName()));					
+			}
+		}												
+		return types;
+	}
+	
+	public final Set<ArtifactType> getArtifactTypes() {
+		/*
+		Properties props = System.getProperties();
+		for(Map.Entry<Object,Object> e : props.entrySet()) {
+			System.out.println(e.getKey()+" = "+e.getValue());
+		}
+		for(File jar : findPluginJars()) {
+			System.out.println("Found PMD jar: "+jar.getName());
+		}
+		*/
+		try {		
+			return extractArtifactTypes(getRuleSets());							
+		} catch (RuleSetNotFoundException e) {
+			LOG.log(Level.SEVERE, "Couldn't find rulesets", e);
+		}
+		return Collections.emptySet();
+	}
+	
+	static void generateRulesXML(PrintWriter pw) {
+		pw.println("<?xml version=\"1.0\"?>");
+		StringBuilder b = new StringBuilder();
+		Entities.start("ruleset", b);
+		Entities.addAttribute("name", "Sierra PMD", b);
+		Entities.addAttribute("xmlns", "http://pmd.sf.net/ruleset/1.0.0", b);
+		Entities.addAttribute("xmlns:xsi", "http://www.w3.org/2001/XMLSchema-instance", b);
+		Entities.addAttribute("xsi:schemaLocation", "http://pmd.sf.net/ruleset/1.0.0 http://pmd.sf.net/ruleset_xml_schema.xsd", b);
+		Entities.addAttribute("xsi:noNamespaceSchemaLocation", "http://pmd.sf.net/ruleset_xml_schema.xsd", b);
+		b.append(">\n");
+		pw.append(b);
+		b.setLength(0);
+		
+		Entities.createTag("description", "Custom Sierra RuleSet", b);
+		try {
+			for(RuleSet rs : getRuleSets()) {
+				Entities.start("rule", b);
+				if (rs.getFileName() == null) {
+					System.out.println(rs.getName());
+				}
+				Entities.addAttribute("ref", rs.getFileName(), b);
+				b.append("/>\n");
+				pw.append(b);
+				b.setLength(0);
 			}
 		} catch (RuleSetNotFoundException e) {
-			LOG.log(Level.SEVERE, "Couldn't find "+rulesets, e);
+			e.printStackTrace();
 		}
-		return types;
+		b.append("\n</ruleset>");
+		pw.append(b);
+		b.setLength(0);
+		
+		// Somehow needs to be a resource on the PMD classpath
+		// -- Not so much a problem for the remote VM, but hard/impossible to do locally (for debugging)
+	}
+	
+	/**
+	 * @return the directory containing all.xml
+	 */
+	static File createAllXml() {
+		final File lib = new File(FileUtility.getSierraDataDirectory(), PMD_LIB);
+		if (!lib.exists()) {
+			lib.mkdirs();
+		}
+		else if (!lib.isDirectory()) {
+			LOG.severe(lib.getAbsolutePath()+" exists, but is not a directory");
+			return null;
+		}
+		try {
+			File allXml = new File(lib, rulesets);
+			if (allXml.exists()) {
+				allXml.delete();
+			}
+			PrintWriter pw = new PrintWriter(allXml);
+			generateRulesXML(pw);
+			pw.close();
+			return lib;
+		} catch (FileNotFoundException e) {
+			LOG.log(Level.SEVERE, "Couldn't create "+rulesets, e);
+		}
+		return null;
+	}
+	
+	static final Iterable<RulePair> ERROR = new Iterable<RulePair>() {
+		public Iterator<RulePair> iterator() {
+			return null;
+		}
+	};
+	
+	static class RulePair {
+		final String name;
+		final InputStream stream;
+		
+		RulePair(String id, InputStream is) {
+			name = id;
+			stream = is;
+		}
+	}
+	
+	static Iterable<RulePair> findRuleSetsInJar(final File jar) throws IOException {
+		// Check if ruleset.properties looks good
+		final ZipFile zf  = new ZipFile(jar);
+		/*
+		Enumeration<? extends ZipEntry> entries = zf.entries();
+		while (entries.hasMoreElements()) {
+			System.out.println("Got "+entries.nextElement().getName());
+		}		
+		*/
+		final ZipEntry ze = zf.getEntry("rulesets/rulesets.properties");
+		if (ze == null) {
+			return null;
+		}
+		final InputStream is = zf.getInputStream(ze);
+		if (is == null) {
+			return ERROR;
+		}
+		final Properties props = new Properties();
+		props.load(is);
+		final String names = props.getProperty("rulesets.filenames");
+		if (names == null) {
+			return ERROR;
+		}				
+		final StringTokenizer st = new StringTokenizer(names, ",");
+		return new Iterable<RulePair>() {
+			public Iterator<RulePair> iterator() {
+				return new Iterator<RulePair>() {				
+					public boolean hasNext() {
+						return st.hasMoreTokens();
+					}
+
+					public RulePair next() {
+						String ruleset = st.nextToken();
+						ZipEntry entry = zf.getEntry(ruleset);
+						if (entry == null) {
+							return null;
+						}
+						InputStream stream = null;
+						try {
+							stream = zf.getInputStream(entry);
+						} catch (IOException e) {
+							LOG.log(Level.WARNING, "Problem while reading "+ruleset, e);
+						}
+						if (stream == null) {
+							return null;
+						}						
+						return new RulePair(ruleset, stream);
+					}
+					
+					public void remove() {
+						throw new UnsupportedOperationException();
+					}
+					
+				};
+			}
+		};	
+	}
+	
+	static List<File> findPluginJars(boolean includeRequired) {
+		final File lib = new File(FileUtility.getSierraDataDirectory(), PMD_LIB);
+		if (!lib.exists() || !lib.isDirectory()) {
+			return Collections.emptyList();
+		}
+		final File[] jars = lib.listFiles(new FilenameFilter() {
+			public boolean accept(File dir, String name) {
+				return name.endsWith(".jar") || name.endsWith(".zip");
+			}			
+		});
+		if (jars == null || jars.length == 0) {
+			return Collections.emptyList();
+		}
+		final List<File> valid = new ArrayList<File>();
+		for(final File jar : jars) {
+			try {
+				final Iterable<RulePair> rulesets = findRuleSetsInJar(jar);
+				
+				// Check if ruleset.properties looks good
+				if (rulesets == ERROR) {
+					continue;
+				}
+				if (rulesets != null) {
+					// Check that the named rulesets all exist				
+					boolean ok = true;
+					for(RulePair is : rulesets) {						
+						if (is == null) {
+							LOG.warning("Missing a ruleset in "+jar.getAbsolutePath());
+							ok = false;
+							break; 
+						}						
+					}
+					if (ok) {
+						valid.add(jar);
+					}
+				} 
+				else if (includeRequired) { 
+					// Not a PMD plugin, but probably a jar needed for a plugin
+					// Check if classes are duplicated on the class path?
+					valid.add(jar);
+				}
+			} catch (IOException e) {
+				LOG.log(Level.WARNING, "Problem opening "+jar.getAbsolutePath(), e);
+			}
+		}
+		return valid;
+	}
+	
+	@Override
+	public List<File> getRequiredJars() {
+		List<File> jars = findPluginJars(true);
+		File all        = createAllXml();
+		if (jars.isEmpty()) {
+			return all == null ? Collections.<File>emptyList() : Collections.singletonList(all);
+		} 
+		else if (all != null) {
+			jars.add(0, all);
+		}		
+		return jars;
 	}
 	
 	protected final IToolInstance create(String name, final ArtifactGenerator generator,
