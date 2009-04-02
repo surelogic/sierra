d4101319fdf157ac4a8cd0b9799e3ceff1b8b3a8
Edwin Chan <edwin.chan@surelogic.com>
2009-04-02 17:55:37 +0000

    Added metadata methods for IToolFactory
    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/sierra-fb@12031 d02ae147-ee12-4a35-9452-91c89e8b2592

--- src/com/surelogic/sierra/tool/findbugs/FindBugsToolFactory.java
+++ src/com/surelogic/sierra/tool/findbugs/FindBugsToolFactory.java
@@ -2,17 +2,26 @@ package com.surelogic.sierra.tool.findbugs;
 
 import java.io.File;
 
-import com.surelogic.sierra.tool.ITool;
-import com.surelogic.sierra.tool.IToolFactory;
+import com.surelogic.sierra.tool.*;
 import com.surelogic.sierra.tool.message.Config;
 
-public class FindBugsToolFactory implements IToolFactory {
+public class FindBugsToolFactory extends AbstractToolFactory {
 	private static final String FINDBUGS = "findbugs";
+	private static final String INFO = "<A HREF=\"http://findbugs.sourceforge.net\">FindBugs</A> is a static analysis tool created at University of Maryland for finding bugs in Java code.";
 	
 	public String getId() {
 		return FINDBUGS;
 	}
 	
+	public String getName() {
+		return "FindBugs\u2122";
+	}
+	
+	public String getHTMLInfo() {
+		return INFO;
+	}
+	
+	@Override
 	public void init(File toolHome) {
 		AbstractFindBugsTool.init(toolHome.getAbsolutePath());
 	}
