4f803f0d854fcc93571bfaeada5b8795b95a03db
Edwin Chan <edwin.chan@surelogic.com>
2009-04-02 13:57:41 +0000

    Changed to depend on common, sierra-tool, sierra-message Changed to use AbstractToolFactory Added attribute to mark as Sierra tool
    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/sierra-pmd@12019 d02ae147-ee12-4a35-9452-91c89e8b2592

--- src/com/surelogic/sierra/tool/pmd/CPDToolFactory.java
+++ src/com/surelogic/sierra/tool/pmd/CPDToolFactory.java
@@ -1,10 +1,9 @@
 package com.surelogic.sierra.tool.pmd;
 
-import com.surelogic.sierra.tool.ITool;
-import com.surelogic.sierra.tool.IToolFactory;
+import com.surelogic.sierra.tool.*;
 import com.surelogic.sierra.tool.message.Config;
 
-public class CPDToolFactory implements IToolFactory {
+public class CPDToolFactory extends AbstractToolFactory {
 	public ITool create(Config config) {
 		return new CPD4_1Tool(config);
 	}
--- src/com/surelogic/sierra/tool/pmd/PMDToolFactory.java
+++ src/com/surelogic/sierra/tool/pmd/PMDToolFactory.java
@@ -1,10 +1,9 @@
 package com.surelogic.sierra.tool.pmd;
 
-import com.surelogic.sierra.tool.ITool;
-import com.surelogic.sierra.tool.IToolFactory;
+import com.surelogic.sierra.tool.*;
 import com.surelogic.sierra.tool.message.Config;
 
-public class PMDToolFactory implements IToolFactory {
+public class PMDToolFactory extends AbstractToolFactory {
 	public ITool create(Config config) {
 		return new AbstractPMDTool(config);
 	}
