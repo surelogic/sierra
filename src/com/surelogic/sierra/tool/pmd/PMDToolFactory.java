d1b9306d9ad3c94be54045a8964107821064ba14
Edwin Chan <edwin.chan@surelogic.com>
2009-04-02 17:55:37 +0000

    Added metadata methods for IToolFactory
    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/sierra-pmd@12031 d02ae147-ee12-4a35-9452-91c89e8b2592

--- src/com/surelogic/sierra/tool/pmd/PMDToolFactory.java
+++ src/com/surelogic/sierra/tool/pmd/PMDToolFactory.java
@@ -5,9 +5,18 @@ import com.surelogic.sierra.tool.message.Config;
 
 public class PMDToolFactory extends AbstractToolFactory {
 	private static final String PMD = "pmd";
-	
+	private static final String INFO = "<A HREF=\"http://pmd.sourceforge.net\">PMD</A> is a static analysis tool to look for multiple issues like potential bugs, dead and sub-optimal code, and over-complicated expressions.";
+
 	public String getId() {
 		return PMD;
+	}
+	
+	public String getName() {
+		return "PMD\u2122";
+	}
+	
+	public String getHTMLInfo() {
+		return INFO;
 	}
 	
 	public ITool create(Config config) {
