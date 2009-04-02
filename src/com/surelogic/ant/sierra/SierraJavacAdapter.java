a360756dec7200f53518502dc24820a9aa568f58
Edwin Chan <edwin.chan@surelogic.com>
2009-04-02 18:28:36 +0000

    Modified to correctly locate tool plugins
    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/sierra-ant@12034 d02ae147-ee12-4a35-9452-91c89e8b2592

--- src/com/surelogic/ant/sierra/SierraJavacAdapter.java
+++ src/com/surelogic/ant/sierra/SierraJavacAdapter.java
@@ -76,6 +76,7 @@ public class SierraJavacAdapter extends DefaultCompilerAdapter {
 		if (!new File(libHome).exists()) {
 			throw new BuildException("No lib subdirectory under "+libHome);
 		}		
+		System.setProperty(ToolUtil.SIERRA_TOOLS_DIR, libHome);
 		config.setExcludedToolsList("checkstyle");
 		config.setToolsDirectory(new File(libHome+"reckoner"));
 		config.putPluginDir(SierraToolConstants.COMMON_PLUGIN_ID,
