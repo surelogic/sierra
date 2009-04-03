319f7514c370378dbaa2d2f3341a319ee300a25b
Tim Halloran <tim.halloran@surelogic.com>
2009-04-03 19:52:30 +0000

    Reworked the entire controls of the ResultExplorer. Added next and previous query controls and made a menu to take the less used options off of the toolbar (like export and show SQL).
    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/sierra-client-eclipse@12068 d02ae147-ee12-4a35-9452-91c89e8b2592

--- src/com/surelogic/sierra/client/eclipse/views/adhoc/QueryResultsView.java
+++ src/com/surelogic/sierra/client/eclipse/views/adhoc/QueryResultsView.java
@@ -3,6 +3,7 @@ package com.surelogic.sierra.client.eclipse.views.adhoc;
 import org.eclipse.swt.widgets.Composite;
 
 import com.surelogic.adhoc.views.results.AbstractQueryResultsView;
+import com.surelogic.common.adhoc.AdHocManager;
 import com.surelogic.common.serviceability.UsageMeter;
 
 public final class QueryResultsView extends AbstractQueryResultsView {
@@ -11,5 +12,10 @@ public final class QueryResultsView extends AbstractQueryResultsView {
 	public void createPartControl(final Composite parent) {
 		UsageMeter.getInstance().tickUse("Sierra QueryResultsView opened");
 		super.createPartControl(parent);
+	}
+
+	@Override
+	public AdHocManager getManager() {
+		return AdHocDataSource.getManager();
 	}
 }
