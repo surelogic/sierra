0ee9a4922e59cedddc03132d9ba0d19ec486373c
Edwin Chan <edwin.chan@surelogic.com>
2009-04-07 16:18:16 +0000

    Moved tool metadata out into separate manifest files
    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/sierra-pmd@12098 d02ae147-ee12-4a35-9452-91c89e8b2592

--- src/com/surelogic/sierra/tool/pmd/CPDToolFactory.java
+++ src/com/surelogic/sierra/tool/pmd/CPDToolFactory.java
@@ -3,27 +3,7 @@ package com.surelogic.sierra.tool.pmd;
 import com.surelogic.sierra.tool.*;
 import com.surelogic.sierra.tool.message.Config;
 
-public class CPDToolFactory extends AbstractToolFactory {
-	private static final String CPD = "CPD";
-	private static final String INFO = "<A HREF=\"http://pmd.sourceforge.net\">CPD</A> is a static analysis tool that looks for duplicated code.";
-		
-	public String getId() {
-		return CPD;
-	}
-	
-	public String getName() {
-		return "CPD";
-	}
-	
-	@Override 
-	public String getVersion() {
-		return "4.1";
-	}
-	
-	public String getHTMLInfo() {
-		return INFO;
-	}
-	
+public class CPDToolFactory extends AbstractToolFactory {	
 	public ITool create(Config config) {
 		return new CPD4_1Tool(this, config);
 	}
--- src/com/surelogic/sierra/tool/pmd/PMDToolFactory.java
+++ src/com/surelogic/sierra/tool/pmd/PMDToolFactory.java
@@ -4,26 +4,11 @@ import com.surelogic.sierra.tool.*;
 import com.surelogic.sierra.tool.message.Config;
 
 public class PMDToolFactory extends AbstractToolFactory {
-	private static final String PMD = "PMD";
-	private static final String INFO = "<A HREF=\"http://pmd.sourceforge.net\">PMD</A> is a static analysis tool to look for multiple issues like potential bugs, dead and sub-optimal code, and over-complicated expressions.";
-
-	public String getId() {
-		return PMD;
-	}
-	
-	public String getName() {
-		return "PMD\u2122";
-	}
-	
 //	@Override
 //	public String getVersion() {
 //		return "4.2.4"/*PMD.VERSION*/;
 //	}
-	
-	public String getHTMLInfo() {
-		return INFO;
-	}
-	
+
 	public ITool create(Config config) {
 		return new AbstractPMDTool(this, config);
 	}
