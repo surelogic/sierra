9e247b20add2887448a2b10700f09fe2f548c38f
Edwin Chan <edwin.chan@surelogic.com>
2009-04-07 16:18:16 +0000

    Moved tool metadata out into separate manifest files
    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/sierra-fb@12098 d02ae147-ee12-4a35-9452-91c89e8b2592

--- src/com/surelogic/sierra/tool/findbugs/FindBugsToolFactory.java
+++ src/com/surelogic/sierra/tool/findbugs/FindBugsToolFactory.java
@@ -5,26 +5,11 @@ import java.io.File;
 import com.surelogic.sierra.tool.*;
 import com.surelogic.sierra.tool.message.Config;
 
-public class FindBugsToolFactory extends AbstractToolFactory {
-	private static final String FINDBUGS = "FindBugs";
-	private static final String INFO = "<A HREF=\"http://findbugs.sourceforge.net\">FindBugs</A> is a static analysis tool created at University of Maryland for finding bugs in Java code.";
-	
-	public String getId() {
-		return FINDBUGS;
-	}
-	
-	public String getName() {
-		return "FindBugs\u2122";
-	}
-	
+public class FindBugsToolFactory extends AbstractToolFactory {	
 //	@Override
 //	public String getVersion() {
 //		return "1.3.7"/*Version.RELEASE_BASE*/;
 //	}
-	
-	public String getHTMLInfo() {
-		return INFO;
-	}
 	
 	@Override
 	public void init(File toolHome, File pluginDir) {
