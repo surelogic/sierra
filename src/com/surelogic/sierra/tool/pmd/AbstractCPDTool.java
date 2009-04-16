0a513667924ef5df4aee0069b3acc1f9f462874b
Edwin Chan <edwin.chan@surelogic.com>
2009-04-16 14:01:45 +0000

    Factored out SourceRoots from AbstractCPDTool Fixed up to match changes
    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/sierra-pmd@12182 d02ae147-ee12-4a35-9452-91c89e8b2592

--- src/com/surelogic/sierra/tool/pmd/AbstractCPDTool.java
+++ src/com/surelogic/sierra/tool/pmd/AbstractCPDTool.java
@@ -1,7 +1,7 @@
 package com.surelogic.sierra.tool.pmd;
 
 import java.io.File;
-import java.net.URI;
+import java.io.IOException;
 import java.util.*;
 
 import net.sourceforge.pmd.cpd.*;
@@ -14,9 +14,10 @@ import com.surelogic.sierra.tool.ArtifactType;
 import com.surelogic.sierra.tool.analyzer.ILazyArtifactGenerator;
 import com.surelogic.sierra.tool.message.*;
 import com.surelogic.sierra.tool.message.ArtifactGenerator.ArtifactBuilder;
-import com.surelogic.sierra.tool.targets.*;
 
 public abstract class AbstractCPDTool extends AbstractToolInstance {
+	public static final String DUPLICATED_CODE = "DuplicatedCode";
+
 	public AbstractCPDTool(CPDToolFactory f, Config config, ILazyArtifactGenerator generator, boolean close) {
 		super(f, config, generator, close);
 	}
@@ -24,15 +25,6 @@ public abstract class AbstractCPDTool extends AbstractToolInstance {
 	public Set<ArtifactType> getArtifactTypes() {
 		return Collections.emptySet();
 	}
-
-	/*
-	@Override
-	public List<File> getRequiredJars() {
-		final List<File> jars = new ArrayList<File>();				
-		addAllPluginJarsToPath(debug, jars,	SierraToolConstants.PMD_PLUGIN_ID, "lib");
-		return jars;
-	}
-	 */
 
 	@Override
 	protected SLStatus execute(SLProgressMonitor monitor)
@@ -48,7 +40,7 @@ public abstract class AbstractCPDTool extends AbstractToolInstance {
 
 		LanguageFactory f = new LanguageFactory();
 		Language language = f.createLanguage(languageString);
-		CPD cpd = new CPD(minimumTokens, language);
+		final CPD cpd = new CPD(minimumTokens, language);
 		cpd.setEncoding(encodingString);
 		if (skipDuplicateFiles) {
 			cpd.skipDuplicates();
@@ -62,35 +54,13 @@ public abstract class AbstractCPDTool extends AbstractToolInstance {
 				System.out.println("CPD Phase " + phase);
 			}
 		});
+		final SourceRoots roots = collectSourceRoots(new SourcePrep() {
+			@Override
+			public void prep(File f) throws IOException {				
+				cpd.add(f);
+			}}
+		);
 
-		Map<String, String> roots = new HashMap<String, String>();
-		for (IToolTarget t : getSrcTargets()) {
-			File location = new File(t.getLocation());
-			String locName = location.getAbsolutePath();
-			if (t instanceof FileTarget) {
-				FileTarget ft = (FileTarget) t;
-				if (!location.exists()
-						|| !location.getName().endsWith(".java")) {
-					continue;
-				}
-				if (ft.getRoot() != null) {
-					String root = new File(ft.getRoot())
-					.getAbsolutePath();
-					roots.put(locName, root);
-				} else {
-					System.out.println("No root for " + locName);
-				}
-				cpd.add(location);
-			} else
-				for (URI loc : t.getFiles()) {
-					File file = new File(loc);
-					if (file.exists()
-							&& file.getName().endsWith(".java")) {
-						roots.put(file.getAbsolutePath(), locName);
-						cpd.add(file);
-					}
-				}
-		}
 		monitor.worked(1);
 		monitor.subTask("Scanning source files");
 		cpd.go();
@@ -105,20 +75,20 @@ public abstract class AbstractCPDTool extends AbstractToolInstance {
 		return SLStatus.OK_STATUS;
 	}
 
-	protected void createArtifact(Map<String, String> roots,
+	protected void createArtifact(SourceRoots roots,
 			ArtifactGenerator generator, Match m) {
 		ArtifactBuilder artifact = generator.artifact();
 		TokenEntry firstMark = m.getFirstMark();
 		setSourceLocation(artifact.primarySourceLocation(),
-				new SrcInfo(roots, firstMark, m.getLineCount()));
+				createSrcInfo(roots, firstMark, m.getLineCount()));
 		for (TokenEntry mark : m.getMarkSet()) {
 			if (mark.getIndex() != firstMark.getIndex()) {
 				setSourceLocation(artifact.sourceLocation(),
-						new SrcInfo(roots, mark, m.getLineCount()));
+						createSrcInfo(roots, mark, m.getLineCount()));
 			}
 		}
 
-		artifact.findingType(getName(), getVersion(), "DuplicatedCode");
+		artifact.findingType(getName(), getVersion(), DUPLICATED_CODE);
 		artifact.message("Found " + m.getMarkCount()
 				+ " instances of the same " + m.getLineCount()
 				+ " lines");
@@ -126,39 +96,11 @@ public abstract class AbstractCPDTool extends AbstractToolInstance {
 		artifact.build();
 	}
 
-	class SrcInfo extends SourceInfo {
-		SrcInfo(Map<String, String> roots, TokenEntry mark,
-				int lineCount) {
-			fileName = mark.getTokenSrcID();
-
-			final String root = roots.get(fileName);
-			if (root == null) {
-				throw new IllegalArgumentException(fileName
-						+ " doesn't have a source root");
-			}
-			final String file;
-			if (fileName.startsWith(root)) {
-				file = fileName.substring(root.length() + 1);
-			} else {
-				throw new IllegalArgumentException(fileName
-						+ " start with root " + root);
-			}
-
-			// Modified from AbstractPMDTool.getCompUnitName()
-			int separator = file.lastIndexOf(File.separatorChar);
-			if (separator < 0) {
-				// Default package
-				packageName = "";
-				cuName = file.substring(0, file.length()
-						- JAVA_SUFFIX_LEN);
-			} else {
-				packageName = file.substring(0, separator).replace(
-						File.separatorChar, '.');
-				cuName = file.substring(separator + 1, file.length()
-						- JAVA_SUFFIX_LEN);
-			}
-			startLine = mark.getBeginLine();
-			endLine = mark.getBeginLine() + lineCount;
-		}
+	private SourceInfo createSrcInfo(SourceRoots roots, TokenEntry mark, int lineCount) {
+		SourceInfo info = new SourceInfo();
+		roots.initSourceInfo(info, mark.getTokenSrcID());
+		info.startLine = mark.getBeginLine();
+		info.endLine = mark.getBeginLine() + lineCount;
+		return info;
 	}
 }
