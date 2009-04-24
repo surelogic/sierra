bf73af826ed3bbf00fa4537cd7dbae26849b103b
Edwin Chan <edwin.chan@surelogic.com>
2009-04-24 18:02:25 +0000

    Commented out code
    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/sierra-client-eclipse@12274 d02ae147-ee12-4a35-9452-91c89e8b2592

--- src/com/surelogic/sierra/client/eclipse/jobs/RefreshDatabaseJob.java
+++ src/com/surelogic/sierra/client/eclipse/jobs/RefreshDatabaseJob.java
@@ -3,10 +3,6 @@ package com.surelogic.sierra.client.eclipse.jobs;
 import com.surelogic.common.jobs.AbstractSLJob;
 import com.surelogic.common.jobs.SLProgressMonitor;
 import com.surelogic.common.jobs.SLStatus;
-import com.surelogic.common.logging.SLLogger;
-import com.surelogic.sierra.client.eclipse.Data;
-import com.surelogic.sierra.client.eclipse.model.ConnectedServerManager;
-import com.surelogic.sierra.client.eclipse.model.DatabaseHub;
 
 public final class RefreshDatabaseJob extends AbstractSLJob {
 
@@ -17,11 +13,15 @@ public final class RefreshDatabaseJob extends AbstractSLJob {
 	public SLStatus run(SLProgressMonitor monitor) {
 		//final String msg = "Refreshing the database from the file system.";
 		monitor.begin();
+		
+		// It seems like I don't need to do anything
+		/*
 		ConnectedServerManager.getInstance().changed();
 		// Projects.getInstance().clear();
 		Data.getInstance().loggedBootAndCheckSchema();
-		DatabaseHub.getInstance().notifyDatabaseDeleted(); // TODO
+		DatabaseHub.getInstance().notifyServerSynchronized(); // TODO fix
 		SLLogger.getLogger().info("The client database has been refreshed");
+		*/
 		return SLStatus.OK_STATUS;
 	}
 }
