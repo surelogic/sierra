56f43026ddc2b14b9e2e0bfbb99fd23c97ba7011
Tim Halloran <tim.halloran@surelogic.com>
2009-05-26 18:54:17 +0000

    Working on a "dialog" prompt to the user to prepare raw data from runs.
    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/sierra-client-eclipse@12325 d02ae147-ee12-4a35-9452-91c89e8b2592

--- src/com/surelogic/sierra/client/eclipse/dialogs/ConfirmPerspectiveSwitch.java
+++ src/com/surelogic/sierra/client/eclipse/dialogs/ConfirmPerspectiveSwitch.java
@@ -7,16 +7,21 @@ import com.surelogic.common.eclipse.dialogs.AbstractConfirmPerspectiveSwitch;
 import com.surelogic.sierra.client.eclipse.perspectives.CodeReviewPerspective;
 import com.surelogic.sierra.client.eclipse.preferences.PreferenceConstants;
 
-public final class ConfirmPerspectiveSwitch extends AbstractConfirmPerspectiveSwitch {
-	public static final ConfirmPerspectiveSwitch prototype = new ConfirmPerspectiveSwitch();	
-	
+public final class ConfirmPerspectiveSwitch extends
+		AbstractConfirmPerspectiveSwitch {
+
+	public static final ConfirmPerspectiveSwitch prototype = new ConfirmPerspectiveSwitch();
+
 	private ConfirmPerspectiveSwitch() {
-		super(CodeReviewPerspective.class.getName(), PreferenceConstants.prototype);
+		super(CodeReviewPerspective.class.getName(),
+				PreferenceConstants.prototype);
 	}
+
 	@Override
 	protected String getLogo() {
 		return CommonImages.IMG_SIERRA_LOGO;
 	}
+
 	@Override
 	protected String getShortPrefix() {
 		return "sierra.";
