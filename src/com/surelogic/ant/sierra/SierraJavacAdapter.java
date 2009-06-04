39825403b56a8ec848a78feef3108ac5e1869596
Tim Halloran <tim.halloran@surelogic.com>
2009-06-04 13:11:07 +0000

    Changed the name of some string constants to indicate that they are, in fact, path fragments.
    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/sierra-ant@12389 d02ae147-ee12-4a35-9452-91c89e8b2592

--- src/com/surelogic/ant/sierra/SierraJavacAdapter.java
+++ src/com/surelogic/ant/sierra/SierraJavacAdapter.java
@@ -79,7 +79,7 @@ public class SierraJavacAdapter extends DefaultCompilerAdapter {
 		if (!new File(libHome).exists()) {
 			throw new BuildException("No lib subdirectory under "+libHome);
 		}		
-		System.setProperty(ToolUtil.SIERRA_TOOLS_DIR, libHome);		
+		System.setProperty(ToolUtil.TOOLS_PATH_PROP_NAME, libHome);		
 		config.setExcludedToolsList("checkstyle");
 		config.setToolsDirectory(new File(libHome+"reckoner"));
 		config.putPluginDir(SierraToolConstants.COMMON_PLUGIN_ID,
