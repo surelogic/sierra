2458992e23a3d953aaff26baa4895097e22f6149
Edwin Chan <edwin.chan@surelogic.com>
2009-06-17 20:42:39 +0000

    Upgraded to PMD 4.2.5 and FindBugs 1.3.8
    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/sierra-jdbc@12493 d02ae147-ee12-4a35-9452-91c89e8b2592

---
+++ src/com/surelogic/sierra/findbugs1_3_8/FindBugs1_3_8ToolInfoGenerator.java
@@ -0,0 +1,36 @@
+package com.surelogic.sierra.findbugs1_3_8;
+
+import java.io.InputStream;
+import java.sql.Connection;
+import java.util.logging.Logger;
+
+import javax.xml.parsers.SAXParser;
+
+import com.surelogic.common.xml.XMLUtil;
+import com.surelogic.sierra.findbugs.AbstractFBToolInfoGenerator;
+import com.surelogic.sierra.jdbc.tool.ToolBuilder.ArtifactTypeBuilder;
+
+public class FindBugs1_3_8ToolInfoGenerator extends AbstractFBToolInfoGenerator {
+  private static final Logger log = Logger
+      .getLogger(FindBugs1_3_8ToolInfoGenerator.class.getName());
+
+  /**
+   * This needs to be in this class to load the right resources
+   */
+  protected InputStream getStream(String name) {
+    return Thread.currentThread().getContextClassLoader().getResourceAsStream(name);
+  }
+  
+  /**
+   * Load the rulesets and persist them to the embedded database.
+   * 
+   */
+  public static void generateTool(Connection conn) {
+    FindBugs1_3_8ToolInfoGenerator gen = new FindBugs1_3_8ToolInfoGenerator(); 
+    SAXParser sp = XMLUtil.createSAXParser();
+    // NEED to change these
+    ArtifactTypeBuilder t = createArtifactTypeBuilder(conn, "1.3.8");
+    gen.parseResources(log, sp, t, "com/surelogic/sierra/findbugs1_3_8/messages.xml",
+                                   "com/surelogic/sierra/findbugs1_3_8/findbugs.xml");
+  }
+}
