38d01b9c08552ac6a99051576b257a0cf138a366
Edwin Chan <edwin.chan@surelogic.com>
2009-07-20 15:57:06 +0000

    Added code to attempt to insert the manifest directly into a jar file
    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/sierra-client-eclipse@12715 d02ae147-ee12-4a35-9452-91c89e8b2592

--- src/com/surelogic/sierra/client/eclipse/wizards/ArtifactTypeSetupWizard.java
+++ src/com/surelogic/sierra/client/eclipse/wizards/ArtifactTypeSetupWizard.java
@@ -2,6 +2,7 @@ package com.surelogic.sierra.client.eclipse.wizards;
 
 import java.io.*;
 import java.util.*;
+import java.util.zip.*;
 
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.jface.wizard.Wizard;
@@ -60,56 +61,121 @@ public class ArtifactTypeSetupWizard extends Wizard {
 	
 	@Override
 	public boolean performFinish() {
+		// Clear internal state
+		for(ArtifactType t : types) {
+			if (AbstractArtifactTypePage.DEFAULT.equals(t.getFindingType())) {
+				t.setFindingType(null);
+			}
+		}
+		
 		Map<String,String> locations = f_manifestLocationPage.getLocationMap();
 		for(Map.Entry<String,List<ArtifactType>> plugin : f_plugins.entrySet()) {
 			try {
-				FileWriter w = new FileWriter(locations.get(plugin.getKey()));
-				PrintWriter out = new PrintWriter(w);
-				out.println("Manifest-Version: 1.0\n");
-				boolean first = true;
-				
-				List<ArtifactType> unmapped = new ArrayList<ArtifactType>();
-				for(final ArtifactType t : plugin.getValue()) {
-					final String fType = t.getFindingType();
-					if (mappedToFindingType(fType)) {
-						if (first) {
-							out.println("Name: "+ToolUtil.FINDING_TYPE_MAPPING_KEY);
-							first = false;
+				final File f = new File(locations.get(plugin.getKey()));
+				if (f.exists() && f.getName().endsWith(".jar")) {
+					// FIX check if it has the expected files??
+					final File temp = File.createTempFile("sierra", ".jar");
+					final File old = File.createTempFile("oldSierra", ".jar");
+					ZipOutputStream zos = new ZipOutputStream(new FileOutputStream(temp));
+					ZipEntry ze = new ZipEntry(ToolUtil.SIERRA_MANIFEST);
+					zos.putNextEntry(ze);
+					writeManifest(zos, plugin.getValue());
+					zos.closeEntry();
+					
+					// Copy the other files
+					final boolean moved = f.renameTo(old);
+					ZipFile zf = moved ? new ZipFile(old) : new ZipFile(f);
+					Enumeration<? extends ZipEntry> e = zf.entries();
+					while (e.hasMoreElements()) {
+						ze = e.nextElement();
+						if (ToolUtil.SIERRA_MANIFEST.equals(ze.getName())) {
+							// Already written
+							continue;
 						}
-						out.println(t.type+": "+fType);
+						copyStream(zos, zf, ze);
+					}
+					zf.close();
+					zos.close();
+					
+					if (moved || f.renameTo(old)) {
+						if (temp.renameTo(f)) {
+							old.delete();
+						}
 					} else {
-						unmapped.add(t);
+						boolean renamed = temp.renameTo(new File(f.getParentFile(), f.getName()+".new"));
+						//System.out.println(renamed);
 					}
+				} else {
+					OutputStream os = new FileOutputStream(f);
+					writeManifest(os, plugin.getValue());
+					os.close();
 				}
-				if (!first) {
-					out.println();
-				}	
-				
-				if (unmapped.size() > 0) {
-					out.println("Name: "+ToolUtil.CATEGORY_MAPPING_KEY);
-					for(final ArtifactType t : unmapped) {
-						out.println(t.type+": "+t.getCategory());			
-					}				
-					out.println();
-				}
-				
-				first = true;
-				for(final ArtifactType t : unmapped) {
-					if (!t.includeInScan()) {
-						if (first) {
-							out.println("Name: "+ToolUtil.SCAN_FILTER_BLACKLIST_KEY);
-							first = false;
-						}
-						out.println(t.type+": ignore");			
-					}
-				}
-				out.println();
-				out.close();
 			} catch(IOException e) {
+				e.printStackTrace();
 				return false;
 			}	
 		}
 		return true;
+	}
+	
+	private void copyStream(ZipOutputStream zos, ZipFile zf, ZipEntry ze) throws IOException {
+		InputStream in = zf.getInputStream(ze);
+		ZipEntry newE = new ZipEntry(ze.getName());
+		zos.putNextEntry(newE);
+
+		// Transfer bytes from in to out
+		final byte[] buf = new byte[4096];
+		int len;
+		while ((len = in.read(buf)) > 0) {
+			zos.write(buf, 0, len);
+		}
+		in.close();
+		zos.closeEntry();
+	}
+
+	private void writeManifest(final OutputStream os, List<ArtifactType> types) throws IOException {
+		Writer w = new OutputStreamWriter(os);
+		PrintWriter out = new PrintWriter(w);
+		out.println("Manifest-Version: 1.0\n");
+		boolean first = true;
+
+		List<ArtifactType> unmapped = new ArrayList<ArtifactType>();
+		for(final ArtifactType t : types) {
+			final String fType = t.getFindingType();
+			if (mappedToFindingType(fType)) {
+				if (first) {
+					out.println("Name: "+ToolUtil.FINDING_TYPE_MAPPING_KEY);
+					first = false;
+				}
+				out.println(t.type+": "+fType);
+			} else {
+				unmapped.add(t);
+			}
+		}
+		if (!first) {
+			out.println();
+		}	
+
+		if (unmapped.size() > 0) {
+			out.println("Name: "+ToolUtil.CATEGORY_MAPPING_KEY);
+			for(final ArtifactType t : unmapped) {
+				out.println(t.type+": "+t.getCategory());			
+			}				
+			out.println();
+		}
+
+		first = true;
+		for(final ArtifactType t : unmapped) {
+			if (!t.includeInScan()) {
+				if (first) {
+					out.println("Name: "+ToolUtil.SCAN_FILTER_BLACKLIST_KEY);
+					first = false;
+				}
+				out.println(t.type+": ignore");			
+			}
+		}
+		out.println();
+		out.flush();
 	}
 
 	private boolean mappedToFindingType(final String fType) {
