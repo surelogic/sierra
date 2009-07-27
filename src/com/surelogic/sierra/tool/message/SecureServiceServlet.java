2f3508a29f84ed0777c5d5fd3a6dfcdf1d0c0588
Nathan Boy <nathan.boy@surelogic.com>
2009-07-27 17:17:54 +0000

    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/sierra-services@12747 d02ae147-ee12-4a35-9452-91c89e8b2592

--- src/com/surelogic/sierra/tool/message/SecureServiceServlet.java
+++ src/com/surelogic/sierra/tool/message/SecureServiceServlet.java
@@ -8,9 +8,9 @@ import javax.servlet.http.HttpServletResponse;
 
 import com.surelogic.sierra.jdbc.server.UserContext;
 import com.surelogic.sierra.jdbc.user.User;
-import com.surelogic.sierra.message.srpc.SRPCServlet;
+import com.surelogic.sierra.message.srpc.MultiPartSRPCServlet;
 
-public abstract class SecureServiceServlet extends SRPCServlet {
+public abstract class SecureServiceServlet extends MultiPartSRPCServlet {
 
 	/**
 	 * 
@@ -18,8 +18,9 @@ public abstract class SecureServiceServlet extends SRPCServlet {
 	private static final long serialVersionUID = -664892243722651966L;
 
 	@Override
-	protected void service(HttpServletRequest req, HttpServletResponse resp)
-			throws ServletException, IOException {
+	protected void service(final HttpServletRequest req,
+			final HttpServletResponse resp) throws ServletException,
+			IOException {
 		try {
 			UserContext.set((User) req.getAttribute("SierraUser"));
 			super.service(req, resp);
