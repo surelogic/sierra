815bda0c38f5d0e994af21b62e279af1377103b8
Edwin Chan <edwin.chan@surelogic.com>
2009-07-27 19:45:25 +0000

    Cached the settings service
    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/sierra-portal@12751 d02ae147-ee12-4a35-9452-91c89e8b2592

--- src/com/surelogic/sierra/gwt/client/data/cache/FindingTypeCache.java
+++ src/com/surelogic/sierra/gwt/client/data/cache/FindingTypeCache.java
@@ -5,19 +5,21 @@ import java.util.List;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.surelogic.sierra.gwt.client.data.FindingType;
 import com.surelogic.sierra.gwt.client.data.Status;
-import com.surelogic.sierra.gwt.client.service.ServiceHelper;
+import com.surelogic.sierra.gwt.client.service.*;
 
 public final class FindingTypeCache extends Cache<FindingType> {
 
 	private static final FindingTypeCache INSTANCE = new FindingTypeCache();
-
+	private final SettingsServiceAsync settings;
+	
 	private FindingTypeCache() {
 		super();
+		settings = ServiceHelper.getSettingsService();
 	}
 
 	@Override
 	protected void doRefreshCall(final AsyncCallback<List<FindingType>> callback) {
-		ServiceHelper.getSettingsService().getFindingTypes(callback);
+		settings.getFindingTypes(callback);
 	}
 
 	@Override
