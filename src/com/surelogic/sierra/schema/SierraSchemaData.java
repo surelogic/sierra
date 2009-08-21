5c686b1455f9bff6f252a0ba57c495561a1b4ede
Edwin Chan <edwin.chan@surelogic.com>
2009-08-21 14:12:37 +0000

    Factored out duplicate code
    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/sierra-jdbc@12827 d02ae147-ee12-4a35-9452-91c89e8b2592

--- src/com/surelogic/sierra/schema/SierraSchemaData.java
+++ src/com/surelogic/sierra/schema/SierraSchemaData.java
@@ -1,62 +1,28 @@
 package com.surelogic.sierra.schema;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.InputStream;
 import java.net.URL;
 
-import com.surelogic.common.jdbc.SchemaAction;
-import com.surelogic.common.jdbc.SchemaData;
 import com.surelogic.common.license.SLLicenseUtility;
+import com.surelogic.flashlight.schema.AbstractSchemaData;
 
-public class SierraSchemaData implements SchemaData {
-
-	private final ClassLoader loader;
-	private final String schemaPackage;
-
+public class SierraSchemaData extends AbstractSchemaData {
 	public SierraSchemaData() {
-		loader = Thread.currentThread().getContextClassLoader();
-		schemaPackage = "com.surelogic.sierra.schema";
+		super("com.surelogic.sierra.schema",
+			  Thread.currentThread().getContextClassLoader(),
+			  SLLicenseUtility.SIERRA_SUBJECT);
 	}
 
-	public SchemaAction getSchemaAction(final String action) {
-		try {
-			return (SchemaAction) loader
-					.loadClass(schemaPackage + "." + action).newInstance();
-		} catch (final InstantiationException e) {
-			throw new IllegalStateException(e);
-		} catch (final IllegalAccessException e) {
-			throw new IllegalStateException(e);
-		} catch (final ClassNotFoundException e) {
-			return null;
-		}
+	protected Object newInstance(String qname) 
+	throws InstantiationException, IllegalAccessException, ClassNotFoundException {
+		return loader.loadClass(qname).newInstance();
 	}
 
 	public URL getSchemaResource(final String name) {
 		return loader.getResource(getSchemaResourcePath(name));
 	}
-
-	public int getVersion() {
-		final BufferedReader reader = new BufferedReader(
-				new InputStreamReader(
-						loader
-								.getResourceAsStream(getSchemaResourcePath("version.txt"))));
-		try {
-			try {
-				return Integer.valueOf(reader.readLine().trim());
-			} finally {
-				reader.close();
-			}
-		} catch (final IOException e) {
-			throw new IllegalStateException(e);
-		}
-	}
-
-	private String getSchemaResourcePath(final String resource) {
-		return "/" + schemaPackage.replace(".", "/") + "/" + resource;
-	}
-
-	public String getLicenseSubject() {
-		return SLLicenseUtility.SIERRA_SUBJECT;
+	
+	protected InputStream getResourceAsStream(String path) {
+		return loader.getResourceAsStream(path);
 	}
 }
