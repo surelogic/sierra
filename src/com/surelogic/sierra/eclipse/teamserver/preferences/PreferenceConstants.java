de349ba0dcdc14a91d43394d3e7ad5e05bd5a059
Tim Halloran <tim.halloran@surelogic.com>
2010-08-06 18:05:33 +0000

    Updated preference store API use to not invoke deprecated methods.
    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/sierra-team-server-client-eclipse@15346 d02ae147-ee12-4a35-9452-91c89e8b2592

--- src/com/surelogic/sierra/eclipse/teamserver/preferences/PreferenceConstants.java
+++ src/com/surelogic/sierra/eclipse/teamserver/preferences/PreferenceConstants.java
@@ -14,24 +14,24 @@ public class PreferenceConstants {
 	public static final String P_SERVER_STAYS_RUNNING = "com.surelogic.sierra.eclipse.teamserver.staysRunning";
 
 	public static boolean warnAboutServerStaysRunning() {
-		return Activator.getDefault().getPluginPreferences().getBoolean(
+		return Activator.getDefault().getPreferenceStore().getBoolean(
 				P_SERVER_STAYS_RUNNING);
 	}
 
 	public static void setWarnAboutServerStaysRunning(boolean value) {
-		Activator.getDefault().getPluginPreferences().setValue(
+		Activator.getDefault().getPreferenceStore().setValue(
 				P_SERVER_STAYS_RUNNING, value);
 	}
 
 	public static final String P_LOG_VISIBLE = "com.surelogic.sierra.eclipse.teamserver.logVisible";
 
 	public static boolean isLogVisible() {
-		return Activator.getDefault().getPluginPreferences().getBoolean(
+		return Activator.getDefault().getPreferenceStore().getBoolean(
 				P_LOG_VISIBLE);
 	}
 
 	public static void setLogVisible(boolean value) {
-		Activator.getDefault().getPluginPreferences().setValue(P_LOG_VISIBLE,
+		Activator.getDefault().getPreferenceStore().setValue(P_LOG_VISIBLE,
 				value);
 	}
 
@@ -46,41 +46,41 @@ public class PreferenceConstants {
 	 * @return 0 for Jetty console log, 1 for Jetty request log.
 	 */
 	public static int getLogShowing() {
-		return Activator.getDefault().getPluginPreferences().getInt(
-				P_LOG_SHOWING);
+		return Activator.getDefault().getPreferenceStore()
+				.getInt(P_LOG_SHOWING);
 	}
 
 	public static void setLogShowing(int value) {
-		Activator.getDefault().getPluginPreferences().setValue(P_LOG_SHOWING,
+		Activator.getDefault().getPreferenceStore().setValue(P_LOG_SHOWING,
 				value);
 	}
 
 	public static final String P_PORT = "com.surelogic.sierra.eclipse.teamserver.port";
 
 	public static int getPort() {
-		return Activator.getDefault().getPluginPreferences().getInt(P_PORT);
+		return Activator.getDefault().getPreferenceStore().getInt(P_PORT);
 	}
 
 	public static void setPort(int value) {
-		Activator.getDefault().getPluginPreferences().setValue(P_PORT, value);
+		Activator.getDefault().getPreferenceStore().setValue(P_PORT, value);
 	}
 
 	public static final String P_SERVER_MEMORY_MB = "com.surelogic.sierra.teamserver.server-memory-mb";
 
 	public static int getServerMemoryMB() {
-		return Activator.getDefault().getPluginPreferences().getInt(
+		return Activator.getDefault().getPreferenceStore().getInt(
 				P_SERVER_MEMORY_MB);
 	}
 
 	public static void setServerMemoryMB(int mb) {
-		Activator.getDefault().getPluginPreferences().setValue(
+		Activator.getDefault().getPreferenceStore().setValue(
 				P_SERVER_MEMORY_MB, mb);
 	}
 
 	public static final String P_SERVER_LOGGING_LEVEL = "com.surelogic.sierra.teamserver.server-logging-level";
 
 	public static Level getServerLoggingLevel() {
-		final String level = Activator.getDefault().getPluginPreferences()
+		final String level = Activator.getDefault().getPreferenceStore()
 				.getString(P_SERVER_LOGGING_LEVEL);
 		return Level.parse(level);
 	}
