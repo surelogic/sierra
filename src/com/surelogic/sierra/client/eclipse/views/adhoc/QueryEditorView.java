695ac8f853b2d8da5411ea9f308082c624fa066d
Tim Halloran <tim.halloran@surelogic.com>
2011-05-19 15:59:23 +0000

    Removed old usage tracking system -- not really useful. This also opens up the "~/.surelogic" file for use by folks.
    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/sierra-client-eclipse@17561 d02ae147-ee12-4a35-9452-91c89e8b2592

--- src/com/surelogic/sierra/client/eclipse/views/adhoc/QueryEditorView.java
+++ src/com/surelogic/sierra/client/eclipse/views/adhoc/QueryEditorView.java
@@ -1,22 +1,12 @@
 package com.surelogic.sierra.client.eclipse.views.adhoc;
 
-import org.eclipse.swt.widgets.Composite;
-
-import com.surelogic.common.ui.adhoc.views.editor.AbstractQueryEditorView;
 import com.surelogic.common.adhoc.AdHocManager;
-import com.surelogic.common.serviceability.UsageMeter;
+import com.surelogic.common.ui.adhoc.views.editor.AbstractQueryEditorView;
 
 public final class QueryEditorView extends AbstractQueryEditorView {
-
-	@Override
-	public void createPartControl(final Composite parent) {
-		UsageMeter.getInstance().tickUse("Sierra QueryEditorView opened");
-		super.createPartControl(parent);
-	}
 
 	@Override
 	public AdHocManager getManager() {
 		return AdHocDataSource.getManager();
 	}
-
 }
--- src/com/surelogic/sierra/client/eclipse/views/adhoc/QueryResultsView.java
+++ src/com/surelogic/sierra/client/eclipse/views/adhoc/QueryResultsView.java
@@ -1,18 +1,9 @@
 package com.surelogic.sierra.client.eclipse.views.adhoc;
 
-import org.eclipse.swt.widgets.Composite;
-
-import com.surelogic.common.ui.adhoc.views.results.AbstractQueryResultsView;
 import com.surelogic.common.adhoc.AdHocManager;
-import com.surelogic.common.serviceability.UsageMeter;
+import com.surelogic.common.ui.adhoc.views.results.AbstractQueryResultsView;
 
 public final class QueryResultsView extends AbstractQueryResultsView {
-
-	@Override
-	public void createPartControl(final Composite parent) {
-		UsageMeter.getInstance().tickUse("Sierra QueryResultsView opened");
-		super.createPartControl(parent);
-	}
 
 	@Override
 	public AdHocManager getManager() {
