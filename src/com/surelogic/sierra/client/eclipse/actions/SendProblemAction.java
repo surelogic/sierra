f4baf0e051d0d86ac179b38b9d70d7e1b7ed3498
Tim Halloran <tim.halloran@surelogic.com>
2011-06-01 20:18:32 +0000

    Fixed tip/problem report wizard so submits to www.surelogic.com
    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/sierra-client-eclipse@17639 d02ae147-ee12-4a35-9452-91c89e8b2592

--- src/com/surelogic/sierra/client/eclipse/actions/SendProblemAction.java
+++ src/com/surelogic/sierra/client/eclipse/actions/SendProblemAction.java
@@ -7,8 +7,8 @@ import org.eclipse.ui.IWorkbenchWindowActionDelegate;
 
 import com.surelogic.common.CommonImages;
 import com.surelogic.common.core.EclipseUtility;
-import com.surelogic.common.ui.EclipseUIUtility;
-import com.surelogic.common.ui.dialogs.SendProblemReportDialog;
+import com.surelogic.common.license.SLLicenseProduct;
+import com.surelogic.common.ui.serviceability.SendServiceMessageWizard;
 import com.surelogic.sierra.client.eclipse.Activator;
 
 public final class SendProblemAction implements IWorkbenchWindowActionDelegate {
@@ -22,8 +22,8 @@ public final class SendProblemAction implements IWorkbenchWindowActionDelegate {
 	}
 
 	public void run(IAction action) {
-		SendProblemReportDialog.open(EclipseUIUtility.getShell(), "Sierra "
-				+ EclipseUtility.getVersion(Activator.getDefault()),
+		SendServiceMessageWizard.openProblemReport(SLLicenseProduct.SIERRA
+				+ " " + EclipseUtility.getVersion(Activator.getDefault()),
 				CommonImages.IMG_SIERRA_LOGO);
 	}
 
