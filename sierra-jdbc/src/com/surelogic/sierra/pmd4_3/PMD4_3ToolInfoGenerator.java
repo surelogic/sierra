58eb90efe5dbc7ea4c707be04a7a4f873b1cbea6
Edwin Chan <edwin.chan@surelogic.com>
2012-01-13 15:32:43 -0500

    Added code to prepare for FB 2.0 and PMD 4.3

---
+++ sierra-jdbc/src/com/surelogic/sierra/pmd4_3/PMD4_3ToolInfoGenerator.java
@@ -0,0 +1,45 @@
+package com.surelogic.sierra.pmd4_3;
+
+import java.sql.Connection;
+import java.util.StringTokenizer;
+import java.util.logging.Logger;
+
+import javax.xml.parsers.SAXParser;
+
+import com.surelogic.common.xml.XMLUtil;
+import com.surelogic.sierra.pmd.AbstractPMDToolInfoGenerator;
+
+/**
+ * Generates the tool and finding type information for pmd. 
+ * 
+ * @author nathan
+ * 
+ */
+public final class PMD4_3ToolInfoGenerator extends AbstractPMDToolInfoGenerator {
+	private static final Logger log = Logger
+			.getLogger(PMD4_3ToolInfoGenerator.class.getName());
+
+	/**
+	 * Load the rulesets and persist them to the embedded database.
+	 * 
+	 */
+	public static void generateTool(Connection conn) {
+		PMD4_3ToolInfoGenerator handler = new PMD4_3ToolInfoGenerator(conn);
+    SAXParser sp = XMLUtil.createSAXParser();
+    ClassLoader cl = Thread.currentThread().getContextClassLoader();
+    String rulesetFilenames = getRulesetNames(log, cl.getResourceAsStream(
+                                                   "com/surelogic/sierra/pmd4_3/rulesets/rulesets.properties"));
+
+		for (StringTokenizer st = new StringTokenizer(rulesetFilenames, ","); st
+				.hasMoreTokens();) {
+			String fileName = st.nextToken();
+			XMLUtil.parseResource(log, sp, 
+			                      cl.getResourceAsStream("com/surelogic/sierra/pmd4_3/" + fileName), 
+			                      handler, "Could not parse a PMD ruleset");
+		}
+	}
+
+	private PMD4_3ToolInfoGenerator(Connection conn) {
+    super(conn, "4.3");
+	}
+}
---
+++ sierra-jdbc/src/com/surelogic/sierra/schema/Schema_0003.java
@@ -0,0 +1,20 @@
+package com.surelogic.sierra.schema;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import com.surelogic.common.jdbc.SchemaAction;
+import com.surelogic.sierra.findbugs2_0_0.*;
+import com.surelogic.sierra.pmd4_3.PMD4_3ToolInfoGenerator;
+
+public class Schema_0003 implements SchemaAction {
+
+	public void run(final Connection conn) throws SQLException {
+		PMD4_3ToolInfoGenerator.generateTool(conn);
+		FindBugs2_0_0ToolInfoGenerator.generateTool(conn);
+		SchemaUtil.updateFindingTypes(conn);
+		SchemaUtil.setupLocalScanFilter(conn);
+		SchemaUtil.generateBuglinkCategories(conn);
+	}
+
+}
