88ff28a639ecfbc8b39588538a26fee553d7a1aa
Tim Halloran <tim.halloran@surelogic.com>
2012-12-10 15:25:12 -0500

    Moved Sierra class to Sierra.

--- sierra-client-eclipse/src/com/surelogic/sierra/client/eclipse/jobs/ShareScanJob.java
+++ sierra-client-eclipse/src/com/surelogic/sierra/client/eclipse/jobs/ShareScanJob.java
@@ -13,7 +13,6 @@ import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Status;
 
 import com.surelogic.common.core.jobs.SLProgressMonitorWrapper;
-import com.surelogic.common.core.jobs.ShareScanRule;
 import com.surelogic.common.core.logging.SLEclipseStatusUtility;
 import com.surelogic.common.i18n.I18N;
 import com.surelogic.common.jobs.SLProgressMonitor;
