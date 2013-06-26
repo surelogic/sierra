adbbdf8492665cf93b2a321096746ae351a38c77
Tim Halloran <tim.halloran@surelogic.com>
2013-06-26 15:42:36 -0400

    Using utility method to check if local team server is installed.

--- sierra-client-eclipse/src/com/surelogic/sierra/client/eclipse/perspectives/CodeReviewPerspective.java
+++ sierra-client-eclipse/src/com/surelogic/sierra/client/eclipse/perspectives/CodeReviewPerspective.java
@@ -5,7 +5,6 @@ import org.eclipse.ui.IPageLayout;
 import org.eclipse.ui.IPerspectiveFactory;
 
 import com.surelogic.common.core.EclipseUtility;
-import com.surelogic.common.logging.SLLogger;
 import com.surelogic.sierra.client.eclipse.views.FindingDetailsView;
 import com.surelogic.sierra.client.eclipse.views.SierraServersView;
 import com.surelogic.sierra.client.eclipse.views.SynchronizeDetailsView;
@@ -17,35 +16,29 @@ import com.surelogic.sierra.client.eclipse.views.selection.FindingsSelectionView
  */
 public final class CodeReviewPerspective implements IPerspectiveFactory {
 
-	@Override
+  @Override
   public void createInitialLayout(IPageLayout layout) {
-		final String localTeamServerId = "com.surelogic.sierra.eclipse.teamserver";
-		final String localTeamServerView = "com.surelogic.sierra.eclipse.teamserver.views.TeamServerView";
-		final String packageExplorer = "org.eclipse.jdt.ui.PackageExplorer";
-		final String editorArea = layout.getEditorArea();
-		final String quickSearchArea = FindingsSelectionView.ID;
+    final String localTeamServerView = "com.surelogic.sierra.eclipse.teamserver.views.TeamServerView";
+    final String packageExplorer = "org.eclipse.jdt.ui.PackageExplorer";
+    final String editorArea = layout.getEditorArea();
+    final String quickSearchArea = FindingsSelectionView.ID;
 
-		final IFolderLayout aboveEditorArea = layout.createFolder(
-				"aboveEditorArea", IPageLayout.TOP, 0.4f, editorArea);
-		aboveEditorArea.addView(quickSearchArea);
-		aboveEditorArea.addView(packageExplorer);
-		/*
-		 * The local team server view only will exist if that optional feature
-		 * is loaded. So we need to check before we add this to the perspective.
-		 */
-		if (EclipseUtility.bundleExists(localTeamServerId)) {
-			aboveEditorArea.addView(localTeamServerView);
-		} else {
-			SLLogger.getLogger().fine("No team server");
-		}
+    final IFolderLayout aboveEditorArea = layout.createFolder("aboveEditorArea", IPageLayout.TOP, 0.4f, editorArea);
+    aboveEditorArea.addView(quickSearchArea);
+    aboveEditorArea.addView(packageExplorer);
+    /*
+     * The local team server view only will exist if that optional feature is
+     * loaded. So we need to check before we add this to the perspective.
+     */
+    if (EclipseUtility.isLocalTeamServerInstalled()) {
+      aboveEditorArea.addView(localTeamServerView);
+    }
 
-		final IFolderLayout leftOfEditorArea = layout.createFolder(
-				"leftOfEditorArea", IPageLayout.LEFT, 0.4f, editorArea);
-		leftOfEditorArea.addView(FindingDetailsView.ID);
-		leftOfEditorArea.addView(SierraServersView.ID);
-		leftOfEditorArea.addView(SynchronizeView.ID);
+    final IFolderLayout leftOfEditorArea = layout.createFolder("leftOfEditorArea", IPageLayout.LEFT, 0.4f, editorArea);
+    leftOfEditorArea.addView(FindingDetailsView.ID);
+    leftOfEditorArea.addView(SierraServersView.ID);
+    leftOfEditorArea.addView(SynchronizeView.ID);
 
-		layout.addPlaceholder(SynchronizeDetailsView.ID, IPageLayout.BOTTOM,
-				0.6f, editorArea);
-	}
+    layout.addPlaceholder(SynchronizeDetailsView.ID, IPageLayout.BOTTOM, 0.6f, editorArea);
+  }
 }
