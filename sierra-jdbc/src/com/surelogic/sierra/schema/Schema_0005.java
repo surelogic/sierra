7e0faae05d4307792f1e80c2e9a3117ab96559f6
edwin <edwin.chan@surelogic.com>
2014-07-21 14:40:34 -0400

    Updated for FindBugs 3.0.0

--- sierra-jdbc/src/com/surelogic/sierra/schema/Schema_0005.java
+++ sierra-jdbc/src/com/surelogic/sierra/schema/Schema_0005.java
@@ -5,6 +5,7 @@ import java.sql.SQLException;
 
 import com.surelogic.common.jdbc.SchemaAction;
 import com.surelogic.sierra.cpd.*;
+import com.surelogic.sierra.findbugs3_0_0.FindBugs3_0_0ToolInfoGenerator;
 import com.surelogic.sierra.pmd5_1_1.PMD5_1_1ToolInfoGenerator;
 
 public class Schema_0005 implements SchemaAction {
@@ -13,7 +14,7 @@ public class Schema_0005 implements SchemaAction {
     public void run(Connection conn) throws SQLException {
 		CPD5_1_1ToolInfoGenerator.generateTool(conn);
 		PMD5_1_1ToolInfoGenerator.generateTool(conn);
-		//FindBugs2_0_3ToolInfoGenerator.generateTool(conn);
+		FindBugs3_0_0ToolInfoGenerator.generateTool(conn);
 		SchemaUtil.updateFindingTypes(conn);
 		SchemaUtil.setupLocalScanFilter(conn);
 		SchemaUtil.generateBuglinkCategories(conn);
