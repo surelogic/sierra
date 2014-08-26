e33083c6e3cee00fb0f421ff9a0727ea5f1afaaa
Tim Halloran <tim.halloran@surelogic.com>
2014-08-26 11:26:55 -0400

    Commas added to memory sizes in display

--- sierra-client-eclipse/src/com/surelogic/sierra/client/eclipse/preferences/SierraPreferencePage.java
+++ sierra-client-eclipse/src/com/surelogic/sierra/client/eclipse/preferences/SierraPreferencePage.java
@@ -16,6 +16,7 @@ import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Listener;
 import org.eclipse.ui.PlatformUI;
 
+import com.surelogic.common.SLUtility;
 import com.surelogic.common.XUtil;
 import com.surelogic.common.core.EclipseUtility;
 import com.surelogic.common.core.MemoryUtility;
@@ -27,210 +28,173 @@ import com.surelogic.sierra.client.eclipse.views.adhoc.SierraDataSource;
 import com.surelogic.sierra.tool.message.Importance;
 
 public class SierraPreferencePage extends AbstractCommonPreferencePage {
-	static private final String TOOL_MB_LABEL = "sierra.eclipse.preference.page.toolMemoryPreferenceLabel";
+  static private final String TOOL_MB_LABEL = "sierra.eclipse.preference.page.toolMemoryPreferenceLabel";
 
-	private BooleanFieldEditor f_balloonFlag;
-	private BooleanFieldEditor f_selectProjectsToScan;
-	private BooleanFieldEditor f_showMarkersInJavaEditorFlag;
-	private RadioGroupFieldEditor f_showAbove;
-	private BooleanFieldEditor f_saveResources;
-	private IntegerFieldEditor f_findingsListLimit;
-	private Label f_estimate;
-	private ScaleFieldEditor f_toolMemoryMB;
+  private BooleanFieldEditor f_balloonFlag;
+  private BooleanFieldEditor f_selectProjectsToScan;
+  private BooleanFieldEditor f_showMarkersInJavaEditorFlag;
+  private RadioGroupFieldEditor f_showAbove;
+  private BooleanFieldEditor f_saveResources;
+  private IntegerFieldEditor f_findingsListLimit;
+  private Label f_estimate;
+  private ScaleFieldEditor f_toolMemoryMB;
 
-	public SierraPreferencePage() {
-		super("sierra.eclipse.", SierraPreferencesUtility
-				.getSwitchPreferences());
-	}
+  public SierraPreferencePage() {
+    super("sierra.eclipse.", SierraPreferencesUtility.getSwitchPreferences());
+  }
 
-	@Override
-	protected Control createContents(final Composite parent) {
-		final Composite panel = new Composite(parent, SWT.NONE);
-		final GridLayout grid = new GridLayout();
-		panel.setLayout(grid);
+  @Override
+  protected Control createContents(final Composite parent) {
+    final Composite panel = new Composite(parent, SWT.NONE);
+    final GridLayout grid = new GridLayout();
+    panel.setLayout(grid);
 
-		final Group dataGroup = new Group(panel, SWT.NONE);
-		dataGroup.setLayoutData(new GridData(SWT.FILL, SWT.TOP, true, false));
-		dataGroup
-				.setText(I18N.msg("sierra.eclipse.preference.page.group.data"));
-		dataGroup.setLayout(new GridLayout());
+    final Group dataGroup = new Group(panel, SWT.NONE);
+    dataGroup.setLayoutData(new GridData(SWT.FILL, SWT.TOP, true, false));
+    dataGroup.setText(I18N.msg("sierra.eclipse.preference.page.group.data"));
+    dataGroup.setLayout(new GridLayout());
 
-		final Label dataDirectory = new Label(dataGroup, SWT.NONE);
-		dataDirectory.setText(SierraPreferencesUtility.getSierraDataDirectory()
-				.getAbsolutePath());
-		dataDirectory.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true,
-				false));
+    final Label dataDirectory = new Label(dataGroup, SWT.NONE);
+    dataDirectory.setText(SierraPreferencesUtility.getSierraDataDirectory().getAbsolutePath());
+    dataDirectory.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false));
 
-		final Group diGroup = new Group(panel, SWT.NONE);
-		diGroup.setLayoutData(new GridData(SWT.FILL, SWT.TOP, true, false));
-		diGroup.setText(I18N.msg("sierra.eclipse.preference.page.group.app"));
+    final Group diGroup = new Group(panel, SWT.NONE);
+    diGroup.setLayoutData(new GridData(SWT.FILL, SWT.TOP, true, false));
+    diGroup.setText(I18N.msg("sierra.eclipse.preference.page.group.app"));
 
-		f_findingsListLimit = new IntegerFieldEditor(
-				SierraPreferencesUtility.FINDINGS_LIST_CUTOFF,
-				I18N.msg("sierra.eclipse.preference.page.findingsListLimit"),
-				diGroup);
-		f_findingsListLimit.fillIntoGrid(diGroup, 2);
-		f_findingsListLimit.setPage(this);
-		f_findingsListLimit.setPreferenceStore(EclipseUIUtility
-				.getPreferences());
-		f_findingsListLimit.load();
+    f_findingsListLimit = new IntegerFieldEditor(SierraPreferencesUtility.FINDINGS_LIST_CUTOFF,
+        I18N.msg("sierra.eclipse.preference.page.findingsListLimit"), diGroup);
+    f_findingsListLimit.fillIntoGrid(diGroup, 2);
+    f_findingsListLimit.setPage(this);
+    f_findingsListLimit.setPreferenceStore(EclipseUIUtility.getPreferences());
+    f_findingsListLimit.load();
 
-		f_balloonFlag = new BooleanFieldEditor(
-				SierraPreferencesUtility.SHOW_BALLOON_NOTIFICATIONS,
-				I18N.msg("sierra.eclipse.preference.page.balloonFlag"), diGroup);
-		f_balloonFlag.fillIntoGrid(diGroup, 2);
-		f_balloonFlag.setPage(this);
-		f_balloonFlag.setPreferenceStore(EclipseUIUtility.getPreferences());
-		f_balloonFlag.load();
+    f_balloonFlag = new BooleanFieldEditor(SierraPreferencesUtility.SHOW_BALLOON_NOTIFICATIONS,
+        I18N.msg("sierra.eclipse.preference.page.balloonFlag"), diGroup);
+    f_balloonFlag.fillIntoGrid(diGroup, 2);
+    f_balloonFlag.setPage(this);
+    f_balloonFlag.setPreferenceStore(EclipseUIUtility.getPreferences());
+    f_balloonFlag.load();
 
-		setupForPerspectiveSwitch(diGroup);
+    setupForPerspectiveSwitch(diGroup);
 
-		f_selectProjectsToScan = new BooleanFieldEditor(
-				SierraPreferencesUtility.ALWAYS_ALLOW_USER_TO_SELECT_PROJECTS_TO_SCAN,
-				I18N.msg("sierra.eclipse.preference.page.selectProjectsToScan"),
-				diGroup);
-		f_selectProjectsToScan.fillIntoGrid(diGroup, 2);
-		f_selectProjectsToScan.setPage(this);
-		f_selectProjectsToScan.setPreferenceStore(EclipseUIUtility
-				.getPreferences());
-		f_selectProjectsToScan.load();
+    f_selectProjectsToScan = new BooleanFieldEditor(SierraPreferencesUtility.ALWAYS_ALLOW_USER_TO_SELECT_PROJECTS_TO_SCAN,
+        I18N.msg("sierra.eclipse.preference.page.selectProjectsToScan"), diGroup);
+    f_selectProjectsToScan.fillIntoGrid(diGroup, 2);
+    f_selectProjectsToScan.setPage(this);
+    f_selectProjectsToScan.setPreferenceStore(EclipseUIUtility.getPreferences());
+    f_selectProjectsToScan.load();
 
-		f_showMarkersInJavaEditorFlag = new BooleanFieldEditor(
-				SierraPreferencesUtility.SHOW_MARKERS,
-				I18N.msg("sierra.eclipse.preference.page.showMarkersInJavaEditorFlag"),
-				diGroup);
-		f_showMarkersInJavaEditorFlag.fillIntoGrid(diGroup, 2);
-		f_showMarkersInJavaEditorFlag.setPage(this);
-		f_showMarkersInJavaEditorFlag.setPreferenceStore(EclipseUIUtility
-				.getPreferences());
-		f_showMarkersInJavaEditorFlag.load();
+    f_showMarkersInJavaEditorFlag = new BooleanFieldEditor(SierraPreferencesUtility.SHOW_MARKERS,
+        I18N.msg("sierra.eclipse.preference.page.showMarkersInJavaEditorFlag"), diGroup);
+    f_showMarkersInJavaEditorFlag.fillIntoGrid(diGroup, 2);
+    f_showMarkersInJavaEditorFlag.setPage(this);
+    f_showMarkersInJavaEditorFlag.setPreferenceStore(EclipseUIUtility.getPreferences());
+    f_showMarkersInJavaEditorFlag.load();
 
-		f_showAbove = new RadioGroupFieldEditor(
-				SierraPreferencesUtility.SHOW_MARKERS_AT_OR_ABOVE_IMPORTANCE,
-				I18N.msg("sierra.eclipse.preference.page.showAbove"), 1,
-				new String[][] {
-						{ Importance.CRITICAL.toStringSentenceCase(),
-								Importance.CRITICAL.toString() },
-						{ Importance.HIGH.toStringSentenceCase(),
-								Importance.HIGH.toString() },
-						{ Importance.MEDIUM.toStringSentenceCase(),
-								Importance.MEDIUM.toString() },
-						{ Importance.LOW.toStringSentenceCase(),
-								Importance.LOW.toString() },
-						{ Importance.IRRELEVANT.toStringSentenceCase(),
-								Importance.IRRELEVANT.toString() } },
+    f_showAbove = new RadioGroupFieldEditor(SierraPreferencesUtility.SHOW_MARKERS_AT_OR_ABOVE_IMPORTANCE,
+        I18N.msg("sierra.eclipse.preference.page.showAbove"), 1, new String[][] {
+            { Importance.CRITICAL.toStringSentenceCase(), Importance.CRITICAL.toString() },
+            { Importance.HIGH.toStringSentenceCase(), Importance.HIGH.toString() },
+            { Importance.MEDIUM.toStringSentenceCase(), Importance.MEDIUM.toString() },
+            { Importance.LOW.toStringSentenceCase(), Importance.LOW.toString() },
+            { Importance.IRRELEVANT.toStringSentenceCase(), Importance.IRRELEVANT.toString() } },
 
-				diGroup);
-		f_showAbove.fillIntoGrid(diGroup, 2);
-		f_showAbove.setPage(this);
-		f_showAbove.setPreferenceStore(EclipseUIUtility.getPreferences());
-		f_showAbove.load();
+        diGroup);
+    f_showAbove.fillIntoGrid(diGroup, 2);
+    f_showAbove.setPage(this);
+    f_showAbove.setPreferenceStore(EclipseUIUtility.getPreferences());
+    f_showAbove.load();
 
-		diGroup.setLayout(new GridLayout(2, false));
+    diGroup.setLayout(new GridLayout(2, false));
 
-		final Group memoryGroup = new Group(panel, SWT.NONE);
-		memoryGroup.setLayoutData(new GridData(SWT.FILL, SWT.TOP, true, false));
-		memoryGroup.setText(I18N
-				.msg("sierra.eclipse.preference.page.group.scan"));
+    final Group memoryGroup = new Group(panel, SWT.NONE);
+    memoryGroup.setLayoutData(new GridData(SWT.FILL, SWT.TOP, true, false));
+    memoryGroup.setText(I18N.msg("sierra.eclipse.preference.page.group.scan"));
 
-		final int estimatedMax = MemoryUtility.computeMaxMemorySizeInMb();
-		int mb = EclipseUtility
-				.getIntPreference(SierraPreferencesUtility.TOOL_MEMORY_MB);
-		if (mb > estimatedMax) {
-			mb = estimatedMax;
-			EclipseUtility.setIntPreference(
-					SierraPreferencesUtility.TOOL_MEMORY_MB, mb);
-		}
+    final int estimatedMax = MemoryUtility.computeMaxMemorySizeInMb();
+    int mb = EclipseUtility.getIntPreference(SierraPreferencesUtility.TOOL_MEMORY_MB);
+    if (mb > estimatedMax) {
+      mb = estimatedMax;
+      EclipseUtility.setIntPreference(SierraPreferencesUtility.TOOL_MEMORY_MB, mb);
+    }
 
-		final String label = I18N.msg(TOOL_MB_LABEL, mb);
-		f_toolMemoryMB = new ScaleFieldEditor(
-				SierraPreferencesUtility.TOOL_MEMORY_MB, label + "     ",
-				memoryGroup);
-		f_toolMemoryMB.fillIntoGrid(memoryGroup, 2);
-		f_toolMemoryMB.setMinimum(256);
-		f_toolMemoryMB.setMaximum(estimatedMax);
-		f_toolMemoryMB.setPageIncrement(256);
-		f_toolMemoryMB.setPage(this);
-		f_toolMemoryMB.setPreferenceStore(EclipseUIUtility.getPreferences());
-		f_toolMemoryMB.load();
-		f_toolMemoryMB.getScaleControl().addListener(SWT.Selection,
-				new Listener() {
-					@Override
-					public void handleEvent(final Event event) {
-						updateMBInLabel();
-					}
-				});
+    final String label = I18N.msg(TOOL_MB_LABEL, SLUtility.toStringHumanWithCommas(mb));
+    f_toolMemoryMB = new ScaleFieldEditor(SierraPreferencesUtility.TOOL_MEMORY_MB, label + "     ", memoryGroup);
+    f_toolMemoryMB.fillIntoGrid(memoryGroup, 2);
+    f_toolMemoryMB.setMinimum(256);
+    f_toolMemoryMB.setMaximum(estimatedMax);
+    f_toolMemoryMB.setPageIncrement(256);
+    f_toolMemoryMB.setPage(this);
+    f_toolMemoryMB.setPreferenceStore(EclipseUIUtility.getPreferences());
+    f_toolMemoryMB.load();
+    f_toolMemoryMB.getScaleControl().addListener(SWT.Selection, new Listener() {
+      @Override
+      public void handleEvent(final Event event) {
+        updateMBInLabel();
+      }
+    });
 
-		f_estimate = new Label(memoryGroup, SWT.NONE);
-		f_estimate.setLayoutData(new GridData(SWT.LEFT, SWT.CENTER, false,
-				false, 2, 1));
-		f_estimate.setText(I18N.msg(
-				"sierra.eclipse.preference.page.computedMaxToolMemoryLabel",
-				estimatedMax));
+    f_estimate = new Label(memoryGroup, SWT.NONE);
+    f_estimate.setLayoutData(new GridData(SWT.LEFT, SWT.CENTER, false, false, 2, 1));
+    f_estimate.setText(I18N.msg("sierra.eclipse.preference.page.computedMaxToolMemoryLabel",
+        SLUtility.toStringHumanWithCommas(estimatedMax)));
 
-		f_saveResources = new BooleanFieldEditor(
-				SierraPreferencesUtility.ALWAYS_SAVE_RESOURCES,
-				I18N.msg("sierra.eclipse.preference.page.saveModified"),
-				memoryGroup);
-		f_saveResources.fillIntoGrid(memoryGroup, 2);
-		f_saveResources.setPage(this);
-		f_saveResources.setPreferenceStore(EclipseUIUtility.getPreferences());
-		f_saveResources.load();
+    f_saveResources = new BooleanFieldEditor(SierraPreferencesUtility.ALWAYS_SAVE_RESOURCES,
+        I18N.msg("sierra.eclipse.preference.page.saveModified"), memoryGroup);
+    f_saveResources.fillIntoGrid(memoryGroup, 2);
+    f_saveResources.setPage(this);
+    f_saveResources.setPreferenceStore(EclipseUIUtility.getPreferences());
+    f_saveResources.load();
 
-		memoryGroup.setLayout(new GridLayout(2, false));
+    memoryGroup.setLayout(new GridLayout(2, false));
 
-		/*
-		 * Allow access to help via the F1 key.
-		 */
-		PlatformUI
-				.getWorkbench()
-				.getHelpSystem()
-				.setHelp(parent,
-						"com.surelogic.sierra.client.eclipse.preferences-sierra");
-		if (XUtil.useExperimental) {
-			final Button exportButton = new Button(parent, SWT.PUSH);
-			exportButton.setText("Export New Queries File");
-			exportButton.setLayoutData(new GridData(SWT.DEFAULT, SWT.DEFAULT,
-					false, false));
-			exportButton.addListener(SWT.Selection, new Listener() {
-				@Override
-				public void handleEvent(final Event event) {
-					new ExportQueryDialog(EclipseUIUtility.getShell(),
-							SierraDataSource.getManager()).open();
-				}
-			});
-		}
-		return panel;
-	}
+    /*
+     * Allow access to help via the F1 key.
+     */
+    PlatformUI.getWorkbench().getHelpSystem().setHelp(parent, "com.surelogic.sierra.client.eclipse.preferences-sierra");
+    if (XUtil.useExperimental) {
+      final Button exportButton = new Button(parent, SWT.PUSH);
+      exportButton.setText("Export New Queries File");
+      exportButton.setLayoutData(new GridData(SWT.DEFAULT, SWT.DEFAULT, false, false));
+      exportButton.addListener(SWT.Selection, new Listener() {
+        @Override
+        public void handleEvent(final Event event) {
+          new ExportQueryDialog(EclipseUIUtility.getShell(), SierraDataSource.getManager()).open();
+        }
+      });
+    }
+    return panel;
+  }
 
-	private void updateMBInLabel() {
-		final int mb = f_toolMemoryMB.getScaleControl().getSelection();
-		f_toolMemoryMB.setLabelText(I18N.msg(TOOL_MB_LABEL, mb));
-	}
+  void updateMBInLabel() {
+    final int mb = f_toolMemoryMB.getScaleControl().getSelection();
+    f_toolMemoryMB.setLabelText(I18N.msg(TOOL_MB_LABEL, SLUtility.toStringHumanWithCommas(mb)));
+  }
 
-	@Override
-	protected void performDefaults() {
-		f_balloonFlag.loadDefault();
-		f_selectProjectsToScan.loadDefault();
-		f_showMarkersInJavaEditorFlag.loadDefault();
-		f_showAbove.loadDefault();
-		f_saveResources.loadDefault();
-		f_findingsListLimit.loadDefault();
-		f_toolMemoryMB.loadDefault();
-		super.performDefaults();
+  @Override
+  protected void performDefaults() {
+    f_balloonFlag.loadDefault();
+    f_selectProjectsToScan.loadDefault();
+    f_showMarkersInJavaEditorFlag.loadDefault();
+    f_showAbove.loadDefault();
+    f_saveResources.loadDefault();
+    f_findingsListLimit.loadDefault();
+    f_toolMemoryMB.loadDefault();
+    super.performDefaults();
 
-		updateMBInLabel();
-	}
+    updateMBInLabel();
+  }
 
-	@Override
-	public boolean performOk() {
-		f_balloonFlag.store();
-		f_selectProjectsToScan.store();
-		f_showMarkersInJavaEditorFlag.store();
-		f_showAbove.store();
-		f_saveResources.store();
-		f_findingsListLimit.store();
-		f_toolMemoryMB.store();
-		return super.performOk();
-	}
+  @Override
+  public boolean performOk() {
+    f_balloonFlag.store();
+    f_selectProjectsToScan.store();
+    f_showMarkersInJavaEditorFlag.store();
+    f_showAbove.store();
+    f_saveResources.store();
+    f_findingsListLimit.store();
+    f_toolMemoryMB.store();
+    return super.performOk();
+  }
 }
--- sierra-team-server-client-eclipse/src/com/surelogic/sierra/eclipse/teamserver/preferences/LocalTeamServerPreferencePage.java
+++ sierra-team-server-client-eclipse/src/com/surelogic/sierra/eclipse/teamserver/preferences/LocalTeamServerPreferencePage.java
@@ -18,125 +18,105 @@ import org.eclipse.ui.IWorkbench;
 import org.eclipse.ui.IWorkbenchPreferencePage;
 import org.eclipse.ui.PlatformUI;
 
+import com.surelogic.common.SLUtility;
 import com.surelogic.common.core.MemoryUtility;
 import com.surelogic.common.i18n.I18N;
 import com.surelogic.common.ui.EclipseUIUtility;
 
-public class LocalTeamServerPreferencePage extends PreferencePage implements
-		IWorkbenchPreferencePage {
+public class LocalTeamServerPreferencePage extends PreferencePage implements IWorkbenchPreferencePage {
 
-	static private final String SERVER_MB_LABEL = "sierra.eclipse.teamserver.preference.page.serverMemoryPreferenceLabel";
+  static private final String SERVER_MB_LABEL = "sierra.eclipse.teamserver.preference.page.serverMemoryPreferenceLabel";
 
-	Label f_estimate;
-	ScaleFieldEditor f_serverMemoryMB;
-	RadioGroupFieldEditor f_showAbove;
+  Label f_estimate;
+  ScaleFieldEditor f_serverMemoryMB;
+  RadioGroupFieldEditor f_showAbove;
 
-	@Override
+  @Override
   public void init(IWorkbench workbench) {
-		setPreferenceStore(EclipseUIUtility.getPreferences());
-		setDescription(I18N
-				.msg("sierra.eclipse.teamserver.preference.page.msg"));
-	}
+    setPreferenceStore(EclipseUIUtility.getPreferences());
+    setDescription(I18N.msg("sierra.eclipse.teamserver.preference.page.msg"));
+  }
 
-	@Override
-	protected Control createContents(Composite parent) {
-		final Composite panel = new Composite(parent, SWT.NONE);
-		GridLayout grid = new GridLayout();
-		panel.setLayout(grid);
+  @Override
+  protected Control createContents(Composite parent) {
+    final Composite panel = new Composite(parent, SWT.NONE);
+    GridLayout grid = new GridLayout();
+    panel.setLayout(grid);
 
-		final Group memoryGroup = new Group(panel, SWT.NONE);
-		memoryGroup.setLayoutData(new GridData(SWT.FILL, SWT.TOP, true, false));
-		memoryGroup.setText(I18N
-				.msg("sierra.eclipse.teamserver.preference.page.group.memory"));
+    final Group memoryGroup = new Group(panel, SWT.NONE);
+    memoryGroup.setLayoutData(new GridData(SWT.FILL, SWT.TOP, true, false));
+    memoryGroup.setText(I18N.msg("sierra.eclipse.teamserver.preference.page.group.memory"));
 
-		final int estimatedMax = MemoryUtility.computeMaxMemorySizeInMb();
-		int mb = LocalTeamServerPreferencesUtility.getServerMemoryMB();
-		if (mb > estimatedMax) {
-			mb = estimatedMax;
-			LocalTeamServerPreferencesUtility.setServerMemoryMB(mb);
-		}
+    final int estimatedMax = MemoryUtility.computeMaxMemorySizeInMb();
+    int mb = LocalTeamServerPreferencesUtility.getServerMemoryMB();
+    if (mb > estimatedMax) {
+      mb = estimatedMax;
+      LocalTeamServerPreferencesUtility.setServerMemoryMB(mb);
+    }
 
-		final String label = I18N.msg(SERVER_MB_LABEL, mb);
-		f_serverMemoryMB = new ScaleFieldEditor(
-				LocalTeamServerPreferencesUtility.SERVER_MEMORY_MB, label
-						+ "     ", memoryGroup);
-		f_serverMemoryMB.fillIntoGrid(memoryGroup, 2);
-		f_serverMemoryMB.setMinimum(256);
-		f_serverMemoryMB.setMaximum(estimatedMax);
-		f_serverMemoryMB.setPageIncrement(256);
-		f_serverMemoryMB.setPage(this);
-		f_serverMemoryMB.setPreferenceStore(EclipseUIUtility.getPreferences());
-		f_serverMemoryMB.load();
-		f_serverMemoryMB.getScaleControl().addListener(SWT.Selection,
-				new Listener() {
-					@Override
-          public void handleEvent(Event event) {
-						updateMBInLabel();
-					}
-				});
+    final String label = I18N.msg(SERVER_MB_LABEL, SLUtility.toStringHumanWithCommas(mb));
+    f_serverMemoryMB = new ScaleFieldEditor(LocalTeamServerPreferencesUtility.SERVER_MEMORY_MB, label + "     ", memoryGroup);
+    f_serverMemoryMB.fillIntoGrid(memoryGroup, 2);
+    f_serverMemoryMB.setMinimum(256);
+    f_serverMemoryMB.setMaximum(estimatedMax);
+    f_serverMemoryMB.setPageIncrement(256);
+    f_serverMemoryMB.setPage(this);
+    f_serverMemoryMB.setPreferenceStore(EclipseUIUtility.getPreferences());
+    f_serverMemoryMB.load();
+    f_serverMemoryMB.getScaleControl().addListener(SWT.Selection, new Listener() {
+      @Override
+      public void handleEvent(Event event) {
+        updateMBInLabel();
+      }
+    });
 
-		f_estimate = new Label(memoryGroup, SWT.NONE);
-		f_estimate.setLayoutData(new GridData(SWT.LEFT, SWT.CENTER, false,
-				false, 2, 1));
-		f_estimate
-				.setText(I18N
-						.msg("sierra.eclipse.teamserver.preference.page.computedMaxToolMemoryLabel",
-								estimatedMax));
+    f_estimate = new Label(memoryGroup, SWT.NONE);
+    f_estimate.setLayoutData(new GridData(SWT.LEFT, SWT.CENTER, false, false, 2, 1));
+    f_estimate.setText(I18N.msg("sierra.eclipse.teamserver.preference.page.computedMaxToolMemoryLabel",
+        SLUtility.toStringHumanWithCommas(estimatedMax)));
 
-		memoryGroup.setLayout(new GridLayout(2, false));
+    memoryGroup.setLayout(new GridLayout(2, false));
 
-		final Group loggingGroup = new Group(panel, SWT.NONE);
-		loggingGroup
-				.setLayoutData(new GridData(SWT.FILL, SWT.TOP, true, false));
-		loggingGroup
-				.setText(I18N
-						.msg("sierra.eclipse.teamserver.preference.page.group.logging"));
+    final Group loggingGroup = new Group(panel, SWT.NONE);
+    loggingGroup.setLayoutData(new GridData(SWT.FILL, SWT.TOP, true, false));
+    loggingGroup.setText(I18N.msg("sierra.eclipse.teamserver.preference.page.group.logging"));
 
-		f_showAbove = new RadioGroupFieldEditor(
-				LocalTeamServerPreferencesUtility.SERVER_LOGGING_LEVEL,
-				I18N.msg("sierra.eclipse.teamserver.preference.page.showAbove"),
-				1, new String[][] {
-						{ Level.SEVERE.toString(), Level.SEVERE.toString() },
-						{ Level.WARNING.toString(), Level.WARNING.toString() },
-						{ Level.INFO.toString(), Level.INFO.toString() },
-						{ Level.FINE.toString(), Level.FINE.toString() },
-						{ Level.FINER.toString(), Level.FINER.toString() },
-						{ Level.FINEST.toString(), Level.FINEST.toString() } },
-				loggingGroup);
-		f_showAbove.setPage(this);
-		f_showAbove.setPreferenceStore(EclipseUIUtility.getPreferences());
-		f_showAbove.load();
+    f_showAbove = new RadioGroupFieldEditor(LocalTeamServerPreferencesUtility.SERVER_LOGGING_LEVEL,
+        I18N.msg("sierra.eclipse.teamserver.preference.page.showAbove"), 1, new String[][] {
+            { Level.SEVERE.toString(), Level.SEVERE.toString() }, { Level.WARNING.toString(), Level.WARNING.toString() },
+            { Level.INFO.toString(), Level.INFO.toString() }, { Level.FINE.toString(), Level.FINE.toString() },
+            { Level.FINER.toString(), Level.FINER.toString() }, { Level.FINEST.toString(), Level.FINEST.toString() } },
+        loggingGroup);
+    f_showAbove.setPage(this);
+    f_showAbove.setPreferenceStore(EclipseUIUtility.getPreferences());
+    f_showAbove.load();
 
-		/*
-		 * Allow access to help via the F1 key.
-		 */
-		PlatformUI
-				.getWorkbench()
-				.getHelpSystem()
-				.setHelp(parent,
-						"com.surelogic.sierra.client.eclipse.preferences-sierra");
+    /*
+     * Allow access to help via the F1 key.
+     */
+    PlatformUI.getWorkbench().getHelpSystem().setHelp(parent, "com.surelogic.sierra.client.eclipse.preferences-sierra");
 
-		return panel;
-	}
+    return panel;
+  }
 
-	private void updateMBInLabel() {
-		final int mb = f_serverMemoryMB.getScaleControl().getSelection();
-		f_serverMemoryMB.setLabelText(I18N.msg(SERVER_MB_LABEL, mb));
-	}
+  void updateMBInLabel() {
+    final int mb = f_serverMemoryMB.getScaleControl().getSelection();
+    f_serverMemoryMB.setLabelText(I18N.msg(SERVER_MB_LABEL, SLUtility.toStringHumanWithCommas(mb)));
+  }
 
-	@Override
-	protected void performDefaults() {
-		f_serverMemoryMB.loadDefault();
-		f_showAbove.loadDefault();
-		super.performDefaults();
+  @Override
+  protected void performDefaults() {
+    f_serverMemoryMB.loadDefault();
+    f_showAbove.loadDefault();
+    super.performDefaults();
 
-		updateMBInLabel();
-	}
+    updateMBInLabel();
+  }
 
-	@Override
-	public boolean performOk() {
-		f_serverMemoryMB.store();
-		f_showAbove.store();
-		return super.performOk();
-	}
+  @Override
+  public boolean performOk() {
+    f_serverMemoryMB.store();
+    f_showAbove.store();
+    return super.performOk();
+  }
 }
