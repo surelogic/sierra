84ec628b7b35371d59cd879c65b77b67fe40a3c6
Nathan Boy <nathan.boy@gmail.com>
2014-08-27 16:50:40 -0400

    Fixed up export job.

--- sierra-client-eclipse/src/com/surelogic/sierra/client/eclipse/actions/ImportSierraScanAction.java
+++ sierra-client-eclipse/src/com/surelogic/sierra/client/eclipse/actions/ImportSierraScanAction.java
@@ -5,48 +5,73 @@ import java.io.File;
 import org.eclipse.jface.action.IAction;
 import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.viewers.ISelection;
-import org.eclipse.swt.widgets.DirectoryDialog;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.FileDialog;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.IWorkbenchWindowActionDelegate;
+import org.eclipse.ui.PlatformUI;
 
 import com.surelogic.common.FileUtility;
 import com.surelogic.common.i18n.I18N;
 import com.surelogic.common.ui.EclipseUIUtility;
+import com.surelogic.sierra.client.eclipse.jobs.ImportScanDocumentJob;
+import com.surelogic.sierra.client.eclipse.model.DatabaseHub;
 import com.surelogic.sierra.client.eclipse.preferences.SierraPreferencesUtility;
 
 public class ImportSierraScanAction implements IWorkbenchWindowActionDelegate {
 
-  @Override
-  public void run(IAction action) {
-    Shell shell = EclipseUIUtility.getShell();
-    DirectoryDialog dd = new DirectoryDialog(shell);
-    String fileName = dd.open();
-    if (fileName != null) {
-      File f = new File(fileName);
-      File scanDirectory = SierraPreferencesUtility.getSierraScanDirectory();
-      if (f.exists() && !f.isDirectory()) {
-        FileUtility.copy(f, new File(scanDirectory, f.getName()));
-        // TODO Do the sierra import to the db and update ui
-      } else {
-        MessageDialog.openError(shell, I18N.msg("sierra.dialog.importScan.error.title"),
-            I18N.msg("sierra.dialog.importScan.error.title", f.getAbsolutePath()));
-      }
+    @Override
+    public void run(IAction action) {
+        Shell shell = EclipseUIUtility.getShell();
+        FileDialog fd = new FileDialog(PlatformUI.getWorkbench()
+                .getActiveWorkbenchWindow().getShell(), SWT.OPEN);
+        fd.setText("Import Scan");
+        fd.setFilterExtensions(new String[] { "*.sierra.gz", "*.sierra", "*.*" });
+        fd.setFilterNames(new String[] { "Scan Documents (*.sierra)",
+                "Compressed Scan Documents (*.sierra.gz)", "All Files (*.*)" });
+        String fileName = fd.open();
+        if (fileName != null) {
+            File f = new File(fileName);
+            if (f.exists() && !f.isDirectory()) {
+                File to = new File(
+                        SierraPreferencesUtility.getSierraScanDirectory(),
+                        f.getName());
+                if (!f.equals(to)) {
+                    FileUtility.copy(f, to);
+                }
+                final Runnable runAfterImport = new Runnable() {
+                    @Override
+                    public void run() {
+                        /* Notify that scan was completed */
+                        DatabaseHub.getInstance().notifyScanLoaded();
+                    }
+                };
+                ImportScanDocumentJob job = new ImportScanDocumentJob(f, null,
+                        runAfterImport);
+                job.schedule();
+            } else {
+                MessageDialog.openError(
+                        shell,
+                        I18N.msg("sierra.dialog.importScan.error.title"),
+                        I18N.msg("sierra.dialog.importScan.error.title",
+                                f.getAbsolutePath()));
+            }
+        }
     }
-  }
 
-  @Override
-  public void selectionChanged(IAction action, ISelection selection) {
-    // Nothing to do
-  }
+    @Override
+    public void selectionChanged(IAction action, ISelection selection) {
+        // Nothing to do
+    }
 
-  @Override
-  public void dispose() {
-    // Nothing to do
-  }
+    @Override
+    public void dispose() {
+        // Nothing to do
+    }
 
-  @Override
-  public void init(IWorkbenchWindow window) {
-    // Nothing to do
-  }
+    @Override
+    public void init(IWorkbenchWindow window) {
+        // Nothing to do
+    }
 }
