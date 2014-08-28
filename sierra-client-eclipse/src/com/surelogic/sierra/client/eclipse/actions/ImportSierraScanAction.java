ee7a91749ad9678942c039706d4df62518d4585a
Nathan Boy <nathan.boy@gmail.com>
2014-08-28 16:05:06 -0400

    working ant tasks.

--- sierra-client-eclipse/src/com/surelogic/sierra/client/eclipse/actions/ImportSierraScanAction.java
+++ sierra-client-eclipse/src/com/surelogic/sierra/client/eclipse/actions/ImportSierraScanAction.java
@@ -1,6 +1,18 @@
 package com.surelogic.sierra.client.eclipse.actions;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.logging.Level;
+import java.util.zip.GZIPInputStream;
+
+import javax.xml.stream.FactoryConfigurationError;
+import javax.xml.stream.XMLInputFactory;
+import javax.xml.stream.XMLStreamConstants;
+import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.XMLStreamReader;
 
 import org.eclipse.jface.action.IAction;
 import org.eclipse.jface.dialogs.MessageDialog;
@@ -14,6 +26,7 @@ import org.eclipse.ui.PlatformUI;
 
 import com.surelogic.common.FileUtility;
 import com.surelogic.common.i18n.I18N;
+import com.surelogic.common.logging.SLLogger;
 import com.surelogic.common.ui.EclipseUIUtility;
 import com.surelogic.sierra.client.eclipse.jobs.ImportScanDocumentJob;
 import com.surelogic.sierra.client.eclipse.model.DatabaseHub;
@@ -33,7 +46,8 @@ public class ImportSierraScanAction implements IWorkbenchWindowActionDelegate {
         String fileName = fd.open();
         if (fileName != null) {
             File f = new File(fileName);
-            if (f.exists() && !f.isDirectory()) {
+            String projectName = getProjectName(f);
+            if (projectName != null && f.exists() && !f.isDirectory()) {
                 File to = new File(
                         SierraPreferencesUtility.getSierraScanDirectory(),
                         f.getName());
@@ -47,8 +61,8 @@ public class ImportSierraScanAction implements IWorkbenchWindowActionDelegate {
                         DatabaseHub.getInstance().notifyScanLoaded();
                     }
                 };
-                ImportScanDocumentJob job = new ImportScanDocumentJob(f, null,
-                        runAfterImport);
+                ImportScanDocumentJob job = new ImportScanDocumentJob(f,
+                        projectName, runAfterImport);
                 job.schedule();
             } else {
                 MessageDialog.openError(
@@ -57,6 +71,43 @@ public class ImportSierraScanAction implements IWorkbenchWindowActionDelegate {
                         I18N.msg("sierra.dialog.importScan.error.title",
                                 f.getAbsolutePath()));
             }
+        }
+    }
+
+    private String getProjectName(File f) {
+        StringBuilder projectName = new StringBuilder();
+        try {
+            InputStream in = new FileInputStream(f);
+            if (f.getName().endsWith(FileUtility.GZIP_SUFFIX)) {
+                in = new GZIPInputStream(in);
+            }
+            XMLStreamReader reader = XMLInputFactory.newInstance()
+                    .createXMLStreamReader(in);
+            while (reader.getEventType() != XMLStreamConstants.START_ELEMENT
+                    || !reader.getLocalName().equals("project")) {
+                reader.next();
+            }
+            reader.next();
+            while (reader.getEventType() == XMLStreamConstants.CHARACTERS) {
+                projectName.append(reader.getText());
+                reader.next();
+            }
+        } catch (FileNotFoundException e) {
+            // Do nothing
+        } catch (XMLStreamException e) {
+            SLLogger.getLoggerFor(ImportSierraScanAction.class).log(
+                    Level.WARNING, "Error reading scan document", e);
+        } catch (FactoryConfigurationError e) {
+            SLLogger.getLoggerFor(ImportSierraScanAction.class).log(
+                    Level.WARNING, "Error reading scan document", e);
+        } catch (IOException e) {
+            SLLogger.getLoggerFor(ImportSierraScanAction.class).log(
+                    Level.WARNING, "Error reading scan document", e);
+        }
+        if (projectName.length() == 0) {
+            return null;
+        } else {
+            return projectName.toString();
         }
     }
 
--- sierra-client-eclipse/src/com/surelogic/sierra/client/eclipse/jobs/ImportScanDocumentJob.java
+++ sierra-client-eclipse/src/com/surelogic/sierra/client/eclipse/jobs/ImportScanDocumentJob.java
@@ -14,58 +14,57 @@ import com.surelogic.sierra.client.eclipse.model.BuglinkData;
 
 public final class ImportScanDocumentJob extends AbstractSierraDatabaseJob {
 
-	private final File f_scanDocument;
-	private final String projectName;
-	private final Runnable runAfter;
+    private final File f_scanDocument;
+    private final String projectName;
+    private final Runnable runAfter;
 
-	public ImportScanDocumentJob(final File scanDocument) {
-		this(scanDocument, null, null);
-	}
+    public ImportScanDocumentJob(final File scanDocument) {
+        this(scanDocument, null, null);
+    }
 
-	public ImportScanDocumentJob(final File scanDocument, final String proj,
-			final Runnable r) {
-		super("Loading "
-				+ (proj != null ? "scan document for " + proj : scanDocument
-						.getName()));
-		setPriority(Job.DECORATE);
+    public ImportScanDocumentJob(final File scanDocument, final String proj,
+            final Runnable r) {
+        super("Loading "
+                + (proj != null ? "scan document for " + proj : scanDocument
+                        .getName()));
+        setPriority(Job.DECORATE);
 
-		f_scanDocument = scanDocument;
-		if (proj == null) {
-			final String fileName = f_scanDocument.getName();
-			projectName = fileName.substring(0, fileName.indexOf(".sierra"));
-		} else {
-			projectName = proj;
-		}
-		runAfter = r;
-	}
+        f_scanDocument = scanDocument;
+        if (proj == null) {
+            throw new IllegalArgumentException("Project may not be null.");
+        } else {
+            projectName = proj;
+        }
+        runAfter = r;
+    }
 
-	@Override
-	protected IStatus run(final IProgressMonitor monitor) {
-		final SLProgressMonitorWrapper slProgressMonitorWrapper = new SLProgressMonitorWrapper(
-				monitor, this.getName());
-		slProgressMonitorWrapper.begin(100);
-		try {
-			loadScanDocument(slProgressMonitorWrapper);
-		} catch (final IllegalStateException e) {
-			if (e.getCause() instanceof SQLException
-					&& e.getMessage().contains("No current connection")) {
-				// Try again and see if we can get through
-				loadScanDocument(slProgressMonitorWrapper);
-			}
-		}
-		BuglinkData.getInstance().refresh();
-		if (slProgressMonitorWrapper.isCanceled()) {
-			return Status.CANCEL_STATUS;
-		} else {
-			if (runAfter != null) {
-				runAfter.run();
-			}
-			return Status.OK_STATUS;
-		}
-	}
+    @Override
+    protected IStatus run(final IProgressMonitor monitor) {
+        final SLProgressMonitorWrapper slProgressMonitorWrapper = new SLProgressMonitorWrapper(
+                monitor, getName());
+        slProgressMonitorWrapper.begin(100);
+        try {
+            loadScanDocument(slProgressMonitorWrapper);
+        } catch (final IllegalStateException e) {
+            if (e.getCause() instanceof SQLException
+                    && e.getMessage().contains("No current connection")) {
+                // Try again and see if we can get through
+                loadScanDocument(slProgressMonitorWrapper);
+            }
+        }
+        BuglinkData.getInstance().refresh();
+        if (slProgressMonitorWrapper.isCanceled()) {
+            return Status.CANCEL_STATUS;
+        } else {
+            if (runAfter != null) {
+                runAfter.run();
+            }
+            return Status.OK_STATUS;
+        }
+    }
 
-	private void loadScanDocument(final SLProgressMonitor wrapper) {
-		ScanDocumentUtility.loadScanDocument(f_scanDocument, wrapper,
-				projectName);
-	}
+    private void loadScanDocument(final SLProgressMonitor wrapper) {
+        ScanDocumentUtility.loadScanDocument(f_scanDocument, wrapper,
+                projectName);
+    }
 }
