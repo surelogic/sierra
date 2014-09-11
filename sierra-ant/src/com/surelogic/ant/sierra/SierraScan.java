e1d9912664c4dd4047a6fe118dc9c3d19a9c1a3f
Nathan Boy <nathan.boy@gmail.com>
2014-09-11 14:37:41 -0400

    Added some more dialog messages around saving ant tasks.

--- sierra-ant/src/com/surelogic/ant/sierra/SierraScan.java
+++ sierra-ant/src/com/surelogic/ant/sierra/SierraScan.java
@@ -1,5 +1,9 @@
 package com.surelogic.ant.sierra;
 
+import static com.surelogic.sierra.tool.SierraToolConstants.PARSED_FILE_SUFFIX;
+import static com.surelogic.sierra.tool.SierraToolConstants.PARSED_ZIP_FILE_SUFFIX;
+import static com.surelogic.sierra.tool.SierraToolConstants.USE_ZIP;
+
 import java.io.File;
 
 import org.apache.tools.ant.BuildException;
@@ -44,6 +48,11 @@ public class SierraScan extends Javac {
         return document;
     }
 
+    public File getScanFile() {
+        return new File(getDocument()
+                + (USE_ZIP ? PARSED_ZIP_FILE_SUFFIX : PARSED_FILE_SUFFIX));
+    }
+
     public void setDocument(String doc) {
         document = doc;
     }
@@ -59,13 +68,20 @@ public class SierraScan extends Javac {
 
         if (newFiles.length > 0) {
             File[] newCompileList = new File[compileList.length
-                                             + newFiles.length];
+                    + newFiles.length];
             System.arraycopy(compileList, 0, newCompileList, 0,
                     compileList.length);
             System.arraycopy(newFiles, 0, newCompileList, compileList.length,
                     newFiles.length);
             compileList = newCompileList;
         }
+    }
+
+    @Override
+    public void execute() {
+        log(String.format("Writing scan document to %s.", getScanFile()
+                .getAbsolutePath()));
+        super.execute();
     }
 
     /**
