68127f48d8f58d0308967eee22c710f7f0e5481b
Nathan Boy <nathan.boy@gmail.com>
2015-04-13 14:56:10 -0400

    Bug fix where filter description text was mismatched.

--- sierra-client-eclipse/src/com/surelogic/sierra/client/eclipse/actions/ImportSierraScanAction.java
+++ sierra-client-eclipse/src/com/surelogic/sierra/client/eclipse/actions/ImportSierraScanAction.java
@@ -36,91 +36,111 @@ import com.surelogic.sierra.client.eclipse.preferences.SierraPreferencesUtility;
 
 public class ImportSierraScanAction implements IWorkbenchWindowActionDelegate {
 
-  @Override
-  public void run(IAction action) {
-    final FileDialog fd = new FileDialog(EclipseUIUtility.getShell(), SWT.OPEN);
-    fd.setText("Import Ant/Maven Scan");
-    fd.setFilterExtensions(new String[] { "*.sierra.gz", "*.sierra", "*.*" });
-    fd.setFilterNames(new String[] { "Scan Documents (*.sierra)", "Compressed Scan Documents (*.sierra.gz)", "All Files (*.*)" });
-    String fileName = fd.open();
-    if (fileName != null) {
-      File f = new File(fileName);
-      final String projectName = getProjectName(f);
-      if (projectName != null && f.exists() && !f.isDirectory()) {
-        File to = new File(SierraPreferencesUtility.getSierraScanDirectory(), f.getName());
-        if (!f.equals(to)) {
-          FileUtility.copy(f, to);
+    @Override
+    public void run(IAction action) {
+        final FileDialog fd = new FileDialog(EclipseUIUtility.getShell(),
+                SWT.OPEN);
+        fd.setText("Import Ant/Maven Scan");
+        fd.setFilterExtensions(new String[] { "*.sierra.gz", "*.sierra", "*.*" });
+        fd.setFilterNames(new String[] {
+                "Compressed Scan Documents (*.sierra.gz)",
+                "Scan Documents (*.sierra)", "All Files (*.*)" });
+        String fileName = fd.open();
+        if (fileName != null) {
+            File f = new File(fileName);
+            final String projectName = getProjectName(f);
+            if (projectName != null && f.exists() && !f.isDirectory()) {
+                File to = new File(
+                        SierraPreferencesUtility.getSierraScanDirectory(),
+                        f.getName());
+                if (!f.equals(to)) {
+                    FileUtility.copy(f, to);
+                }
+                final Runnable runAfterImport = new Runnable() {
+                    @Override
+                    public void run() {
+                        /* Notify that scan was completed */
+                        DatabaseHub.getInstance().notifyScanLoaded();
+                        SLUIJob job = new SLUIJob() {
+
+                            @Override
+                            public IStatus runInUIThread(
+                                    IProgressMonitor monitor) {
+                                MessageDialog
+                                .openInformation(
+                                        EclipseUIUtility.getShell(),
+                                        I18N.msg("sierra.dialog.importScan.success.title"),
+                                        I18N.msg(
+                                                "sierra.dialog.importScan.success.msg",
+                                                projectName));
+                                return Status.OK_STATUS;
+                            }
+                        };
+                        job.schedule();
+                    }
+                };
+                ImportScanDocumentJob job = new ImportScanDocumentJob(f,
+                        projectName, runAfterImport);
+                job.schedule();
+            } else {
+                MessageDialog.openError(
+                        EclipseUIUtility.getShell(),
+                        I18N.msg("sierra.dialog.importScan.error.title"),
+                        I18N.msg("sierra.dialog.importScan.error.msg",
+                                f.getAbsolutePath()));
+            }
         }
-        final Runnable runAfterImport = new Runnable() {
-          @Override
-          public void run() {
-            /* Notify that scan was completed */
-            DatabaseHub.getInstance().notifyScanLoaded();
-            SLUIJob job = new SLUIJob() {
-
-              @Override
-              public IStatus runInUIThread(IProgressMonitor monitor) {
-                MessageDialog.openInformation(EclipseUIUtility.getShell(), I18N.msg("sierra.dialog.importScan.success.title"),
-                    I18N.msg("sierra.dialog.importScan.success.msg", projectName));
-                return Status.OK_STATUS;
-              }
-            };
-            job.schedule();
-          }
-        };
-        ImportScanDocumentJob job = new ImportScanDocumentJob(f, projectName, runAfterImport);
-        job.schedule();
-      } else {
-        MessageDialog.openError(EclipseUIUtility.getShell(), I18N.msg("sierra.dialog.importScan.error.title"),
-            I18N.msg("sierra.dialog.importScan.error.msg", f.getAbsolutePath()));
-      }
     }
-  }
 
-  private String getProjectName(File f) {
-    StringBuilder projectName = new StringBuilder();
-    try {
-      InputStream in = new FileInputStream(f);
-      if (f.getName().endsWith(FileUtility.GZIP_SUFFIX)) {
-        in = new GZIPInputStream(in);
-      }
-      XMLStreamReader reader = XMLInputFactory.newInstance().createXMLStreamReader(in);
-      while (reader.getEventType() != XMLStreamConstants.START_ELEMENT || !reader.getLocalName().equals("project")) {
-        reader.next();
-      }
-      reader.next();
-      while (reader.getEventType() == XMLStreamConstants.CHARACTERS) {
-        projectName.append(reader.getText());
-        reader.next();
-      }
-    } catch (FileNotFoundException e) {
-      // Do nothing
-    } catch (XMLStreamException e) {
-      SLLogger.getLoggerFor(ImportSierraScanAction.class).log(Level.WARNING, "Error reading scan document", e);
-    } catch (FactoryConfigurationError e) {
-      SLLogger.getLoggerFor(ImportSierraScanAction.class).log(Level.WARNING, "Error reading scan document", e);
-    } catch (IOException e) {
-      SLLogger.getLoggerFor(ImportSierraScanAction.class).log(Level.WARNING, "Error reading scan document", e);
+    private String getProjectName(File f) {
+        StringBuilder projectName = new StringBuilder();
+        try {
+            InputStream in = new FileInputStream(f);
+            if (f.getName().endsWith(FileUtility.GZIP_SUFFIX)) {
+                in = new GZIPInputStream(in);
+            }
+            XMLStreamReader reader = XMLInputFactory.newInstance()
+                    .createXMLStreamReader(in);
+            while (reader.getEventType() != XMLStreamConstants.START_ELEMENT
+                    || !reader.getLocalName().equals("project")) {
+                reader.next();
+            }
+            reader.next();
+            while (reader.getEventType() == XMLStreamConstants.CHARACTERS) {
+                projectName.append(reader.getText());
+                reader.next();
+            }
+        } catch (FileNotFoundException e) {
+            // Do nothing
+        } catch (XMLStreamException e) {
+            SLLogger.getLoggerFor(ImportSierraScanAction.class).log(
+                    Level.WARNING, "Error reading scan document", e);
+        } catch (FactoryConfigurationError e) {
+            SLLogger.getLoggerFor(ImportSierraScanAction.class).log(
+                    Level.WARNING, "Error reading scan document", e);
+        } catch (IOException e) {
+            SLLogger.getLoggerFor(ImportSierraScanAction.class).log(
+                    Level.WARNING, "Error reading scan document", e);
+        }
+        if (projectName.length() == 0) {
+            return null;
+        } else {
+            return projectName.toString();
+        }
     }
-    if (projectName.length() == 0) {
-      return null;
-    } else {
-      return projectName.toString();
+
+    @Override
+    public void selectionChanged(IAction action, ISelection selection) {
+        // Nothing to do
     }
-  }
 
-  @Override
-  public void selectionChanged(IAction action, ISelection selection) {
-    // Nothing to do
-  }
+    @Override
+    public void dispose() {
+        // Nothing to do
+    }
 
-  @Override
-  public void dispose() {
-    // Nothing to do
-  }
-
-  @Override
-  public void init(IWorkbenchWindow window) {
-    // Nothing to do
-  }
+    @Override
+    public void init(IWorkbenchWindow window) {
+        // Nothing to do
+    }
 }
